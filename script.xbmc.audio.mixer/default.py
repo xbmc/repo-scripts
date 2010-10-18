9ff8a54ac134b79c4e19ae1548cd000022b72310
Zeljko Ametovic <amet@no.spam>
2010-10-18 15:14:10 +0400

    [script.xbmc.audio.mixer] -v1.0.0 initial commit

---
+++ script.xbmc.audio.mixer/default.py
@@ -0,0 +1,45 @@
+# *
+# *      Copyright (C) 2005-2010 Team XBMC
+# *      http://www.xbmc.org
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with XBMC; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# */
+
+
+import sys
+import os
+import xbmcaddon
+
+BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( os.getcwd(), "resources", "lib" ) )
+sys.path.append (BASE_RESOURCE_PATH)
+
+__scriptname__ = "XBMC Audio Mixer"
+__author__     = "Team XBMC"
+__GUI__        = "Team XBMC"
+__scriptId__   = "script.xbmc.audio.mixer"
+__settings__   = xbmcaddon.Addon(id=__scriptId__)
+__language__   = __settings__.getLocalizedString
+__version__    = __settings__.getAddonInfo("version")
+
+xbmc.output("##### [%s] - Version: %s" % (__scriptname__,__version__,),level=xbmc.LOGDEBUG )
+
+if ( __name__ == "__main__" ):
+    import gui
+    ui = gui.GUI( "%s.xml" % __scriptId__.replace(".","-") , os.getcwd(), "Default")
+    ui.doModal()
+    del ui
+    sys.modules.clear()
