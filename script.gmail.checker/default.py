6dc90ad26c95aa7949a0c71b2d7916b80c804e77
Zeljko Ametovic <amet@no.spam>
2010-11-29 12:22:07 +0400

    [script.gmail.checker] -v 1.0.4
    more os.getcwd() removals

--- script.gmail.checker/default.py
+++ script.gmail.checker/default.py
@@ -7,7 +7,7 @@ __author__ = "Amet"
 __url__ = ""
 __svn_url__ = ""
 __credits__ = ""
-__version__ = "1.0.0"
+__version__ = "1.0.4"
 __XBMC_Revision__ = "22240"
 
 
@@ -15,12 +15,12 @@ __settings__   = xbmcaddon.Addon(id='script.gmail.checker')
 __language__   = __settings__.getLocalizedString
 __cwd__        = __settings__.getAddonInfo('path')
 
-BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( os.getcwd(), 'resources', 'lib' ) )
+BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) )
 sys.path.append (BASE_RESOURCE_PATH)
 
 if __name__ == "__main__":
     import gui
-    ui = gui.GUI( "script-GmailChecker-main.xml" , os.getcwd(), "Default")
+    ui = gui.GUI( "script-GmailChecker-main.xml" , __cwd__, "Default")
     ui.doModal()
     del ui
     sys.modules.clear()
