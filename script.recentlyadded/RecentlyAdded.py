bf203e9138c4719fe0102b64d98719b3af3b4f5e
amet <amet.nospam@gmail.com>
2011-05-22 11:36:18 +0400

    [script.recentlyadded] -v 3.0.0.
    - removed all code and print the warnings for Eden-pre repo
    
    Recently added script is no longer needed and the skins should remove dependencies for it
    https://github.com/xbmc/xbmc/commit/a61cf8750163905eff53a3adb0090954e4494a22
    forum thread can be found at http://forum.xbmc.org/showthread.php?t=99502

---
+++ script.recentlyadded/RecentlyAdded.py
@@ -0,0 +1,31 @@
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with XBMC; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# *  this file was taken from http://xbmc-addons.googlecode.com/svn/packages/scripts/RecentlyAdded.py
+# *  and modified to be a XBMC Add-on under gpl2 license on 20. November 2010.
+# *
+# *  Thanks to:
+# *
+# *  Nuka for the original RecentlyAdded.py
+# *
+# *  ronie and Hitcher for the improvements made while in official repo
+# *  
+# *  Team XBMC
+
+print "#########################################################################################"
+print "Recently added script is no longer needed and the skins should remove dependencies for it"
+print "https://github.com/xbmc/xbmc/commit/a61cf8750163905eff53a3adb0090954e4494a22"
+print "forum thread can be found at http://forum.xbmc.org/showthread.php?t=99502"
+print "#########################################################################################"
