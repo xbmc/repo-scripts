0da2f410378a81c7ecb2870bcbc6ba46da393cef
amet <amet.nospam@gmail.com>
2011-09-13 01:01:44 +0400

    [script.xbmc-pbx-addon] -v 1.0.8
      * Updated images (fanart and icons) to comply with this guideline: http://wiki.xbmc.org/index.php?title=Add-ons_for_XBMC_%28Development%29
      * Removed (unnecessary) check for Linux64 that was causing trouble with XBMC Eden(-pre). Thanks freebs (XBMC Forum member);
      * Added option to disable background service (so video is not going to be paused and notification popups are not going to be shown);
      * Added check for Front-end and Back-end versions to make sure they match;
      * Some other minor changes;

---
+++ script.xbmc-pbx-addon/bgservice.py
@@ -0,0 +1,258 @@
+'''
+
+    XBMC PBX Addon
+        Fron-end (XBMC) side
+        This script will be running as a service in background
+
+
+'''
+
+# Script constants
+__addon__       = "XBMC PBX Addon"
+__addon_id__    = "script.xbmc-pbx-addon"
+__author__      = "hmronline"
+__url__         = "http://code.google.com/p/xbmc-pbx-addon/"
+__version__     = "1.0.8"
+
+# Modules
+import sys, os
+import xbmc, xbmcaddon
+import re, traceback, time
+
+
+xbmc.log("[%s]: Version %s\n" % (__addon__,__version__))
+
+# Get environment OS
+__os__          = os.environ.get( "OS", "win32" )
+xbmc.log("[%s]: XBMC for %s\n" % (__addon__,__os__))
+
+__language__    = xbmcaddon.Addon(__addon_id__).getLocalizedString
+CWD             = xbmcaddon.Addon(__addon_id__).getAddonInfo('path')
+RESOURCE_PATH   = os.path.join(CWD, "resources" )
+
+sys.path.append(os.path.join(RESOURCE_PATH,'lib'))
+from Asterisk.Manager import Manager
+import Asterisk.Manager, Asterisk.Util
+
+
+
+#############################################################################################################
+def log(msg):
+    try:
+        xbmc.log("[%s]: %s\n" % (__addon__,str(msg)))
+    except:
+        pass
+
+#############################################################################################################
+class get_incoming_call(object):
+
+    def __init__(self):
+        log("__init__()")
+        global asterisk_series
+        self.asterisk_series = asterisk_series
+        self.DEBUG = False
+        self.xbmc_player_paused = False
+        self.asterisk_now_playing = False
+        self.ast_first_uniqid = 0
+        self.ast_my_uniqid = 0
+        self.event_callerid = ""
+        self.events = Asterisk.Util.EventCollection()
+        self.events.clear()
+        self.events.subscribe('Newchannel',self.NewChannel)
+        self.events.subscribe('Newcallerid',self.NewCallerID)       # Asterisk 1.4
+        self.events.subscribe('NewCallerid',self.NewCallerID)       # Asterisk 1.6
+        self.events.subscribe('Hangup',self.Hangup)
+
+    #####################################################################################################
+    def NewChannel(self,pbx,event):
+        settings = xbmcaddon.Addon(__addon_id__)
+        DEBUG = settings.getSetting("xbmc_debug")
+        arr_chan_states = ['Down','Ring']
+        asterisk_chan_state = str(arr_chan_states[int(settings.getSetting("asterisk_chan_state"))])
+        del settings
+        self.DEBUG = False
+        if (DEBUG == "true"):
+            self.DEBUG = True
+        if (self.DEBUG):
+            log("> NewChannel()")
+            log(">> UniqueID: " + event.Uniqueid)
+        if (self.asterisk_series == "1.4"):
+            # Asterisk 1.4
+            event_state = str(event.State)
+        else:
+            # Asterisk 1.6+
+            event_state = str(event.ChannelStateDesc)
+        if (self.DEBUG): log(">> State: " + event_state)
+        if (event_state == asterisk_chan_state and self.ast_my_uniqid == 0 and self.ast_first_uniqid == 0):
+            self.ast_my_uniqid = event.Uniqueid
+            if (self.DEBUG):
+                log(">>> Will attach to this one:")
+                log(">>> UniqueID: " + self.ast_my_uniqid)
+                log(">>> State: " + asterisk_chan_state)
+        if (self.ast_first_uniqid == 0):
+            self.ast_first_uniqid = event.Uniqueid
+
+    #####################################################################################################
+    def NewCallerID(self,pbx,event):
+        if (self.DEBUG):
+            log("> NewCallerID()")
+            log(">> UniqueID: " + event.Uniqueid)
+        if (self.asterisk_series == "1.4"):
+            # Asterisk 1.4
+            event_callerid_num = str(event.CallerID)
+        else:
+            # Asterisk 1.6+
+            event_callerid_num = str(event.CallerIDNum)
+        if (event.CallerIDName != "" and event_callerid_num != ""):
+            self.event_callerid = str(event.CallerIDName + " <" + event_callerid_num + ">")
+        if (self.DEBUG): log(">> CallerID: " + self.event_callerid)
+        if (event.Uniqueid == self.ast_my_uniqid and self.event_callerid != ""):
+            self.newcall_actions(event)
+
+    #####################################################################################################
+    def Hangup(self,pbx,event):
+        if (self.DEBUG):
+            log("> Hangup()")
+            log(">> UniqueID: " + event.Uniqueid)
+        if (event.Uniqueid == self.ast_first_uniqid):
+            self.ast_first_uniqid = 0
+        if (event.Uniqueid == self.ast_my_uniqid):
+            self.ast_my_uniqid = 0
+            self.hangup_actions(event)
+
+    #####################################################################################################
+    def hangup_actions(self,event):
+        log("> hangup_actions()")
+        if (self.DEBUG):
+            log(">> UniqueID: " + event.Uniqueid)
+        self.asterisk_now_playing = False
+        xbmc_player = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+        if (xbmc_player.isPlaying() == 1):
+            # Resume media
+            xbmc_remaining_time = xbmc_player.getTotalTime() - xbmc_player.getTime()
+            time.sleep(1)
+            xbmc_new_remaining_time = xbmc_player.getTotalTime() - xbmc_player.getTime()
+            if (self.xbmc_player_paused and xbmc_remaining_time == xbmc_new_remaining_time):
+                log(">> Resume media...")
+                xbmc_player.pause()
+                self.xbmc_player_paused = False
+        del xbmc_player
+
+    #####################################################################################################
+    def newcall_actions(self,event):
+        log("> newcall_actions()")
+        asterisk_alert_info = str(pbx.Getvar(event.Channel,"ALERT_INFO",""))
+        if (self.DEBUG):
+            log(">> Channel: " + str(event.Channel))
+            log(">> UniqueID: " + self.ast_my_uniqid)
+            log(">> CallerID: " + self.event_callerid)
+            log(">> ALERT_INFO: " + asterisk_alert_info)
+        settings = xbmcaddon.Addon(__addon_id__)
+        arr_timeout = [5,10,15,20,25,30]
+        xbmc_oncall_notification_timeout = int(arr_timeout[int(settings.getSetting("xbmc_oncall_notification_timeout"))])
+        cfg_asterisk_cid_alert_info = settings.getSetting("asterisk_cid_alert_info").strip(' \t\n\r')
+        cfg_asterisk_redir_alert_info = settings.getSetting("asterisk_redir_alert_info").strip(' \t\n\r')
+        asterisk_now_playing_context = settings.getSetting("asterisk_now_playing_context").strip(' \t\n\r')
+        xbmc_img = xbmc.translatePath(os.path.join(RESOURCE_PATH,'media','xbmc-pbx-addon.png'))
+        xbmc_oncall_pause_media = False
+        if (settings.getSetting("xbmc_oncall_pause_media") == "true"):
+            xbmc_oncall_pause_media = True
+        asterisk_now_playing_enabled = False
+        if (settings.getSetting("asterisk_now_playing_enabled") == "true"):
+            asterisk_now_playing_enabled = True
+        xbmc_oncall_notification = False
+        if (settings.getSetting("xbmc_oncall_notification") == "true"):
+            xbmc_oncall_notification = True
+        del settings
+        xbmc_player = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+        if (xbmc_player.isPlaying() == 1):
+            log(">> XBMC is playing content...")
+            if (xbmc_player.isPlayingAudio() == 1):
+                info_tag = xbmc_player.getMusicInfoTag(object)
+                if (self.DEBUG):
+                    log(">> Music title: " + info_tag.getTitle())
+                del info_tag
+            if (xbmc_player.isPlayingVideo() == 1):
+                xbmc_remaining_time = xbmc_player.getTotalTime() - xbmc_player.getTime()
+                info_tag = xbmc_player.getVideoInfoTag(object)
+                xbmc_video_title = info_tag.getTitle()
+                xbmc_video_rating = info_tag.getRating()
+                del info_tag
+                if (self.DEBUG):
+                    log(">> Video title: " + xbmc_video_title)
+                    log(">> Rating: " + str(xbmc_video_rating))
+                    log(">> Remaining time (minutes): " + str(round(xbmc_remaining_time/60)))
+                # Pause Video
+                if (xbmc_oncall_pause_media):
+                    time.sleep(1)
+                    if (asterisk_alert_info == cfg_asterisk_cid_alert_info or cfg_asterisk_cid_alert_info == ''):
+                        xbmc_new_remaining_time = xbmc_player.getTotalTime() - xbmc_player.getTime()
+                        if (not self.xbmc_player_paused and xbmc_remaining_time > xbmc_new_remaining_time):
+                            log(">> Pausing player...")
+                            xbmc_player.pause()
+                            self.xbmc_player_paused = True
+                # Redirect Incoming Call
+                if (asterisk_now_playing_enabled):
+                    try:
+                        if ((asterisk_alert_info == cfg_asterisk_redir_alert_info or cfg_asterisk_redir_alert_info == '') and not self.asterisk_now_playing):
+                            log(">> Redirecting call...")
+                            pbx.Setvar(event.Channel,"xbmc_video_title",xbmc_video_title)
+                            pbx.Setvar(event.Channel,"xbmc_remaining_time",round(xbmc_remaining_time/60))
+                            pbx.Redirect(event.Channel,asterisk_now_playing_context)
+                            self.asterisk_now_playing = True
+                    except:
+                        xbmc_notification = str(sys.exc_info()[1])
+                        log(">> Notification: " + xbmc_notification)
+                        xbmc.executebuiltin("XBMC.Notification("+ __language__(30051) +","+ xbmc_notification +","+ str(15*1000) +","+ xbmc_img +")")
+        del xbmc_player
+        # Show Incoming Call Notification Popup
+        if (xbmc_oncall_notification):
+            if (asterisk_alert_info == cfg_asterisk_cid_alert_info or cfg_asterisk_cid_alert_info == ''):
+                xbmc_notification = self.event_callerid
+                log(">> Notification: " + xbmc_notification)
+                xbmc.executebuiltin("XBMC.Notification("+ __language__(30050) +","+ xbmc_notification +","+ str(xbmc_oncall_notification_timeout*1000) +","+ xbmc_img +")")
+
+
+#################################################################################################################
+# Starts here
+#################################################################################################################
+
+try:
+    log("Running in background...")
+    settings = xbmcaddon.Addon(__addon_id__)
+    DEBUG = settings.getSetting("xbmc_debug")
+    xbmc_bgservice = settings.getSetting("xbmc_bgservice")
+    manager_host_port = settings.getSetting("asterisk_manager_host"),int(settings.getSetting("asterisk_manager_port"))
+    manager_user = settings.getSetting("asterisk_manager_user")
+    manager_pass = settings.getSetting("asterisk_manager_pass")
+    vm = settings.getSetting("asterisk_vm_mailbox") +"@"+ settings.getSetting("asterisk_vm_context")
+    arr_timeout = [5,10,15,20,25,30]
+    xbmc_vm_notification_timeout = int(arr_timeout[int(settings.getSetting("xbmc_vm_notification_timeout"))])
+    del settings
+    if (xbmc_bgservice == "true"):
+        log(">> Background service is disabled.")
+    else:
+        pbx = Manager(manager_host_port,manager_user,manager_pass)
+        asterisk_version = str(pbx.Command("core show version")[1])
+        asterisk_series = asterisk_version[9:12]
+        if (DEBUG == "true"): log(">> " + asterisk_version)
+        log(">> Asterisk: " + asterisk_series)
+        vm_count = str(pbx.MailboxCount(vm)[0])
+        xbmc_notification = __language__(30053) + vm_count
+        xbmc_img = xbmc.translatePath(os.path.join(RESOURCE_PATH,'media','xbmc-pbx-addon.png'))
+        log(">> Notification: " + xbmc_notification)
+        xbmc.executebuiltin("XBMC.Notification("+ __language__(30052) +","+ xbmc_notification +","+ str(xbmc_vm_notification_timeout*1000) +","+ xbmc_img +")")
+        grab = get_incoming_call()
+        pbx.events += grab.events
+        pbx.serve_forever()
+except:
+    xbmc_notification = str(sys.exc_info()[1])
+    xbmc_img = xbmc.translatePath(os.path.join(RESOURCE_PATH,'media','xbmc-pbx-addon.png'))
+    log(">> Notification: " + xbmc_notification)
+    xbmc.executebuiltin("XBMC.Notification("+ __language__(30051) +","+ xbmc_notification +","+ str(15*1000) +","+ xbmc_img +")")
+try:
+    del grab
+    del pbx
+except:
+    pass
+
---
+++ script.xbmc-pbx-addon/default.py
@@ -0,0 +1,336 @@
+'''
+
+    XBMC PBX Addon
+        Fron-end (XBMC) side
+        This script is the User Interface
+
+
+'''
+
+# Script constants
+__addon__       = "XBMC PBX Addon"
+__addon_id__    = "script.xbmc-pbx-addon"
+__author__      = "hmronline"
+__url__         = "http://code.google.com/p/xbmc-pbx-addon/"
+__version__     = "1.0.8"
+
+# Modules
+import sys, os
+import xbmc, xbmcaddon, xbmcgui
+import re, traceback, time
+import urllib, urlparse, urllib2, xml.dom.minidom
+
+xbmc.log("[%s]: Version %s\n" % (__addon__,__version__))
+
+# Get environment OS
+__os__          = os.environ.get( "OS", "win32" )
+xbmc.log("[%s]: XBMC for %s\n" % (__addon__,__os__))
+
+__language__    = xbmcaddon.Addon(__addon_id__).getLocalizedString
+CWD             = xbmcaddon.Addon(__addon_id__).getAddonInfo('path')
+RESOURCE_PATH   = os.path.join(CWD, "resources" )
+
+sys.path.append(os.path.join(RESOURCE_PATH,'lib'))
+from Asterisk.Manager import Manager
+import Asterisk.Manager, Asterisk.Util
+
+ACTION_EXIT_SCRIPT  = (9,10,247,275,61467,216,257,61448,)
+
+#############################################################################################################
+def log(msg):
+    try:
+        xbmc.log("[%s]: %s\n" % (__addon__,str(msg)))
+    except:
+        pass
+
+#############################################################################################################
+class MainGUI(xbmcgui.WindowXML):
+
+    def __init__(self,*args,**kwargs):
+        log("__init__()")
+        xbmcgui.WindowXML.__init__(self)
+
+    #####################################################################################################
+    def onInit(self):
+        log("> onInit()")
+        settings = xbmcaddon.Addon(__addon_id__)
+        DEBUG = settings.getSetting("xbmc_debug")
+        del settings
+        if (DEBUG == "true"):
+            self.DEBUG = True
+        else:
+            self.DEBUG = False
+        dialog = xbmcgui.DialogProgress()
+        # Starting...
+        dialog.create(__addon__,__language__(30061))
+        try:
+            # Skin Setup...
+            dialog.update(25,__language__(30062))
+            self.skinSetup()
+            # Fetching Asterisk Info...
+            dialog.update(50,__language__(30063))
+            self.getInfo()
+            # Displaying Asterisk Info...
+            dialog.update(75,__language__(30064))
+            self.showInfo()
+            # Done...
+            dialog.update(100,__language__(30065))
+        except:
+            xbmc_notification = str(sys.exc_info()[1])
+            xbmc_img = xbmc.translatePath(os.path.join(RESOURCE_PATH,'media','xbmc-pbx-addon.png'))
+            log(">> Notification: " + xbmc_notification)
+            xbmc.executebuiltin("XBMC.Notification("+ __language__(30051) +","+ xbmc_notification +","+ str(15*1000) +","+ xbmc_img +")")
+        dialog.close()
+        del dialog
+        log(">> Done.")
+
+    #####################################################################################################
+    def skinSetup(self):
+        log("> skinSetup()")
+        if (__os__ == 'xbox'): xbmcgui.lock()
+        self.getControl(109).setLabel(__language__(30107))  # CDR toggle button
+        self.getControl(110).setLabel(__language__(30101))  # CDR toggle button
+        self.getControl(111).setLabel(__language__(30102))  # VM toggle button
+        self.getControl(112).setLabel(__language__(30103))  # Settings button
+        self.getControl(140).setLabel(__language__(30130))  # CDR - start
+        self.getControl(141).setLabel(__language__(30116))  # CDR - channel
+        self.getControl(142).setLabel(__language__(30112))  # CDR - src
+        self.getControl(143).setLabel(__language__(30115))  # CDR - clid
+        self.getControl(144).setLabel(__language__(30113))  # CDR - dst
+        self.getControl(145).setLabel(__language__(30125))  # CDR - disposition
+        self.getControl(146).setLabel(__language__(30123))  # CDR - duration
+        self.getControl(161).setLabel(__language__(30159))  # VM - origtime
+        self.getControl(162).setLabel(__language__(30157))  # VM - callerid
+        self.getControl(163).setLabel(__language__(30155))  # VM - priority
+        self.getControl(164).setLabel(__language__(30151))  # VM - origmailbox
+        self.getControl(165).setLabel(__language__(30162))  # VM - duration
+        if (__os__ == 'xbox'): xbmcgui.unlock()
+
+    #####################################################################################################
+    def getInfo(self):
+        log("> getInfo()")
+        settings = xbmcaddon.Addon(__addon_id__)
+        manager_host_port = settings.getSetting("asterisk_manager_host"),int(settings.getSetting("asterisk_manager_port"))
+        manager_user = settings.getSetting("asterisk_manager_user")
+        manager_pass = settings.getSetting("asterisk_manager_pass")
+        asterisk_vm_mailbox = settings.getSetting("asterisk_vm_mailbox")
+        asterisk_vm_context = settings.getSetting("asterisk_vm_context")
+        str_url = settings.getSetting("asterisk_info_url")
+        del settings
+        pbx = Manager(manager_host_port,manager_user,manager_pass)
+        asterisk_version = str(pbx.Command("core show version")[1])
+        del pbx
+        log(">> " + asterisk_version)
+        str_url = str_url +"?vm&cdr&mailbox="+ asterisk_vm_mailbox
+        str_url = str_url +"&vmcontext="+ asterisk_vm_context
+        if (self.DEBUG):
+            log(">> " + str_url)
+        f = urllib.urlopen(str_url)
+        self.dom = xml.dom.minidom.parse(f)
+        if (self.DEBUG):
+            log(self.dom.toxml())
+        f.close()
+        del f
+
+    #####################################################################################################
+    def showInfo(self):
+        log("> showInfo()")
+        backend_version = "unknown"
+        for node in self.dom.getElementsByTagName('version'):
+            backend_version = node.firstChild.data
+        options = {"cdr":120,"vm":121}
+        if (__os__ == 'xbox'): xbmcgui.lock()
+        for option in options.keys():
+            self.getControl(options[option]).reset()
+            # Parse CDR/VM XML content
+            for node in self.dom.getElementsByTagName(option):
+                listitem = xbmcgui.ListItem()
+                for childNode in node.childNodes:
+                    if (childNode.nodeName != "#text"):
+                        if (childNode.firstChild):
+                            listitem.setProperty(childNode.nodeName,childNode.firstChild.data)
+                        else:
+                            listitem.setProperty(childNode.nodeName,"")
+                self.getControl(options[option]).addItem(listitem)
+                del listitem
+        if (__os__ == 'xbox'): xbmcgui.unlock()
+        del self.dom
+        if (backend_version != __version__):
+            log(">> Version mismatch!: Frontend is " + __version__ + " while Backend is " + backend_version)
+            xbmc_notification = "You have to update the backend!"
+            xbmc_img = xbmc.translatePath(os.path.join(RESOURCE_PATH,'media','xbmc-pbx-addon.png'))
+            log(">> Notification: " + xbmc_notification)
+            xbmc.executebuiltin("XBMC.Notification("+ __language__(30051) +","+ xbmc_notification +","+ str(15*1000) +","+ xbmc_img +")")
+
+    #####################################################################################################
+    def onAction(self,action):
+        if (self.DEBUG):
+            log("> onAction(" + str(action.getButtonCode()) + "," + str(action.getId()) + ")")
+        try:
+            if (action and (action.getButtonCode() in ACTION_EXIT_SCRIPT or action.getId() in ACTION_EXIT_SCRIPT)):
+                self.close()
+                del self.vm_player
+        except:
+            log(">> Seems XBMC PlayerControls were closed...")
+
+    def onClick(self,controlId):
+        if (self.DEBUG):
+            log("> onClick(" + str(controlId) + ")")
+        # Initiate outgoing call
+        if (controlId == 120):
+            number_to_call = self.getControl(120).getSelectedItem().getProperty("src")
+            if (number_to_call != ""):
+                dialog = xbmcgui.Dialog()
+                if (dialog.yesno(__addon__,__language__(30104) + " '" + number_to_call + "'?")):
+                    self.make_outgoing_call(number_to_call)
+                del dialog
+        # Play Voice Mail
+        elif (controlId == 121):
+            recindex = self.getControl(121).getSelectedItem().getProperty("recindex")
+            if (recindex != ""):
+                dialog = xbmcgui.Dialog()
+                if (dialog.yesno(__addon__,__language__(30105))):
+                    self.play_voice_mail(recindex)
+                del dialog
+        # Settings
+        elif (controlId == 112):
+            settings = xbmcaddon.Addon(__addon_id__)
+            settings.openSettings()
+            del settings
+            self.onInit()
+        # Refresh
+        elif (controlId == 109):
+            self.onInit()
+
+    def onFocus(self,controlId):
+        pass
+
+    #####################################################################################################
+    def make_outgoing_call(self,number_to_call):
+        log("> make_outgoing_call()")
+        settings = xbmcaddon.Addon(__addon_id__)
+        manager_host_port = settings.getSetting("asterisk_manager_host"),int(settings.getSetting("asterisk_manager_port"))
+        pbx = Manager(manager_host_port,settings.getSetting("asterisk_manager_user"),settings.getSetting("asterisk_manager_pass"))
+        pbx.Originate(settings.getSetting("asterisk_outbound_extension"),settings.getSetting("asterisk_outbound_context"),number_to_call,1)
+        del pbx
+        del settings
+        log(">> Done.")
+
+    #####################################################################################################
+    def play_voice_mail(self,recindex):
+        log("> play_voice_mail()")
+        settings = xbmcaddon.Addon(__addon_id__)
+        audio_format = ["wav","gsm","mp3"]
+        asterisk_vm_format = audio_format[int(settings.getSetting("asterisk_vm_format"))]
+        self.url_vm = settings.getSetting("asterisk_info_url") +"?recindex="+ recindex
+        self.url_vm = self.url_vm +"&mailbox="+ settings.getSetting("asterisk_vm_mailbox")
+        self.url_vm = self.url_vm +"&vmcontext="+ settings.getSetting("asterisk_vm_context")
+        self.url_vm = self.url_vm +"&format="+ asterisk_vm_format
+        del settings
+        self.vm_player = VoiceMailPlayer(xbmc.PLAYER_CORE_DVDPLAYER,function=self.voice_mail_ended)
+        self.xbmc_player = xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER)
+        listitem = xbmcgui.ListItem('VoiceMail')
+        listitem.setInfo(type='music',infoLabels={'title':recindex,'genre':audio_format,'artist':'Voice Mail','album':'XBMC PBX Addon'})
+        self.xbmc_player.play(self.url_vm, listitem)
+        del listitem
+        del self.xbmc_player
+        xbmc.executebuiltin("XBMC.ActivateWindow(playercontrols)")
+
+    #####################################################################################################
+    def voice_mail_ended(self):
+        log("> voice_mail_ended()")
+        del self.vm_player
+        if (self.url_vm != ""):
+            dialog = xbmcgui.Dialog()
+            if (dialog.yesno(__addon__,__language__(30106))):
+                self.delete_voice_mail()
+                self.onInit()
+            del dialog
+            self.url_vm = ""
+
+    #####################################################################################################
+    def delete_voice_mail(self):
+        log("> delete_voice_mail()")
+        if (self.url_vm != ""):
+            self.url_vm = self.url_vm +"&delete"
+            if (self.DEBUG):
+                log(">> " + self.url_vm)
+            f = urllib.urlopen(self.url_vm)
+            f.close()
+            del f
+
+
+#############################################################################################################
+class VoiceMailPlayer(xbmc.Player):
+    def __init__(self,*args,**kwargs):
+        xbmc.Player.__init__(self)
+        self.function = kwargs["function"]
+
+    #####################################################################################################
+    def onPlayBackStopped(self):
+        self.function()
+
+    #####################################################################################################
+    def onPlayBackEnded(self):
+        self.function()
+
+
+#############################################################################################################
+class FirstTimeGUI(xbmcgui.Window):
+
+    def __init__(self):
+        log("__init__()")
+        settings = xbmcaddon.Addon(__addon_id__)
+        DEBUG = settings.getSetting("xbmc_debug")
+        del settings
+        if (DEBUG == "true"):
+            self.DEBUG = True
+        else:
+            self.DEBUG = False
+        dialog = xbmcgui.ControlTextBox(1,1,600,600,"font12","0xFFFFFFFF")
+        msg = ""
+        for i in range(1,10):
+            msg = msg + __language__(30170 + i) + "\n"
+        self.addControl(dialog)
+        dialog.setText(msg)
+        log(">> Done.")
+
+    #####################################################################################################
+    def onAction(self,action):
+        if (self.DEBUG):
+            log("> onAction()")
+        settings = xbmcaddon.Addon(__addon_id__)
+        settings.openSettings()
+        del settings
+        self.close()
+
+
+
+#################################################################################################################
+ # Starts here
+#################################################################################################################
+
+try:
+    log("PATH: "+ CWD)
+    log("Launching GUI...")
+    settings = xbmcaddon.Addon(__addon_id__)
+    first_time_use = settings.getSetting("first_time_use")
+    settings.setSetting("first_time_use","false")
+    DEBUG = settings.getSetting("xbmc_debug")
+    del settings
+    if (first_time_use == "true"):
+        ui = FirstTimeGUI()
+    else:
+        ui = MainGUI("main_gui.xml",CWD,"Default")
+    ui.doModal()
+except:
+    xbmc_notification = str(sys.exc_info()[1])
+    xbmc_img = xbmc.translatePath(os.path.join(RESOURCE_PATH,'media','xbmc-pbx-addon.png'))
+    log(">> Notification: " + xbmc_notification)
+    xbmc.executebuiltin("XBMC.Notification("+ __language__(30051) +","+ xbmc_notification +","+ str(15*1000) +","+ xbmc_img +")")
+try:
+    del ui
+    log("EXIT!")
+    sys.modules.clear()
+except:
+    pass
