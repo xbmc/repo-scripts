1c7e2c4c44fc47bec53b340684b7ccb70109bdd0
Arne Morten Kvarving <akva@math.sintef.no>
2011-09-19 10:14:16 +0200

    [script.xbmc-pbx-addon] - v 0.0.9

--- script.xbmc-pbx-addon/backend_files/xbmc-pbx-addon.php
+++ script.xbmc-pbx-addon/backend_files/xbmc-pbx-addon.php
@@ -12,7 +12,7 @@ $__addon__              = "XBMC PBX Addon";
 $__addon_id__           = "script.xbmc-pbx-addon";
 $__author__             = "hmronline";
 $__url__                = "http://code.google.com/p/xbmc-pbx-addon/";
-$__version__            = "0.0.8";
+$__version__            = "0.0.9";
 
 
 // ************************************************************************************************************
