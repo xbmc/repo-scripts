47982b545d993ef9f298ca7a67085287b7251217
amet <amet.nospam@gmail.com>
2012-01-10 23:51:39 +0400

    [service.libraryautoupdate] -v 0.3.9
    - running video and music scans side by side never really worked. Now checks if scan is running and waits until complete before running the next scan.

--- service.libraryautoupdate/default.py
+++ service.libraryautoupdate/default.py
@@ -11,6 +11,7 @@ class AutoUpdater:
         
         while(not xbmc.abortRequested):
             now = time.time()
+            sleep_time = 10
             
             timer_amounts = {}
             timer_amounts['0'] = 1
@@ -20,23 +21,44 @@ class AutoUpdater:
             timer_amounts['4'] = 15
             timer_amounts['5'] = 24
 
-            #only do this if we are not playing anything
-            if(xbmc.Player().isPlaying() == False and now > self.last_run + (timer_amounts[Addon.getSetting('timer_amount')] * 60 * 60)):
-                #run the update since we have just started the program
-                if(Addon.getSetting('update_video') == 'true'):
-                    xbmc.log('Updating Video')
-                    xbmc.executebuiltin('UpdateLibrary(video)')
-                if(Addon.getSetting('update_music') == 'true'):
-                    xbmc.log('Updating Music')
-                    xbmc.executebuiltin('UpdateLibrary(music)')
-                    
-                #reset the last run timer    
-                self.last_run = now
-                xbmc.log("Update Library will run again in " + str(timer_amounts[Addon.getSetting("timer_amount")]) + " hours")
-                
-            #put the thread to sleep for x number of seconds
-            time.sleep(60)
+            #check if we should run an update
+            if(now > self.last_run + (timer_amounts[Addon.getSetting('timer_amount')] * 60 * 60)):
 
+                #make sure player isn't running
+                if(xbmc.Player().isPlaying() == False):
+                    
+                    if(self.scanRunning() == False):
+                        
+                        #run the update
+                        if(Addon.getSetting('update_video') == 'true'):
+                            xbmc.log('Updating Video')
+                            xbmc.executebuiltin('UpdateLibrary(video)')
+                            time.sleep(1)
+                            
+                        if(Addon.getSetting('update_music') == 'true'):
+                            #check if scan is running again, wait until finished if it is
+                            while(self.scanRunning()):
+                                time.sleep(10)
+                            
+                            xbmc.log('Update Music')
+                            xbmc.executebuiltin('UpdateLibrary(music)')
+                        
+                        #reset the last run timer    
+                        self.last_run = now
+                        xbmc.log("Update Library will run again in " + str(timer_amounts[Addon.getSetting("timer_amount")]) + " hours")
+                else:
+                    xbmc.log("Player is running, waiting until finished")
+                    
+            #put the thread to sleep for x number of seconds
+            time.sleep(sleep_time)
+            
+    def scanRunning(self):
+        #check if any type of scan is currently running
+        if(xbmc.getCondVisibility('Library.IsScanningVideo') or xbmc.getCondVisibility('Library.IsScanningMusic')):
+            return True
+        else:
+            return False
+           
 #run the program
 xbmc.log("Update Library Service starting...")
 AutoUpdater().runProgram()
