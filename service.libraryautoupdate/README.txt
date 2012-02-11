227ac87ce048654f859d81bc4e07ef24a29590ac
amet <amet.nospam@gmail.com>
2012-02-11 20:58:51 +0400

    [service.libraryautoupdate] -v 0.5.0
    -major changes to settings, split them by General and Timer category
    -Advanced timer functions now add the ability to do cron-like scheduling of the update process
    -option to skip during media playback or run the update anyway

--- service.libraryautoupdate/README.txt
+++ service.libraryautoupdate/README.txt
@@ -1,4 +1,20 @@
 The XBMC Library Updater will update your music and/or video libraries according to times specified by you. 
 
-Things to keep in mind:
-	Restarting XBMC will no longer automatically start a library update. The timer now keeps track of the last time it updated via a written file so restarts that happen in between updates may not necessarily trigger an update unless it is needed. 
+A note on Timers:
+
+Standard Timer - specify an interval to run the library update process. It will be launched every X hours within the interval unless on of the conditions specified by you as been met (don't run during media playback, etc) in which case it will be run at the next earliest convenience. 
+
+Advanced Timer - specify a cron expression to use as an interval for the update process. By default the expression will run at the top of every hour. More advanced expressions can be configured such as: 
+
+    .--------------- minute (0 - 59)
+    |   .------------ hour (0 - 23)
+    |   |   .--------- day of month (1 - 31)
+    |   |   |   .------ month (1 - 12) or Jan, Feb ... Dec
+    |   |   |   |  .---- day of week (0 - 6) or Sun(0 or 7), Mon(1) ... Sat(6)
+    V   V   V   V  V
+    *   *   *   *  *
+
+0 */5 * * 1-5 - every five hours Monday - Friday
+0,15,30,45 0,15-18 * * * - every quarter hour during the midnight hour and 3pm - 6pm
+
+Read up on cron for more information on how to create these expressions
--- service.libraryautoupdate/service.py
+++ service.libraryautoupdate/service.py
@@ -1,67 +1,100 @@
 import time
 import xbmc
 import xbmcaddon
-                
+from cronex import CronExpression
+
 class AutoUpdater:
     addon_id = "service.libraryautoupdate"
     Addon = xbmcaddon.Addon(addon_id)
-    datadir = "special://userdata/addon_data/" + addon_id + "/"
+    datadir = Addon.getAddonInfo('profile')
+    sleep_time = 10
+    forceUpdate = False
     
+    #setup the timer amounts
+    timer_amounts = {}
+    timer_amounts['0'] = 1
+    timer_amounts['1'] = 2
+    timer_amounts['2'] = 5
+    timer_amounts['3'] = 10
+    timer_amounts['4'] = 15
+    timer_amounts['5'] = 24
+        
     def runProgram(self):
-        #setup the timer amounts
-        timer_amounts = {}
-        timer_amounts['0'] = 1
-        timer_amounts['1'] = 2
-        timer_amounts['2'] = 5
-        timer_amounts['3'] = 10
-        timer_amounts['4'] = 15
-        timer_amounts['5'] = 24
-           
-        #check if we should delay the first run
-        if(int(self.Addon.getSetting("startup_delay")) != 0):
-            self.readLastRun()
-            
-            #check if we would have run an update anyway
-            if(time.time() >= self.last_run + (timer_amounts[self.Addon.getSetting('timer_amount')] * 60 * 60)):
-                #trick system by subtracting the timer amount then adding a delay (now - timer + delay = nextrun)
-                self.last_run = time.time() - (timer_amounts[self.Addon.getSetting('timer_amount')] * 60 *60) + (int(self.Addon.getSetting("startup_delay")) * 60)
-                self.writeLastRun()
-                xbmc.log("Setting delay at " + self.Addon.getSetting("startup_delay") + " minute")
-            
+
+        if(self.Addon.getSetting('use_advanced_timer') == 'false'):        
+            #check if we should delay the first run
+            if(int(self.Addon.getSetting("startup_delay")) != 0):
+                self.readLastRun()
+                
+                #check if we would have run an update anyway
+                if(time.time() >= self.last_run + (timer_amounts[self.Addon.getSetting('timer_amount')] * 60 * 60)):
+                    #trick system by subtracting the timer amount then adding a delay (now - timer + delay = nextrun)
+                    self.last_run = time.time() - (self.timer_amounts[self.Addon.getSetting('timer_amount')] * 60 *60) + (int(self.Addon.getSetting("startup_delay")) * 60)
+                    self.writeLastRun()
+                    self.log("Setting delay at " + self.Addon.getSetting("startup_delay") + " minute")
+
+        #run until XBMC quits
         while(not xbmc.abortRequested):
-            now = time.time()
-            sleep_time = 10
-            self.readLastRun()
 
-            #check if we should run an update
-            if(now >= self.last_run + (timer_amounts[self.Addon.getSetting('timer_amount')] * 60 * 60)):
-
-                #make sure player isn't running
-                if(xbmc.Player().isPlaying() == False):
-                    
-                    if(self.scanRunning() == False):
-
-                        self.runUpdates()
-
-                        xbmc.log("Update Library will run again in " + str(timer_amounts[self.Addon.getSetting("timer_amount")]) + " hours")
-                        
-                else:
-                    xbmc.log("Player is running, waiting until finished")
-                    
-            #put the thread to sleep for x number of seconds
-            time.sleep(sleep_time)
+            if(self.Addon.getSetting('use_advanced_timer') == 'true'):
+                self.runAdvanced()
+            else:
+                self.runStandard()
             
+            #put the thread to sleep for x number of seconds
+            time.sleep(self.sleep_time)
+            
+
+    def runStandard(self):        
+        now = time.time()
+
+        self.readLastRun()
+            
+        #check if we should run an update
+        if(now >= self.last_run + (self.timer_amounts[self.Addon.getSetting('timer_amount')] * 60 * 60)):
+            #make sure player isn't running
+            if(xbmc.Player().isPlaying() == False or self.Addon.getSetting('run_during_playback') == 'true'):
+                if(self.scanRunning() == False):
+                    self.runUpdates()
+                    self.log("will run again in " + str(self.timer_amounts[self.Addon.getSetting("timer_amount")]) + " hours")
+                        
+            else:
+                self.log("Player is running, waiting until finished")
+                    
+            
+
+    def runAdvanced(self):        
+        self.readLastRun()
+
+        #create the cron expression
+        cron = CronExpression(self.Addon.getSetting("cron_expression") + " XBMC_COMMAND")
+
+        #check if we should run, and that we haven't already run the update within the past minute - alternatively check that we shouldn't force an update
+        structTime = time.localtime()
+        if((cron.check_trigger((structTime[0],structTime[1],structTime[2],structTime[3],structTime[4])) and time.time() > self.last_run + 60) or self.forceUpdate):
+            #make sure player isn't running
+            if(xbmc.Player().isPlaying() == False or self.Addon.getSetting('run_during_playback') == 'true'):
+                self.forceUpdate = False
+                if(self.scanRunning() == False):
+                    self.runUpdates()
+                    self.log("will run again according to: " + self.Addon.getSetting("cron_expression"))
+                            
+            else:
+                #force an update if this 
+                self.forceUpdate = True
+                self.log("Player is running, waiting until finished")
+    
     def scanRunning(self):
         #check if any type of scan is currently running
         if(xbmc.getCondVisibility('Library.IsScanningVideo') or xbmc.getCondVisibility('Library.IsScanningMusic')):
-            return True
+            return True            
         else:
             return False
         
     def runUpdates(self):
         #run the update
         if(self.Addon.getSetting('update_video') == 'true'):
-            xbmc.log('Updating Video')
+            self.log('Updating Video')
             xbmc.executebuiltin('UpdateLibrary(video)')
             time.sleep(1)
                             
@@ -70,7 +103,7 @@ class AutoUpdater:
             while(self.scanRunning()):
                 time.sleep(10)
                             
-            xbmc.log('Update Music')
+            self.log('Update Music')
             xbmc.executebuiltin('UpdateLibrary(music)')
 
         #reset the last run timer    
@@ -94,4 +127,7 @@ class AutoUpdater:
         #write out the value for the last time the program ran
         f.write(str(self.last_run));
         f.close();
+
+    def log(self,message):
+        xbmc.log('service.libraryautoupdate: ' + message)
 
