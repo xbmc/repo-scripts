92aed0ac82d17ff58323fd00d31f75b8a7463ae2
amet <amet.nospam@gmail.com>
2012-02-18 20:56:18 +0400

    [script.njoy] -v0.1.1
    script to control N7 Njoy tuner from XBMC

---
+++ script.njoy/resources/lib/api.py
@@ -0,0 +1,177 @@
+from default import *
+
+import socket
+import workerpool
+import urllib
+import re
+import traceback
+try: import json
+except: import simplejson as json
+from xml.dom import minidom
+import xbmcgui
+
+socket.setdefaulttimeout(3)
+
+## A lot of the code borrowed from http://tv.i-njoy.eu/repo/ , all credit to them
+
+#N7 API GLOBALS
+PORT     = 80
+DISCOVER = "n7mac.html"
+
+def _(addr, command):
+    """Call WebApi N7 and returns python dict"""
+    return request(addr, "utelisys/%s" % (command, ))
+
+def request(addr, command=DISCOVER, ping=False):
+    """Raw request to N7 webserver"""
+    try:
+        r = urllib.urlopen("http://%s/%s" % (addr, command, ))
+        data = r.read()
+        r.close()
+    except:
+        return False
+
+    if ping:
+        if data.find("N7") < 0:
+            return False
+        return True
+    
+    return translate(data)
+
+def translate(data):
+    """Translate html data from webapi to python dict"""
+    try:
+        #Get text between body
+        body = re.compile('body>(.*?)<\/body', re.DOTALL + re.MULTILINE).search(str(data)).group(1)
+        #Prapare for splitting based on br tag
+        body = body.replace('<br>', '||').replace('&nbsp;', '').replace('\x05', '')
+        #remove all html tags
+        body = re.sub('<.*?>', ' ', body)
+        #split data into lines
+        lines = body.split("||")
+    except:
+        return False
+
+    result = []
+    for i, line in enumerate(lines):
+        r ={}
+        #if data has attribute make it available
+        if line.find(':') != -1:
+            v = line.split(':', 1)
+            r[v[0].strip()] = v[1].strip()
+            r[i] = v[1].strip()
+        #else use line number
+        else:
+            r[i] = line.strip()
+        result.append(r)
+    return result
+
+def channel_list(addr):
+#    <item>
+#        <guid>http://192.168.1.23:8080/chlist/0001</guid>
+#        <boxee:media-type type="movie" expression="full" name="feature"/>
+#        <title>1. CSB TV</title>
+#        <media:content type="video/mpg2" url="http://192.168.1.23:8080/chlist/0001"/>
+#        <media:thumbnail url="http://www.anyseedirect.eu/images/csb_tv.png"/>
+#        <description></description>
+#    </item>
+    channel_list = []
+    r = urllib.urlopen('http://%s/n7channel.rss' % (addr, ))
+    result = r.read()
+    r.close()
+    xmldoc = minidom.parseString(result)
+    items = xmldoc.getElementsByTagName("item")
+    for item in items:
+        channel_list.append(
+        {'guid'       :item.getElementsByTagName("guid")[0].firstChild.nodeValue,
+         'title'      :item.getElementsByTagName("title")[0].firstChild.nodeValue,
+         'description':item.getElementsByTagName("description")[0].firstChild.nodeValue,
+         'url'        :item.getElementsByTagName("media:content")[0].getAttribute('url'),
+         'thumb'      :item.getElementsByTagName("media:thumbnail")[0].getAttribute('url')})  
+    return channel_list
+
+def get_tuner_epg(addr, id):
+    count = 4 - len(str(id))
+    active = request(addr, 'utelisys/set_chview/' + '0'*count + str(id))
+
+    if active:
+        epg = request(addr, 'utelisys/get_currentch_epg')
+        if epg and epg > 14:
+            pass
+
+def get_internet_epg(id):
+    try:
+        url = "http://web2py.formatics.nl/epg_grabber/xml/getNowNext.json/%s" % (id, )
+        r = urllib.urlopen(url)
+        data = r.read()
+        r.close()
+        return json.loads(data)
+    except:
+#        if DEBUG:
+#            traceback.print_exc()
+        return {"current": "", "next": ""}
+
+class scan:
+    """Detects (multiple) N7 in network"""
+    def __init__(self):
+        self.n7         = False
+        self.ip         = self.get_local_ip()
+
+    def get_local_ip(self):
+        """Find local ip addresses at the host"""
+        #adresses = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")]
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("google.com",80))
+        adresses = [(s.getsockname()[0])]
+        s.close()
+        
+        if adresses:
+            print "Found IP: %s" % (", ".join(adresses), )
+            return adresses
+        else:
+            return False
+
+    def get_lan(self, addr):
+        """Get the LAN from an IP"""
+        network = addr.split(".")
+        del network[3]
+        return ".".join(network)
+
+
+    def is_n7(self, addr):
+        """Find N7 tuner BASED ON OPEN PORT AND N7 MAKE FILE"""
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.10)    ## set a timeout of 0.10 sec
+        try:
+            s.connect((addr,PORT))
+            s.close()
+        except:
+            return
+
+        response = request(addr, DISCOVER, True)
+        if response:
+            return addr
+
+    def run(self):
+        """FIND N7 IN THREADED POOL"""
+        lan       = [self.get_lan(ip) for ip in self.ip]
+        scan_list = [".".join([l, str(i)]) for l in lan for i in xrange(1,256)]
+        
+        print "Ready for scanning LAN: %s" % (", ".join(lan), )
+
+        pool   = workerpool.WorkerPool(size=25)
+        result = pool.map(self.is_n7, scan_list)
+        pool.shutdown()
+        pool.wait()
+
+        print "Scan Finished"
+
+        result = filter(None, result)
+        if result:
+            self.n7 = result
+            print " - Found N7 at: " + ", ".join(result)
+            return True
+        else:
+            print " - No N7 found"
+            return False
+
---
+++ script.njoy/resources/lib/gui.py
@@ -0,0 +1,209 @@
+import os
+import sys
+import xbmc
+import xbmcaddon
+import xbmcgui
+import api
+from threading import Thread
+import traceback
+
+## A lot of the code borrowed from http://tv.i-njoy.eu/repo/ , all credit to them
+
+CANCEL_DIALOG  = ( 9, 10, 92, 216, 247, 257, 275, 61467, 61448, )
+
+_              = sys.modules[ "__main__" ].__language__
+__scriptname__ = sys.modules[ "__main__" ].__scriptname__
+__addon__      = sys.modules[ "__main__" ].__addon__
+__cwd__        = sys.modules[ "__main__" ].__cwd__
+__profile__    = sys.modules[ "__main__" ].__profile__ 
+
+def log(msg):
+  xbmc.log("### [%s] - %s" % (__scriptname__,msg),level=xbmc.LOGDEBUG )
+
+class GUI( xbmcgui.WindowXMLDialog ):
+    def __init__( self, *args, **kwargs ):
+        """Init main Application"""
+        self.tuner         = False
+        self.WINDOW        = xbmcgui.Window(10000)
+        self.channel       = -1
+        self.items         = []
+        self.current_label = ""
+        
+    def onInit( self ):
+        if self.setup():
+            self.fetch_channels()
+            self.epg = _EPG(window = self.getControl)
+            self.epg.start()
+            self.setFocusId(120)
+            self.getControl(120).selectItem(0)
+        else:
+            self.close()    
+
+    def setup(self, force=False):
+        n7      = __addon__.getSetting("N7")
+        warning = __addon__.getSetting("warning")
+
+        if warning != 'done':
+            dialog = xbmcgui.Dialog()
+            info = dialog.yesno("N7 TV APP", _(30100), _(30101), _(30102), _(30103), _(30104))
+            if info:
+                __addon__.setSetting("warning", "done")
+            else:
+                return False
+
+        if n7 and not force:
+            self.getControl( 205 ).setLabel( n7 )
+            if api.request(n7, api.DISCOVER, True):
+                self.tuner = n7
+                log("N7 tuner ip loaded from cache: %s" % n7)
+                return True
+            else:
+                dialog = xbmcgui.Dialog()
+                dialog.ok(_(30114), _(30105) % (n7, ))
+
+        #Automatic scan
+        dialog = xbmcgui.Dialog()
+        if dialog.yesno(_(30107), _(30108),_(30109), "", _(30110), _(30111)):
+            scanner = api.scan()
+            if scanner.run():
+                dialog = xbmcgui.Dialog()
+                if len(scanner.n7) > 1:
+                    pos = dialog.select(_(30106), scanner.n7)
+                    n7  = scanner.n7[pos]         
+                else:
+                    n7 = scanner.n7[0]
+                    txt = _(30112) % n7
+                    xbmc.executebuiltin("Notification(%s,%s,2)" % (__scriptname__ , txt))
+                __addon__.setSetting("N7", n7)
+                self.tuner = n7
+                log("N7 tuner selected: %s" % n7)
+                self.getControl( 205 ).setLabel( n7 )
+                del scanner
+                return True
+            else:
+                xbmc.executebuiltin("Notification(%s,%s,2)" % (__scriptname__ ,_(30117)))
+
+        #Manual IP
+        n7 = dialog.numeric(3, _(30111))
+        if api.request(n7, api.DISCOVER, True):
+            __addon__.setSetting("N7", n7)
+            txt = _(30112) % n7
+            self.tuner = n7
+            self.getControl( 205 ).setLabel( n7 )
+            log("N7 tuner manually entered: %s" % n7)
+            return True
+        else:
+            dialog.ok(_(30114), _(30105) % n7, _(30115))
+            __addon__.setSetting("N7", "")
+            xbmc.sleep(3000)
+            self.close()
+
+    def tune(self,pos):
+        self.getControl(150).setVisible(True)
+        focus   = self.getControl(120).getListItem(pos)
+        url     = focus.getProperty("url")
+        self.current_label = focus.getLabel()
+        self.getControl( 206 ).setLabel( self.current_label )
+        if xbmc.Player().isPlaying():
+            xbmc.Player().stop()
+        xbmc.sleep(3000)        
+        xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(url, 0, 1)
+        self.getControl(150).setVisible(False)
+        self.channel = pos
+        return self.current_label 
+
+    def fetch_channels(self):
+        """get channel list"""
+        self.getControl(150).setVisible(True)
+        self.getControl( 120 ).reset()
+        if self.tuner:
+            items = api.channel_list(self.tuner)
+            self.items = items
+            if items:
+                for item in items:
+                    listitem = xbmcgui.ListItem( label=item['title'], thumbnailImage=item['thumb'] )
+                    listitem.setProperty( "thumb", item['thumb'] )
+                    listitem.setProperty( "url", item['url'] )
+                    self.getControl( 120 ).addItem( listitem )
+            else:
+                dialog = xbmcgui.Dialog()
+                dialog.ok(_(30114), _(30116))
+        self.getControl(150).setVisible(False)
+        self.setFocusId(120)
+        self.getControl(120).selectItem(0)
+
+    def onClick( self, controlId ):
+        if controlId == 120:
+            pos = self.getControl(120).getSelectedPosition()
+            if pos != self.channel:
+                self.tune(pos)
+            else:
+                import player
+                ui = player.PLAYER( "script-njoy-player.xml" , __cwd__ , "Default",
+                                    ch_list = self.items, current_label = self.current_label,
+                                    current_channel = self.channel, tune = self.tune)
+                ui.doModal()
+                self.getControl(120).selectItem(ui.current_pos)
+                del ui                    
+        elif controlId == 110:
+            self.fetch_channels()
+            
+        elif controlId == 115:
+            self.getControl(150).setVisible(True)
+            self.setup(True)
+            self.fetch_channels() 
+            
+    def onFocus( self, controlId ):
+        pass
+
+    def onAction( self, action ):
+        if ( action.getId() in CANCEL_DIALOG):
+            self.epg.stop()
+            xbmc.Player().stop()
+            self.close()
+
+class _EPG(Thread):
+    """init epg class"""
+    def __init__ (self, *args, **kwargs):
+        Thread.__init__(self)
+        self.active = True
+        self.window = kwargs['window']
+
+    def run(self):
+        """main epg poller, checks every 2 seconds if focus has changed."""
+        last = ''
+        count = 0
+        while self.active:
+            try:
+                focus   = self.window(120).getListItem(self.window(120).getSelectedPosition())
+                thumb   = focus.getProperty("thumb")
+                current = thumb.rsplit('/', 1)[1].replace('.png', '').replace('.jpg', '').replace('_', '%20')
+                if last != current:
+                    self.reset_epg()
+                    count = 0
+                else:
+                    if count == 4:
+                        data = api.get_internet_epg( current )
+                        self.push_epg(data)
+                    count += 1
+                last = current
+                xbmc.sleep(500)
+            except:
+                pass
+
+    
+    def push_epg(self, data):
+        """set epg data to gui"""
+        self.window(200).setLabel( data['current'].encode('utf-8') )
+        self.window(201).setLabel( data['next'].encode('utf-8') )
+
+    def reset_epg(self):
+        """reset epg data in gui"""
+        self.window(200).setLabel('')
+        self.window(201).setLabel('')
+
+    def stop(self):
+        """stop epg polling thread"""
+        self.active = False
+
+
