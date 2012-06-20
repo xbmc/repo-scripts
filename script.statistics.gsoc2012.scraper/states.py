dad48645cbe298346f1a4a86054764066cc6405c
Martijn Kaijser <martijn@xbmc.org>
2012-06-20 10:51:18 +0200

    [script.statistics.gsoc2012.scraper] -v0.0.4

--- script.statistics.gsoc2012.scraper/states.py
+++ script.statistics.gsoc2012.scraper/states.py
@@ -5,7 +5,7 @@ import server
 import string
 
 
-__settings__ = xbmcaddon.Addon(id='script.statistics2012.gsoc.scraper')
+__settings__ = xbmcaddon.Addon(id='script.statistics.gsoc2012.scraper')
 
 
 def chunks(l, n):
