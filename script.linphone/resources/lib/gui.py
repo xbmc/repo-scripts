293f22e2de98dc851367b1d54318d06b5b3217c2
Martijn Kaijser <mcm.kaijser@gmail.com>
2012-11-18 11:11:33 +0100

    [script.linphone] 0.0.8

--- script.linphone/resources/lib/gui.py
+++ script.linphone/resources/lib/gui.py
@@ -7,7 +7,7 @@
 # *
 # * zip script.linphone.zip script.linphone/* -r
 
-import os, sys
+import os, sys, ast
 import xbmc
 import xbmcgui
 import time
@@ -30,6 +30,9 @@ _  = sys.modules[ "__main__" ].__language__
 RIGHT_SHIFT = 340
 DOWN_SHIFT = 100
 
+SIZE_KEYCODE = 58
+SELFVIEW_KEYCODE = 59
+
 WIDTH = int(xbmcgui.Window().getWidth())
 HEIGHT = int(xbmcgui.Window().getHeight())
 RIGHT_SHIFT = int(WIDTH * 350 / 1280)
@@ -37,6 +40,30 @@ RIGHT_SHIFT = int(WIDTH * 350 / 1280)
 PERCENT1 = ((HEIGHT - RIGHT_SHIFT) * 100) / HEIGHT
 PERCENT2 = ((WIDTH - DOWN_SHIFT) * 100) / WIDTH
 PERCENT = int((PERCENT1 + PERCENT2) / 2)
+
+VIEW_SIZE = 4
+SELF_VIEW_POS = 1
+
+proport = float(WIDTH) / HEIGHT
+
+if proport < 1.6:
+    min_width = 800
+    min_height = int(800 / proport)
+else:
+    min_width = int(500 * proport)
+    min_height = 500
+
+step = int((WIDTH - RIGHT_SHIFT - min_width) / 2)
+
+VIEW_SIZES = [[], [], [], [], []]
+VIEW_SIZES[0] = [min_width, min_height]
+#VIEW_SIZES[1] = [WIDTH - RIGHT_SHIFT - step * 2, int((WIDTH - RIGHT_SHIFT - step * 2)/ proport)]
+VIEW_SIZES[1] = [WIDTH - RIGHT_SHIFT - step    , int((WIDTH - RIGHT_SHIFT - step    )/ proport)]
+VIEW_SIZES[2] = [WIDTH - RIGHT_SHIFT, int((WIDTH - RIGHT_SHIFT) / proport)]
+VIEW_SIZES[3] = [WIDTH - RIGHT_SHIFT / 2, int((WIDTH - RIGHT_SHIFT / 2) / proport)]
+VIEW_SIZES[4] = [WIDTH, HEIGHT]
+
+SELF_VIEWS = [-1, 0, 3, 1, 2]
 
 wm = pyinotify.WatchManager()
 mask = pyinotify.IN_MODIFY  # watched events
@@ -68,14 +95,14 @@ class GUI( xbmcgui.WindowXML ):
         wm.add_watch('/home/' + SYSTEM_USER + '/linphone.log', mask)
 
         LP_WIN = self
-        self.slider.setPercent(PERCENT)
-        xlog(PERCENT)
+        #self.slider.setPercent(PERCENT)
         self.btn_settings.setLabel(__language__(30501))
         #self.btn_recieved.setLabel(__language__(30503))1
-        if (list_type == 'contacts'):
-            self.list_contacts.addItems(pb.keys()[-10:])
+        if list_type == 'contacts':
+            for row in pb:
+                self.list_contacts.addItem(row[0])
         else:
-            self.list_contacts.addItems(last_calls[-10:])
+            self.list_contacts.addItems(last_calls)
 
         LP_WIN.btn_hangup.setVisible(False)
         LP_WIN.btn_answer.setVisible(False)
@@ -96,15 +123,19 @@ class GUI( xbmcgui.WindowXML ):
         self.control_btn_recent_id = 5003
         #self.control_btn_recieved_id = 5007 #2px
         self.control_list_contacts_id = 5008
-        self.control_slider_id = 5009
+        #self.control_slider_id = 5009
+        self.control_btn_scaler_id = 5010
         self.control_btn_add_contact_id = 5011
+        self.control_btn_self_view_id = 5012
         self.control_label_status_id = 5051
         self.control_btn_context_id = 5019
 
         #controls
         self.btn_hangup = self.getControl(self.control_btn_hangup_id)
         self.btn_contacts = self.getControl(self.control_btn_contacts_id)
-        self.slider = self.getControl(self.control_slider_id)
+        #self.slider = self.getControl(self.control_slider_id)
+        self.btn_scaler = self.getControl(self.control_btn_scaler_id)
+        self.btn_self_view = self.getControl(self.control_btn_self_view_id)
         self.btn_close = self.getControl(self.control_btn_close_id)
         self.btn_call_to = self.getControl(self.control_btn_call_to_id)
         self.btn_answer = self.getControl(self.control_btn_answer_id)
@@ -119,9 +150,7 @@ class GUI( xbmcgui.WindowXML ):
         list_top_pos = int(HEIGHT / 720) + 90
 
         self.addControl(xbmcgui.ControlImage(970, list_top_pos - 3, 306, 306, __cwd__ + '/resources/skins/Default/media/list_bg.png'))
-
-        self.list_contacts = xbmcgui.ControlList(973, list_top_pos, 300, 300, font = 'font12', selectedColor = "0xFF000000", textColor = '0xFFFFFFFF', buttonTexture = 'button-nofocus.png', buttonFocusTexture = 'button-focus.png', itemHeight = 25, space = 2)
-
+        self.list_contacts = xbmcgui.ControlList(973, list_top_pos, 300, 330, font = 'font12', selectedColor = "0xFF000000", textColor = '0xFFFFFFFF', buttonTexture = 'button-nofocus.png', buttonFocusTexture = 'button-focus.png', itemHeight = 25, space = 2)
         self.addControl(self.list_contacts)
         self.control_list_contacts_id = self.list_contacts.getId()
 
@@ -131,9 +160,11 @@ class GUI( xbmcgui.WindowXML ):
         self.list_contacts.controlRight(self.btn_context)
 
         if is_full_ver > 0:
-            self.slider.setVisible(True)
+            self.btn_scaler.setVisible(True)
+            self.btn_self_view.setVisible(True)
         else:
-            self.slider.setVisible(False)
+            self.btn_scaler.setVisible(False)
+            self.btn_self_view.setVisible(False)
         self.btn_recent.controlDown(self.list_contacts)
         self.btn_contacts.controlDown(self.list_contacts)
         self.btn_call_to.controlUp(self.list_contacts)
@@ -154,32 +185,41 @@ class GUI( xbmcgui.WindowXML ):
         global call_status
         global list_type
         global selected_list_item
+        global current_name
         xlog("(onClick)CONTROL ID =  "+str(controlId))
 
-        if controlId == self.control_slider_id:
-            prev_percent = PERCENT
-            PERCENT = int(self.slider.getPercent())
-            height = int((PERCENT * HEIGHT) / 100.0)
-            width = int((PERCENT * WIDTH) / 100.0)
-            if width < 800 or height < 500:
-                self.slider.setPercent(prev_percent)
-            else:
-                xlog("HEIGHT = %s, WIDTH = %s" % (height, width))
-                linphoneGeneric("pwindow size %s %s" % (width, height) )
-                linphoneGeneric("scale size %s %s" % (width, height) )
+        if controlId == self.control_btn_self_view_id:
+            self.changeSelfViewPos()
+
+        if controlId == self.control_btn_scaler_id:
+            self.changeViewSize()
+
+        #if controlId == self.control_slider_id:
+        #    prev_percent = PERCENT
+        #    PERCENT = int(self.slider.getPercent())
+        #    height = int((PERCENT * HEIGHT) / 100.0)
+        #    width = int((PERCENT * WIDTH) / 100.0)
+        #    if width < 800 or height < 500:
+        #        self.slider.setPercent(prev_percent)
+        #    else:
+        #        xlog("HEIGHT = %s, WIDTH = %s" % (height, width))
+        #        linphoneGeneric("pwindow size %s %s" % (width, height) )
+        #        linphoneGeneric("scale size %s %s" % (width, height) )
 
         if controlId == self.control_list_contacts_id:
             self.item = self.list_contacts.getSelectedItem()
             LP_WIN.label_status.setLabel("Status: Dialing...")
             if list_type == 'contacts':
-                linphoneCall(pb[self.item.getLabel()])
+                for row in pb:
+                    if row[0] == self.item.getLabel():
+                        linphoneCall(row[1])
             else:
                 linphoneCall(self.item.getLabel())
 
         if controlId == self.control_btn_recent_id:
             if list_type != 'recent':
                 self.list_contacts.reset()
-                self.list_contacts.addItems(last_calls[-10:])
+                self.list_contacts.addItems(last_calls)
                 list_type = 'recent'
                 self.btn_recent.setLabel('   Recent', 'font14', '0xFFFFFFFF')
                 self.btn_contacts.setLabel('  Contacts', 'font12', '0xFFAAAAAA')
@@ -187,7 +227,8 @@ class GUI( xbmcgui.WindowXML ):
         if controlId == self.control_btn_contacts_id:
             if list_type != 'contacts':
                 self.list_contacts.reset()
-                self.list_contacts.addItems(pb.keys())
+                for row in pb:
+                    self.list_contacts.addItem(row[0])
                 list_type = 'contacts'
                 self.btn_recent.setLabel('   Recent', 'font12', '0xFFAAAAAA')
                 self.btn_contacts.setLabel('  Contacts', 'font14', '0xFFFFFFFF')
@@ -224,9 +265,10 @@ class GUI( xbmcgui.WindowXML ):
             xlog("M-media Size = %s" % multimedia_size)
             #linphoneGeneric("vsize " + multimedia_size)
             initLinphone()
-
         if controlId == self.control_btn_add_contact_id:
             linphoneGeneric("pwindow hide")
+            current_name = ''
+            selected_list_item = 0
             contact_win = AddContact( "add_contact.xml" , __cwd__, "Default")
             contact_win.doModal()
             del contact_win
@@ -269,6 +311,28 @@ class GUI( xbmcgui.WindowXML ):
         linphoneGeneric("pwindow show")
         return ret
 
+    def changeSelfViewPos(self):
+        global SELF_VIEW_POS
+        global SELF_VIEWS
+        if SELF_VIEW_POS < 4:
+            SELF_VIEW_POS = SELF_VIEW_POS + 1;
+        else:
+            SELF_VIEW_POS = 0
+        linphoneGeneric("pwindow selfview %s" % (SELF_VIEWS[SELF_VIEW_POS]))
+
+    def changeViewSize(self):
+        global VIEW_SIZE
+        global VIEW_SIZES
+        if VIEW_SIZE < 4:
+            VIEW_SIZE = VIEW_SIZE + 1
+        else:
+            VIEW_SIZE = 0
+        width = VIEW_SIZES[VIEW_SIZE][0]
+        height = VIEW_SIZES[VIEW_SIZE][1]
+        xlog("SCALER %s, WIDTH = %s, HEIGHT = %s" % (VIEW_SIZE, width, height))
+        linphoneGeneric("pwindow size %s %s" % (width, height) )
+        linphoneGeneric("scale size %s %s" % (width, height) )
+
     ##--------- Focus -----------##
     def onFocus( self, controlId ):
         pass
@@ -278,6 +342,13 @@ class GUI( xbmcgui.WindowXML ):
         ##----------------ESC Button ---------------------##
         if action.getId() == 10:
             linphoneTerminate()
+
+        if action.getId() == SELFVIEW_KEYCODE and is_full_ver:
+            self.changeSelfViewPos()
+
+        if action.getId() == SIZE_KEYCODE and is_full_ver:
+            self.changeViewSize()
+
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
@@ -311,17 +382,25 @@ class ContactAction(xbmcgui.WindowXMLDialog):
         self.label_uri = self.getControl(self.control_label_uri_id)
         item = LP_WIN.list_contacts.getSelectedItem()
         self.label_name.setLabel(item.getLabel())
-        self.label_uri.setLabel(pb[item.getLabel()])
+        for row in pb:
+            if row[0] == item.getLabel():
+                self.label_uri.setLabel(row[1])
 
     def onClick(self, controlId):
         global pb
+        global current_name
+        global selected_list_item
         if controlId == self.control_btn_close_id:
+            selected_list_item = 0
+            current_name = ''
             self.close()
 
         if controlId == self.control_btn_call_id:
             item = LP_WIN.list_contacts.getSelectedItem()
             if list_type == 'contacts':
-                linphoneCall(pb[item.getLabel()])
+                for row in pb:
+                    if row[0] == item.getLabel():
+                        self.label_uri.setLabel(row[1])
                 self.close()
 
         if controlId == self.control_btn_edit_id:
@@ -332,16 +411,20 @@ class ContactAction(xbmcgui.WindowXMLDialog):
 
         if controlId == self.control_btn_delete_id:
             if list_type == 'contacts' and selected_list_item:
-                del pb[selected_list_item]
+                pbb = []
+                for row in pb:
+                    if row[0] != selected_list_item:
+                        pbb.append(row)
+                pb = pbb
                 set_phone_book(pb)
                 LP_WIN.list_contacts.reset()
-                LP_WIN.list_contacts.addItems(pb.keys())
+                for row in pb:
+                    LP_WIN.list_contacts.addItem(row[0])
             self.close()
 
     def onAction(self, action):
         if action.getId() == 10:
             self.close()
-
 
 class AddContact(xbmcgui.WindowXMLDialog):
     global current_name
@@ -368,7 +451,9 @@ class AddContact(xbmcgui.WindowXMLDialog):
             self.win_label.setLabel("Edit contact")
             self.btn_name.setLabel(selected_list_item)
             current_name = selected_list_item
-            self.btn_uri.setLabel(pb[selected_list_item])
+            for row in pb:
+                if row[0] == selected_list_item:
+                    self.btn_uri.setLabel(row[1])
 
     def onClick(self, controlId):
         global pb
@@ -394,12 +479,17 @@ class AddContact(xbmcgui.WindowXMLDialog):
             self.number = self.btn_uri.getLabel()
             if len(self.name) > 1  and len(self.number) > 3:
                 if selected_list_item and current_name != self.name:
-                    del pb[current_name]
-                pb[self.name] = self.number
+                    pbb = []
+                    for row in pb:
+                        if row[0] != current_name:
+                            pbb.append(row)
+                    pb = pbb
+                pb.append([self.name, self.number])
                 set_phone_book(pb)
                 if list_type == 'contacts':
                     LP_WIN.list_contacts.reset()
-                    LP_WIN.list_contacts.addItems(pb.keys())
+                    for row in pb:
+                        LP_WIN.list_contacts.addItem(row[0])
             selected_list_item = 0
             current_name = ''
             self.close()
@@ -437,6 +527,21 @@ def linphonecsh(cmd):
     xlog(cmd)
     return os.system("/usr/local/bin/linphonecsh " + cmd)
 
+def limitLinphonecCodecs():
+    lines = sub.Popen("linphonecsh generic 'vcodec list'", shell=True, stdout=sub.PIPE).stdout.readlines()
+    cindex = 0
+    for line in lines:
+        if 'H264' not in line and 'enabled' in line:
+            linphoneGeneric("vcodec disable %s" % (cindex))
+        cindex = cindex + 1
+
+    lines = sub.Popen("linphonecsh generic 'codec list'", shell=True, stdout=sub.PIPE).stdout.readlines()
+    cindex = 0
+    for line in lines:
+        if 'PCMU' not in line and 'PCMA' not in line and 'enabled' in line:
+            linphoneGeneric("codec disable %s" % (cindex))
+        cindex = cindex + 1
+
 def linphoneCall(target):
     global calling_to
     global call_status
@@ -456,6 +561,7 @@ def linphoneTerminate():
     global last_calls
     global last_try_exit
     global last_try_exit_cnt
+    global notifier
     xbmc.log("##### [%s] - Debug msg: %s" % (__scriptname__, call_status),level=xbmc.LOGDEBUG )
 
     tpass = (time.time() - last_try_exit)
@@ -490,7 +596,8 @@ def linphoneTerminate():
         time.sleep(1)
         # LP_WIN.exit_script()
         LP_WIN.close()
-        # os.system('killall -9 xbmc.bin')
+        #os.system('killall -9 xbmc.bin')
+
 
 def checkLogFile(self, the_event):
     global last_log_pos
@@ -515,7 +622,7 @@ def checkLogFile(self, the_event):
         LP_WIN.btn_answer.setVisible(False)
         LP_WIN.btn_hangup.setVisible(True)
         fixUpDown(False, True)
-    if 'LinphoneCallIncomingReceived' in logdata:
+    if 'LinphoneCallIncomingReceived' in logdata or '-CALL_NEW' in logdata:
         call_status = 'ringing'
         LP_WIN.btn_answer.setVisible(True)
         LP_WIN.btn_hangup.setVisible(True)
@@ -557,18 +664,70 @@ def xlog(msg):
     xbmc.log("##### [%s] - Debug msg: %s" % (__scriptname__,msg,),level=xbmc.LOGDEBUG )
 
 def fixUpDown(answer, hangup):
-    if answer:
-        ctrl_down = 5002
-        ctrl_up = 5002
-    elif hangup:
-        ctrl_down = 5004
-        ctrl_up = 5004
-    else:
-        ctrl_down = 5009
-        ctrl_up = 5011
+    # 5001 - Call to, 5002 - Dial, 5004 - Hangup, 5005 - Settings, 5010 - Size, 5011 - Add contact, 5012 - SelfView
 
-    LP_WIN.getControl(5011).controlDown(LP_WIN.getControl(ctrl_down))
-    LP_WIN.getControl(5009).controlUp(LP_WIN.getControl(ctrl_up))
+    #5001Down, 5011Down - answer:5002 / hangup:5004 / full:5010 / 5005
+    if answer:
+        b5001Down = 5002
+    elif hangup:
+        b5001Down = 5004
+    elif is_full_ver:
+        b5001Down = 5010
+    else:
+        b5001Down = 5005
+    LP_WIN.getControl(5001).controlDown(LP_WIN.getControl(b5001Down))
+    LP_WIN.getControl(5011).controlDown(LP_WIN.getControl(b5001Down))
+
+    #5002Right, 5002Left - hangup:5004 / 5002
+    #5004Right, 5004Left - answer:5002 / 5004
+    if answer and hangup:
+        LP_WIN.getControl(5002).controlRight(LP_WIN.getControl(5004)) # from Dial to Hangup
+        LP_WIN.getControl(5004).controlLeft(LP_WIN.getControl(5002))
+    elif answer:
+        LP_WIN.getControl(5002).controlRight(LP_WIN.getControl(5002))
+    elif hangup:
+        LP_WIN.getControl(5004).controlLeft(LP_WIN.getControl(5004))
+
+    #5002Down, 5004Down - full:5010 / 5005
+    b500xDown = 5010 if is_full_ver else 5005
+    if answer:
+        LP_WIN.getControl(5002).controlDown(LP_WIN.getControl(b500xDown))
+    if hangup:
+        LP_WIN.getControl(5004).controlDown(LP_WIN.getControl(b500xDown))
+
+    #5010Up - answer:5002 / hangup:5004 / 5001
+    #5012Up - answer:5002 / hangup:5004 / 5001
+    if answer:
+        b500xUp = 5002
+    elif hangup:
+        b500xUp = 5004
+    else:
+        b500xUp = 5001
+    LP_WIN.getControl(5010).controlUp(LP_WIN.getControl(b500xUp))
+    LP_WIN.getControl(5012).controlUp(LP_WIN.getControl(b500xUp))
+
+    #5005Up - full:5010 / answer:5002 / hangup:5004 / 5001
+    b5005Up = 5010 if is_full_ver else b500xUp
+    LP_WIN.getControl(5005).controlUp(LP_WIN.getControl(b5005Up))
+
+    #if answer:
+    #    ctrl_down = 5002
+    #    ctrl_up = 5002
+    #elif hangup:
+    #    ctrl_down = 5004
+    #    ctrl_up = 5004
+    #else:
+    #    ctrl_down = 5010
+    #    ctrl_up = 5011
+    #LP_WIN.getControl(5011).controlDown(LP_WIN.getControl(ctrl_down))
+    #LP_WIN.getControl(5010).controlUp(LP_WIN.getControl(ctrl_up))
+    #if answer and hangup:
+    #    LP_WIN.getControl(5002).controlRight(LP_WIN.getControl(5004)) # from Dial to Hangup
+    #    LP_WIN.getControl(5004).controlLeft(LP_WIN.getControl(5002))
+    #elif answer:
+    #    LP_WIN.getControl(5004).controlLeft(LP_WIN.getControl(5004))
+    #elif hangup:
+    #    LP_WIN.getControl(5002).controlLeft(LP_WIN.getControl(5002))
 
 def initLinphone():
     ##------------------Initialisation Settings----------------------##
@@ -605,7 +764,7 @@ def initLinphone():
 
     ##----------------STARTING LINPHONE------------------------##
 
-    if auto_answer:
+    if auto_answer == "true":
         auto_answer = '-a'
     else:
         auto_answer = ''
@@ -712,19 +871,21 @@ def get_phone_book():
     except IOError:
         os.system("touch " + __cwd__ + "/phone_book.pb")
         f = open(__cwd__ + "/phone_book.pb", "r+")
-        f.write("{}")
+        f.write("[]")
         f.seek(0)
 
     str_pb = f.read()
     str_pb = str_pb.replace("\n", "")
-    book = eval(str_pb)
+    str_pb = eval(str_pb)
     f.close()
-    return book
+    return str_pb
 
 def set_phone_book(pb):
     f = open(__cwd__ + "/phone_book.pb", "w")
-    str_pb = str(pb)
-    str_pb = str_pb.replace("', u'", "', \nu'")
+    str_pb = repr(pb)
+    str_pb = str_pb.replace("], [", "], \n[")
+    str_pb = str_pb.replace("[u'", "['")
+    str_pb = str_pb.replace("', u'", "', '")
     f.write(str_pb)
     f.close()
 
@@ -732,6 +893,7 @@ def sound_card_list_creating(soundcard_list):
     try:
         f = open(__cwd__ + '/soundcards', 'w')
     except:
+        limitLinphonecCodecs()
         os.system("touch %s" % __cwd__ + '/soundcards')
         f = open(__cwd__ + '/soundcards', 'w')
     for i in soundcard_list:
@@ -771,7 +933,7 @@ pb = get_phone_book()
 placed, recieved, last_calls = parsing('/home/' + SYSTEM_USER + '/.linphonerc')
 cmd = str()
 
-# ppa.launchpad.net
 # 91.189.95.83
 # register_only_when_network_is_up
 # reg_sendregister=0
+#dpkg -i .deb
