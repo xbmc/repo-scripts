0c7fe50dc1ac11438f27b452bdc34b421cfc292f
Martijn Kaijser <mcm.kaijser@gmail.com>
2012-12-20 21:33:49 +0100

    [script.module.gmusicapi] -2012.11.09

--- script.module.gmusicapi/lib/gmusicapi/protocol.py
+++ script.module.gmusicapi/lib/gmusicapi/protocol.py
@@ -266,6 +266,11 @@ class Metadata_Expectations(object):
         val_type = "boolean"
     class matchedId(_Metadata_Expectation):
         mutable = False
+    
+    #Seems to be a matching track in the store.
+    class storeId(_Metadata_Expectation):
+        mutable = False
+        optional = True
         
     
     #Dependent metadata:
@@ -310,6 +315,7 @@ class Metadata_Expectations(object):
         mutable = False
         volatile = True
         val_type = "integer"
+        optional = True #only appears if song has been played
 
     
 class WC_Protocol(object):
@@ -553,7 +559,6 @@ class WC_Protocol(object):
                 req = {}
                 res = {"type":"object",
                        "properties":{
-                           "magicPlaylists": WC_Protocol.pl_array,
                            "playlists": WC_Protocol.pl_array,
                            },
                        "additionalProperties":False
@@ -607,7 +612,7 @@ class WC_Protocol(object):
             res = {"type":"object",
                    "properties":{
                        "downloadCounts":{
-                           "type":"array",
+                           "type":"object",
                            "items":{
                                "type":"object",
                                "properties":{
@@ -748,7 +753,7 @@ class MM_Protocol(object):
 
         for filename in filenames:
 
-            if not filename.split(".")[-1] == "mp3":
+            if not filename.split(".")[-1].lower() == "mp3":
                 self.log.error("Cannot upload '%s' because it is not an mp3.")
 
             track = metadata.tracks.add()
@@ -787,7 +792,13 @@ class MM_Protocol(object):
             track.duration = int(audio.info.length * 1000)
 
             #GM requires at least a title.
-            track.title = audio["title"][0] if "title" in audio else filename.split(r'/')[-1]
+            if "title" in audio:
+                track.title = audio["title"][0] 
+            else:
+                #attempt to handle unicode filenames.
+                enc = utils.guess_str_encoding(filename)[0]
+                track.title = filename.decode(enc).split(r'/')[-1]
+
 
             if "album" in audio: track.album = audio["album"][0]
             if "artist" in audio: track.artist = audio["artist"][0]
--- script.module.gmusicapi/lib/gmusicapi/utils/apilogging.py
+++ script.module.gmusicapi/lib/gmusicapi/utils/apilogging.py
@@ -31,7 +31,6 @@
 import logging
 
 root_logger_name = "gmusicapi"
-log_filename = "gmusicapi.log"
 
 #Logging code adapted from: 
 # http://docs.python.org/howto/logging-cookbook.html#logging-cookbook
@@ -43,20 +42,14 @@ class LogController(object):
     logger = logging.getLogger(root_logger_name)
     logger.setLevel(logging.DEBUG)
 
-    # create file handler to log debug info
-    fh = logging.FileHandler(log_filename)
-    fh.setLevel(logging.DEBUG)
-
     # create console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setLevel(logging.WARNING)
 
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
     ch.setFormatter(formatter)
 
-    logger.addHandler(fh)
     logger.addHandler(ch)
 
     logger.info("!-- Starting log --!")
