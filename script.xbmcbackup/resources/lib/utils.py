c49f5a5512907beeb50ac0f4346f96812e2370fc
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-10-08 10:36:06 +0200

    [script.xbmcbackup] 0.4.1

--- script.xbmcbackup/resources/lib/utils.py
+++ script.xbmcbackup/resources/lib/utils.py
@@ -14,7 +14,7 @@ def log(message,loglevel=xbmc.LOGNOTICE):
     xbmc.log(encode(__addon_id__ + ": " + message),level=loglevel)
 
 def showNotification(message):
-    xbmc.executebuiltin("Notification(" + getString(30010) + "," + message + ",4000," + xbmc.translatePath(__Addon.getAddonInfo('path') + "/icon.png") + ")")
+    xbmc.executebuiltin("Notification(" + encode(getString(30010)) + "," + encode(message) + ",4000," + xbmc.translatePath(__Addon.getAddonInfo('path') + "/icon.png") + ")")
 
 def getSetting(name):
     return __Addon.getSetting(name)
