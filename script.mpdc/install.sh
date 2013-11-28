4fcd94308e6cd52e58eebaa75bf252f0cabc8d5c
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-11-28 18:47:51 +0100

    repo cleanup: remove unneeded files

--- script.mpdc/install.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-# do NOT use this script from XBMC addons directory, it is intented for development only
-DESTDIR=~/.xbmc/addons/script.mpdc
-
-rm -rf ${DESTDIR}
-mkdir -p ${DESTDIR}
-cp -a * ${DESTDIR}
