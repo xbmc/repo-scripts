2d11e0125c62a7bf3cba60fa0b2c61218e088085
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-02-08 21:35:26 +0100

    [script.xbmcbackup] 0.4.5

--- script.xbmcbackup/resources/lib/utils.py
+++ script.xbmcbackup/resources/lib/utils.py
@@ -14,7 +14,7 @@ def openSettings():
     __Addon.openSettings()
 
 def log(message,loglevel=xbmc.LOGNOTICE):
-    xbmc.log(encode(__addon_id__ + ": " + message),level=loglevel)
+    xbmc.log(encode(__addon_id__ + "-" + __Addon.getAddonInfo('version') +  ": " + message),level=loglevel)
 
 def showNotification(message):
     xbmc.executebuiltin("Notification(" + encode(getString(30010)) + "," + encode(message) + ",4000," + xbmc.translatePath(__Addon.getAddonInfo('path') + "/icon.png") + ")")
@@ -29,4 +29,11 @@ def getString(string_id):
     return __Addon.getLocalizedString(string_id)
 
 def encode(string):
-    return string.encode('UTF-8','replace')
+    result = ''
+
+    try:
+        result = string.encode('UTF-8','replace')
+    except UnicodeDecodeError:
+        result = 'Unicode Error'
+    
+    return result
