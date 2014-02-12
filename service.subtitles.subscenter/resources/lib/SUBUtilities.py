ee1a316b886a9d5de360fb32edf4b7d71af7da61
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-02-12 08:48:29 +0100

    [service.subtitles.subscenter] 3.1.1

--- service.subtitles.subscenter/resources/lib/SUBUtilities.py
+++ service.subtitles.subscenter/resources/lib/SUBUtilities.py
@@ -7,12 +7,19 @@ import unicodedata
 import json
 import zlib
 import shutil
-# from stubs import xbmc
-# from stubs import xbmcvfs
-# from stubs import xbmcaddon
-import xbmc
-import xbmcvfs
-import xbmcaddon
+try:
+    import StorageServer
+except ImportError:
+    import storageserverdummy as StorageServer
+
+try:
+    import xbmc
+    import xbmcvfs
+    import xbmcaddon
+except ImportError:
+    from stubs import xbmc
+    from stubs import xbmcvfs
+    from stubs import xbmcaddon
 
 
 __addon__ = xbmcaddon.Addon()
@@ -21,7 +28,7 @@ __scriptname__ = __addon__.getAddonInfo('name')
 __profile__ = xbmc.translatePath(__addon__.getAddonInfo('profile')).decode("utf-8")
 __temp__ = xbmc.translatePath(os.path.join(__profile__, 'temp')).decode("utf-8")
 
-
+cache = StorageServer.StorageServer(__scriptname__, int(24 * 364 / 2))  # 6 months
 
 #===============================================================================
 # Private utility functions
@@ -35,6 +42,9 @@ def normalizeString(str):
 def log(module, msg):
     xbmc.log((u"### [%s] - %s" % (module, msg,)).encode('utf-8'), level=xbmc.LOGDEBUG)
 
+def get_cache_key(prefix="", str=""):
+    str = re.sub('\W+', '_', str).lower()
+    return prefix + str
 
 class SubscenterHelper:
     BASE_URL = "http://www.subscenter.org"
@@ -52,18 +62,29 @@ class SubscenterHelper:
         results = []
 
         search_string = re.split(r'\s\(\w+\)$', item["tvshow"])[0] if item["tvshow"] else item["title"]
-        query = {"q": search_string.lower()}
-        search_result = self.urlHandler.request(self.BASE_URL + "/he/subtitle/search/?" + urllib.urlencode(query))
-        if search_result is None:
-            return results # return empty set
+        
+        if item["tvshow"]:
+            cache_key = get_cache_key("tv-show_", search_string)
+            results = cache.get(cache_key)
+            if results:
+                results = eval(results)
+            
+        if not results:
+            query = {"q": search_string.lower()}
+            search_result = self.urlHandler.request(self.BASE_URL + "/he/subtitle/search/?" + urllib.urlencode(query))
+            if search_result is None:
+                return results # return empty set
 
-        urls = re.findall('<a href=".*/he/subtitle/(movie|series)/([^/]+)/">[^/]+ / ([^<]+)</a>', search_result)
-        years = re.findall(u'<span class="special">[^:]+: </span>(\d{4}).<br />', search_result)
-        for i, url in enumerate(urls):
-            year = years[i] if len(years)>i else ''
-            urls[i] += (year,)
-
-        results = self._filter_urls(urls, search_string, item)
+            urls = re.findall('<a href=".*/he/subtitle/(movie|series)/([^/]+)/">[^/]+ / ([^<]+)</a>', search_result)
+            years = re.findall(u'<span class="special">[^:]+: </span>(\d{4}).<br />', search_result)
+            for i, url in enumerate(urls):
+                year = years[i] if len(years)>i else ''
+                urls[i] += (year,)
+            results = self._filter_urls(urls, search_string, item)
+            
+            if item["tvshow"]:
+                cache.set(cache_key, repr(results))
+            
         return results
 
 
@@ -219,173 +240,3 @@ class URLHandler():
 #         '3let_language': ['en', 'he']}
 # helper = SubscenterHelper()
 # helper.get_subtitle_list(item)
-#
-#
-# def get_rating(subsfile, videofile):
-#     x = 0
-#     rating = 0
-#     log(__scriptname__, "# Comparing Releases:\n %s [subtitle-rls] \n %s [filename-rls]" % (subsfile, videofile))
-#     videofile = "".join(videofile.split('.')[:-1]).lower()
-#     subsfile = subsfile.lower().replace('.', '')
-#     videofile = videofile.replace('.', '')
-#     for release_type in releases_types:
-#         if release_type in videofile:
-#             x += 1
-#             if (release_type in subsfile): rating += 1
-#     if x: rating = (rating / float(x)) * 4
-#     # Compare group name
-#     if videofile.split('-')[-1] == subsfile.split('-')[-1]:
-#         rating += 1
-#     # Group name didn't match
-#     # try to see if group name is in the beginning (less info on file less weight)
-#     elif videofile.split('-')[0] == subsfile.split('-')[-1]:
-#         rating += 0.5
-#     if rating > 0:
-#         rating *= 2
-#     log(__scriptname__, "# Result is: %f" % rating)
-#     return round(rating)
-#
-# # The function receives a subtitles page id number, a list of user selected
-# # languages and the current subtitles list and adds all found subtitles matching
-# # the language selection to the subtitles list.
-# def prepare_subtitle_list(subtitle_page_uri, language_list, file_name):
-#     subtitles_list = []
-#     # Retrieve the subtitles page (html)
-#     try:
-#         subtitlePage = getURL(BASE_URL + subtitle_page_uri)
-#     except:
-#         # Didn't find the page - no such episode?
-#         return
-#
-#     # Didn't find the page - no such episode?
-#     if not subtitlePage:
-#         return
-#
-#     log(__scriptname__, "data=%s" % (subtitlePage))
-#     found_subtitles = json.loads(subtitlePage, encoding="utf-8")
-#
-#     for language in found_subtitles.keys():
-#         if xbmc.convertLanguage(language, xbmc.ISO_639_2) in language_list:
-#             for translator in found_subtitles[language]:
-#                 for quality in found_subtitles[language][translator]:
-#                     for rating in found_subtitles[language][translator][quality]:
-#                         current = found_subtitles[language][translator][quality][rating]
-#                         title = current["subtitle_version"]
-#                         subtitle_rate = get_rating(title, file_name)
-#                         subtitles_list.append(
-#                             {'lang_index': language_list.index(xbmc.convertLanguage(language, xbmc.ISO_639_2)),
-#                              'filename': title,
-#                              'link': current["key"],
-#                              'language_name': xbmc.convertLanguage(language, xbmc.ENGLISH_NAME),
-#                              'language_flag': language,
-#                              'ID': current["id"],
-#                              'rating': str(subtitle_rate),
-#                              'sync': subtitle_rate >= 8,
-#                              'hearing_imp': current["hearing_impaired"] > 0
-#                             })
-#     return subtitles_list
-#
-#
-# def search(item):
-#     if item['tvshow']:
-#         searchString = item['tvshow'].replace(" ", "+")
-#     else:
-#         searchString = item['title'].replace(" ", "+")
-#     log(__scriptname__, "Search string = %s" % (searchString.lower()))
-#
-#     # Retrieve the search results (html)
-#     searchResults = getURL(BASE_URL + "/he/subtitle/search/?q=" + searchString.lower())
-#     # Search most likely timed out, no results
-#     if not searchResults:
-#         return
-#
-#     # Look for subtitles page links
-#     if item['tvshow']:
-#         subtitleIDs = re.findall(TV_SEARCH_RESULTS_PATTERN, searchResults, re.DOTALL)
-#     else:
-#         subtitleIDs = re.findall(MOVIES_SEARCH_RESULTS_PATTERN, searchResults, re.DOTALL)
-#         # Look for more subtitle pages
-#
-#     pages = re._search(MULTI_RESULTS_PAGE_PATTERN, unicode(searchResults, "utf-8"))
-#     # If we found them look inside for subtitles page links
-#     if (pages):
-#         # Limit to only 2 pages
-#         while (int(pages.group("curr_page")) <= 2):
-#             searchResults = getURL(BASE_URL + "/he/subtitle/search/?q=" + searchString.lower() + "&page=" + str(
-#                 int(pages.group("curr_page")) + 1))
-#
-#             if item['tvshow']:
-#                 tempSIDs = re.findall(TV_SEARCH_RESULTS_PATTERN, searchResults, re.DOTALL)
-#             else:
-#                 tempSIDs = re.findall(MOVIES_SEARCH_RESULTS_PATTERN, searchResults, re.DOTALL)
-#
-#             for sid in tempSIDs:
-#                 subtitleIDs.append(sid)
-#             pages = re._search(MULTI_RESULTS_PAGE_PATTERN, unicode(searchResults, "utf-8"))
-#
-#     # Uniqify the list
-#     subtitleIDs = list(set(subtitleIDs))
-#     # If looking for tvshows try to append season and episode to url
-#     for i in range(len(subtitleIDs)):
-#         subtitleIDs[i] = subtitleIDs[i].replace("/subtitle/", "/cinemast/data/")
-#         if item['tvshow']:
-#             subtitleIDs[i] = subtitleIDs[i].replace("/series/", "/series/sb/")
-#             subtitleIDs[i] += item["season"] + "/" + item["episode"] + "/"
-#         else:
-#             subtitleIDs[i] = subtitleIDs[i].replace("/movie/", "/movie/sb/")
-#
-#     file_name = os.path.basename(item['file_original_path']);
-#     subtitles_list = []
-#     for sid in subtitleIDs:
-#         subtitles_list += prepare_subtitle_list(sid, item['3let_language'], file_name)
-#
-#     if subtitles_list:
-#         # Sort the subtitles
-#         subtitles_list = sorted(subtitles_list, key=lambda x: int(float(x['rating'])), reverse=True)
-#         for it in subtitles_list:
-#             listitem = xbmcgui.ListItem(label=it["language_name"],
-#                                         label2=it["filename"],
-#                                         iconImage=it["rating"],
-#                                         thumbnailImage=it["language_flag"]
-#             )
-#             if it["sync"]:
-#                 listitem.setProperty("sync", "true")
-#             else:
-#                 listitem.setProperty("sync", "false")
-#
-#             if it.get("hearing_imp", False):
-#                 listitem.setProperty("hearing_imp", "true")
-#             else:
-#                 listitem.setProperty("hearing_imp", "false")
-#
-#             url = "plugin://%s/?action=download&link=%s&ID=%s&filename=%s" % (
-#                 __scriptid__, it["link"], it["ID"], it["filename"])
-#             xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=listitem, isFolder=False)
-#
-#
-# def download(id, key, filename, stack=False):
-#     subtitle_list = []
-#     exts = [".srt", ".sub"]
-#
-#     ## Cleanup temp dir, we recomend you download/unzip your subs in temp folder and
-#     ## pass that to XBMC to copy and activate
-#     if xbmcvfs.exists(__temp__):
-#         shutil.rmtree(__temp__)
-#     xbmcvfs.mkdirs(__temp__)
-#
-#     zip = os.path.join(__temp__, "subs.zip")
-#     url = BASE_URL + "/subtitle/download/he/" + str(id) + "/?v=" + filename + "&key=" + key
-#     log(__scriptname__, "Fetching subtitles using url %s" % url)
-#     f = urllib.urlopen(url)
-#     with open(zip, "wb") as subFile:
-#         subFile.write(f.read())
-#     subFile.close()
-#     xbmc.sleep(500)
-#     xbmc.executebuiltin(('XBMC.Extract("%s","%s")' % (zip, __temp__,)).encode('utf-8'), True)
-#
-#     for file in xbmcvfs.listdir(__temp__)[1]:
-#         full_path = os.path.join(__temp__, file)
-#         if os.path.splitext(full_path)[1] in exts:
-#             subtitle_list.append(full_path)
-#
-#     return subtitle_list
