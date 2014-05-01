b6b1bdb71c69fe63a61762d6b548c70fce94d91a
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-05-01 12:46:47 +0200

    [script.tv.show.next.aired] REVERT 6.0.8 DUE TO CRASHING REPORTS

--- script.tv.show.next.aired/service.py
+++
@@ -1,6 +0,0 @@
-from default import NextAired
-
-# This will startup the Next-Aired script as a background updater.  If the skin is
-# also starting one up, the skin's version notices that we're running and exits.
-sys.argv = [ 'default.py', 'service=true' ]
-NextAired()
