f41ecbccb077e6160ea2eb81454d7fb4b8004f0c
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-08-26 21:36:49 +0200

    [script.keymap] 0.9.7

--- script.keymap/actions.py
+++ script.keymap/actions.py
@@ -1,17 +1,20 @@
-'''
-    This program is free software, you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    [at your option) any later version.
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Thomas Amland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http,//www.gnu.org/licenses/>.
-'''
 _actions = [
   ["Navigation", [
     "left"              , "Move Left",
@@ -167,6 +170,10 @@ _actions = [
   ]],
 
   ["Other", [
+    "updatelibrary(video)", "Update Video Library",
+    "updatelibrary(music)", "Update Music Library",
+    "cleanlibrary(video)",  "Clean Video Library",
+    "cleanlibrary(music)", "Clean Music Library",
     "codecinfo"         , "Show codec info",
     "screenshot"        , "Take screenshot",
     "reloadkeymaps"     , "Reload keymaps",
@@ -327,6 +334,8 @@ _activate_window = [
 
 _windows = [
   "global"                   , "Global",
+  "fullscreenvideo"          , "Fullscreen Video",
+  "fullscreenlivetv"         , "Fullscreen Live TV",
   "home"                     , "Home",
   "programs"                 , "Programs",
   "videos"                   , "Videos",
@@ -341,8 +350,6 @@ _windows = [
   "musicosd"                 , "Music OSD",
   "osdvideosettings"         , "Video OSD Settings",
   "osdaudiosettings"         , "Audio OSD Settings",
-  "fullscreenvideo"          , "Fullscreen Video",
-  "fullscreenlivetv"         , "Fullscreen Live TV",
   "visualisation"            , "Visualisation",
   "slideshow"                , "Slideshow"
 ]
