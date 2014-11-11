da499a7dcc45d4605330f2e5b9126aee4af1af9b
amet <amet.nospam@gmail.com>
2014-11-11 23:36:19 +0400

    [service.subtitles.opensubtitles] -v5.0.11
    5.0.11
    - fix: search issues
    - cosmetics
    - add slash or backslash at the end of path (fix xbmcvfs.exists in Helix), thx Ondrej Bima

--- service.subtitles.opensubtitles/service.py
+++ service.subtitles.opensubtitles/service.py
@@ -20,7 +20,7 @@ __language__   = __addon__.getLocalizedString
 __cwd__        = xbmc.translatePath( __addon__.getAddonInfo('path') ).decode("utf-8")
 __profile__    = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode("utf-8")
 __resource__   = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) ).decode("utf-8")
-__temp__       = xbmc.translatePath( os.path.join( __profile__, 'temp') ).decode("utf-8")
+__temp__       = xbmc.translatePath( os.path.join( __profile__, 'temp', '') ).decode("utf-8")
 
 if xbmcvfs.exists(__temp__):
   shutil.rmtree(__temp__)
