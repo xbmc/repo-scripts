526c9ad23770691851fc1dea1d8157d9f2f70599
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-04-18 19:13:55 +0200

    [service.subtitles.napisy24pl] 1.0.10

--- service.subtitles.napisy24pl/service.py
+++ service.subtitles.napisy24pl/service.py
@@ -242,7 +242,7 @@ def Download(sub_id):  #standard input
     ## Cleanup temp dir, we recomend you download/unzip your subs in temp folder and
     ## pass that to XBMC to copy and activate
     if xbmcvfs.exists(__temp__):
-        shutil.rmtree(__temp__)
+        shutil.rmtree(__temp__.encode(sys.getfilesystemencoding()))
     xbmcvfs.mkdirs(__temp__)
 
     cj = CookieJar()
