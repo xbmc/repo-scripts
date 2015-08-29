926ed8e9f1b6351ab709a72c51b48c5f45729ac6
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-08-29 10:26:58 +0200

    [screensaver.weather] 1.1.0

--- screensaver.weather/default.py
+++ screensaver.weather/default.py
@@ -16,22 +16,74 @@ sys.path.append(__lib__)
 from settings import log
 
 
+# Monitor class to handle events like the screensaver deactivating
+class ScreensaverExitMonitor(xbmc.Monitor):
+    def __init__(self):
+        self.stopScreensaver = False
+
+    # Called when the screensaver should be stopped
+    def onScreensaverDeactivated(self):
+        log("Deactivate Screensaver")
+        self.stopScreensaver = True
+
+    def onScreensaverActivated(self):
+        log("Activate Screensaver")
+        self.stopScreensaver = False
+
+    def isStopScreensaver(self):
+        return self.stopScreensaver
+
+
 ##################################
 # Main of the Weather Screensaver
 ##################################
 if __name__ == '__main__':
     log("WeatherScreensaver Starting %s" % __addon__.getAddonInfo('version'))
 
-    # Check if the weather weather is already showing, if so we do not need
-    # to change the currently active window
+    # Start the monitor so we can see when the screensaver quits
+    exitMon = ScreensaverExitMonitor()
+
+    # Do something to stop the screensaver and see if we need to navigate
+    # to the weather screen, or if it is already showing
+
+    # A bit of a hack, but we need Kodi to think a user is "doing things" so
+    # that itstops the screensaver, so we just send the message
+    # to open the Context menu - which in our case will do nothing
+    # but it does make Kodi think the user has done something
+    xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Input.ContextMenu", "id": 1}')
+
+    # Limit the maximum amount of time to wait for the screensaver to end
+    maxWait = 30
+    while (not exitMon.isStopScreensaver()) and (maxWait > 0):
+        log("WeatherScreensaver: still running initial screensaver, waiting for stop")
+        xbmc.sleep(100)
+        maxWait = maxWait - 1
+
+    del exitMon
+    log("WeatherScreensaver: Initial screensaver stopped")
+
+    # Now it is safe to check to see if the Weather screen is showing
+    # If we checked before the screensaver was stopped then it would think
+    # the active window was the screensaver, rather than the weather
+    backRequired = 'true'
     if not xbmc.getCondVisibility("Window.IsVisible(weather)"):
         log("WeatherScreensaver: Navigating to weather screen")
-        # Activating a window will actually stop the screensaver, so we will
-        # need to check for any movement ourselves and handle that
+
+        # Load the screensaver window
         xbmc.executebuiltin("ActivateWindow(Weather)", True)
-
-        xbmc.executebuiltin('RunScript(%s)' % (os.path.join(__cwd__, "monitor.py")))
     else:
-        log("WeatherScreensaver: Already showing weather screen")
+        log("WeatherScreensaver: Weather screen already displayed")
+        backRequired = 'false'
 
-    log("WeatherScreensaver: Leaving initial script")
+    # Give a little bit of time for everything to shake out before starting the screensaver
+    # waiting another few seconds to start a screensaver is not going to make a difference
+    # to the user
+    maxWait = 30
+    while (not xbmc.abortRequested) and (not xbmc.getCondVisibility("Window.IsVisible(weather)")) and (maxWait > 0):
+        log("WeatherScreensaver: waiting for weather to load")
+        xbmc.sleep(100)
+        maxWait = maxWait - 1
+
+    xbmc.executebuiltin('RunScript(%s,%s)' % (os.path.join(__cwd__, "monitor.py"), backRequired))
+
+    log("WeatherScreensaver Finished")
--- screensaver.weather/monitor.py
+++ screensaver.weather/monitor.py
@@ -23,7 +23,8 @@ class WeatherScreen(xbmcgui.WindowXMLDialog):
     DIM_CONTROL = 3002
 
     def __init__(self, strXMLname, strFallbackPath):
-        pass
+        self.isClosedFlag = False
+        self.ignoreContextMenuAction = False
 
     @staticmethod
     def createWeatherScreen():
@@ -36,40 +37,92 @@ class WeatherScreen(xbmcgui.WindowXMLDialog):
         # Set the value of the dimming for the video
         dimLevel = Settings.getDimValue()
         if dimLevel is not None:
-            log("Setting Dim Level to: %s" % dimLevel)
+            log("WeatherScreen: Setting Dim Level to: %s" % dimLevel)
             dimControl = self.getControl(WeatherScreen.DIM_CONTROL)
             dimControl.setColorDiffuse(dimLevel)
 
     # Handle any activity on the screen, this will result in a call
     # to close the screensaver window
     def onAction(self, action):
-        log("Action received")
+        log("WeatherScreen: Action received %s" % str(action.getId()))
+
+        # Check if we have been told to ignore a context menu action
+        if self.ignoreContextMenuAction and (action.getId() == 117):
+            log("WeatherScreen: Ignoring context menu call")
+            # Only want to ignore one, so reset the flag
+            self.ignoreContextMenuAction = False
+            return
+
         # For any action we want to close, as that means activity
         self.close()
 
     # The user clicked on a control
     def onClick(self, control):
-        log("OnClick received")
+        log("WeatherScreen: OnClick received")
         self.close()
+
+    def close(self):
+        log("WeatherScreen: Closing window")
+        self.isClosedFlag = True
+        xbmcgui.WindowXML.close(self)
+
+    def isClosed(self):
+        return self.isClosedFlag
+
+    def ignoreNextContextMenu(self):
+        self.ignoreContextMenuAction = True
 
 
 ##################################
 # Main of the Weather Screensaver
 ##################################
 if __name__ == '__main__':
-    log("WeatherScreensaver waiting for activity to return")
+    log("WeatherScreensaver: Monitor started")
+
+    backRequired = False
+    try:
+        if len(sys.argv) > 1:
+            if sys.argv[1].lower() == 'true':
+                backRequired = True
+    except:
+        log("WeatherScreensaver: Error checking for back argument")
+
+    log("WeatherScreensaver: Back required is %s" % str(backRequired))
 
     if xbmc.getCondVisibility("Window.IsVisible(weather)"):
         log("WeatherScreensaver: Waiting for key stroke")
         # Display the window that will check for the need to end the screensaver and
         # return to the previous page
         weather = WeatherScreen.createWeatherScreen()
-        weather.doModal()
+        weather.show()
+
+        # Make sure that we stop the screensaver coming in, the minimum value
+        # for the screensaver is 1 minute - so set at 40 seconds to keep active
+        stopScreensaver = 40000
+        sleepInterval = 100
+
+        while (not weather.isClosed()) and (not xbmc.abortRequested):
+            xbmc.sleep(sleepInterval)
+            stopScreensaver = stopScreensaver - sleepInterval
+            if stopScreensaver < sleepInterval:
+                # A bit of a hack, but we need Kodi to think a user is "doing things" so
+                # that it does not start the screensaver, so we just send the message
+                # to open the Context menu - which in our case will do nothing
+                # but it does make Kodi think the user has done something
+
+                # We do not want the context menu call to result in the Weather screensaver
+                # thinking there is user activity and exiting, so we need to let the
+                # window control know to ignore this context menu message
+                weather.ignoreNextContextMenu()
+                xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Input.ContextMenu", "id": 1}')
+                stopScreensaver = 40000
+
         del weather
 
-        log("WeatherScreensaver: Navigating to previous page from before weather displayed")
-        xbmc.executebuiltin("Action(back)")
+        if backRequired:
+            log("WeatherScreensaver: Navigating to previous page from before weather displayed")
+            xbmc.executebuiltin("Action(back)")
     else:
         log("WeatherScreensaver: Weather screen not visible")
 
-    log("WeatherScreensaver Finished")
+    log("WeatherScreensaver: Monitor Finished")
