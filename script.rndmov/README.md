d5ed5aa352aba5f18f492a1753c6769b1335690f
Martijn Kaijser <mcm.kaijser@gmail.com>
2015-12-23 16:31:10 +0100

    [script.rndmov] 1.0.1

---
+++ script.rndmov/README.md
@@ -0,0 +1,8 @@
+# Random Movie Addon for Kodi
+
+A Kodi script add-on to play a random movie from your movies library.
+
+After install you'll find the script under Settings » Add-ons » Program Add-ons » Random Movie
+
+I've added a setting for filtering types of movies to select from; all, watched, and unwatched.
+You can even set the addon to ask each time you start it.
---
+++ script.rndmov/addon.py
@@ -0,0 +1,34 @@
+﻿import xbmc,xbmcaddon,xbmcgui,json,random
+
+def getAllMovies(randomType):
+  rpccmd = {'jsonrpc': '2.0', 'method': 'VideoLibrary.GetMovies', 'params': { 'properties': [ 'file' ] }, 'id': 'libMovies'}
+
+  if (randomType == '1'):
+    rpccmd = {'jsonrpc': '2.0', 'method': 'VideoLibrary.GetMovies', 'params': { 'filter': { 'field': 'playcount', 'operator': 'greaterthan', 'value': '0' }, 'properties': [ 'file' ] }, 'id': 'libMovies'}
+  if (randomType == '2'):
+    rpccmd = {'jsonrpc': '2.0', 'method': 'VideoLibrary.GetMovies', 'params': { 'filter': { 'field': 'playcount', 'operator': 'lessthan', 'value': '1' }, 'properties': [ 'file' ] }, 'id': 'libMovies'}
+
+  rpccmd = json.dumps(rpccmd)
+  result = xbmc.executeJSONRPC(rpccmd)
+  result = json.loads(result)
+  return result
+
+addon     = xbmcaddon.Addon()
+addonName = addon.getAddonInfo('name')
+addonIcon = addon.getAddonInfo('icon')
+
+randomType         = addon.getSetting('randomType')
+askForTypeOnLaunch = addon.getSetting('askForTypeOnLaunch')
+
+if (askForTypeOnLaunch == 'true'):
+  randomType = xbmcgui.Dialog().select(addon.getLocalizedString(32002), [addon.getLocalizedString(32004), addon.getLocalizedString(32005), addon.getLocalizedString(32006), addon.getLocalizedString(32007)])
+
+if (randomType == '3') or (randomType == 3):
+  quit()
+else:
+  movies = getAllMovies(randomType)
+  movie  = random.choice(movies['result']['movies'])
+  time   = 10000
+
+  xbmc.executebuiltin('PlayMedia(%s)'%(movie['file']))
+  xbmc.executebuiltin('Notification(%s, %s %s, %d, %s)'%(addonName,"Playing ",movie['label'],time,addonIcon))
