83eb3222c1f86da9a9786dd68dc2a40babfdace8
Martijn Kaijser <mcm.kaijser@gmail.com>
2016-02-27 11:02:01 +0100

    [script.playrandomvideos 1.0.3

--- script.playrandomvideos/resources/lib/pykodi.py
+++ script.playrandomvideos/resources/lib/pykodi.py
@@ -15,7 +15,7 @@ def execute_jsonrpc(jsonrpc_command):
             jsonrpc_command = jsonrpc_command.encode('utf-8')
 
     json_result = xbmc.executeJSONRPC(jsonrpc_command)
-    return _json_to_str(json.loads(json_result))
+    return json.loads(json_result, cls=UTF8JSONDecoder)
 
 def get_base_json_request(method):
     return {'jsonrpc': '2.0', 'method': method, 'params': {}, 'id': 1}
@@ -35,16 +35,6 @@ def log(message, level=xbmc.LOGDEBUG):
     file_message = '[%s] %s' % (addonid, message)
     xbmc.log(file_message, level)
 
-def _json_to_str(jsoninput):
-    if isinstance(jsoninput, dict):
-        return {_json_to_str(key): _json_to_str(value) for key, value in jsoninput.iteritems()}
-    elif isinstance(jsoninput, list):
-        return [_json_to_str(item) for item in jsoninput]
-    elif isinstance(jsoninput, unicode):
-        return jsoninput.encode('utf-8')
-    else:
-        return jsoninput
-
 class LogJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, (dict, list, basestring)):
@@ -56,3 +46,22 @@ class LogJSONEncoder(json.JSONEncoder):
         if hasattr(obj, '__dict__'):
             return obj.__dict__
         return str(obj)
+
+class UTF8JSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super(UTF8JSONDecoder, self).__init__(*args, **kwargs)
+
+    def raw_decode(self, s, idx=0):
+        result, end = super(UTF8JSONDecoder, self).raw_decode(s)
+        result = self._json_unicode_to_str(result)
+        return result, end
+
+    def _json_unicode_to_str(self, jsoninput):
+        if isinstance(jsoninput, dict):
+            return dict((self._json_unicode_to_str(key), self._json_unicode_to_str(value)) for key, value in jsoninput.iteritems())
+        elif isinstance(jsoninput, list):
+            return [self._json_unicode_to_str(item) for item in jsoninput]
+        elif isinstance(jsoninput, unicode):
+            return jsoninput.encode('utf-8')
+        else:
+            return jsoninput
