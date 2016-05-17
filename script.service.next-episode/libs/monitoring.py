82e082eff1bbd1b299b818fa4924a5c313929a42
Roman Miroshnychenko <romanvm@yandex.ua>
2016-05-17 14:07:00 +0300

    [script.service.next-episode] 1.1.0

--- script.service.next-episode/libs/monitoring.py
+++ script.service.next-episode/libs/monitoring.py
@@ -3,9 +3,11 @@
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 # License: GPL v. 3 <http://www.gnu.org/licenses/gpl-3.0.en.html>
 
+import json
 import xbmc
 from xbmcgui import Dialog
-from commands import sync_library, sync_new_items, login, addon
+from commands import sync_library, sync_new_items, login, addon, update_single_item
+from medialibrary import get_item_details
 from gui import ui_string
 # Here ``addon`` is imported from another module to prevent a bug
 # when username and hash are not stored in the addon settings.
@@ -21,6 +23,23 @@ class UpdateMonitor(xbmc.Monitor):
         if library == 'video':
             sync_new_items()
             xbmc.log('next-episode.net: new items updated', xbmc.LOGNOTICE)
+
+    def onNotification(self, sender, method, data):
+        """
+        Example data::
+
+            16:05:29 T:8216  NOTICE: Sender: xbmc
+            16:05:29 T:8216  NOTICE: Method: VideoLibrary.OnUpdate
+            16:05:29 T:8216  NOTICE: Data: {"item":{"id":3,"type":"movie"},"playcount":1}
+
+            16:10:14 T:8216  NOTICE: Sender: xbmc
+            16:10:14 T:8216  NOTICE: Method: VideoLibrary.OnUpdate
+            16:10:14 T:8216  NOTICE: Data: {"item":{"id":10,"type":"episode"},"playcount":1}
+        """
+        if method == 'VideoLibrary.OnUpdate' and 'playcount' in data:
+            item = json.loads(data)['item']
+            item.update(get_item_details(item['id'], item['type']))
+            update_single_item(item)
 
 
 def initial_prompt():
