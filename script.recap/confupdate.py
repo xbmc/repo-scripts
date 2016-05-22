c4bcd2cb63348317a56a163cb6d1e9c44bc4dfd3
robwebset <rob@deadlyduck.com>
2016-05-22 14:41:27 +0100

    [script.recap] 1.0.0

---
+++ script.recap/confupdate.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+import traceback
+import xbmc
+import xbmcaddon
+import xbmcvfs
+import xbmcgui
+import datetime
+
+# Import the common settings
+from resources.lib.settings import log
+from resources.lib.settings import os_path_join
+
+ADDON = xbmcaddon.Addon(id='script.recap')
+
+
+# Ideally we would use an XML parser to do this like ElementTree
+# However they all end up re-ordering the attributes, so doing a diff
+# between changed files is very hard, so for this reason we do it
+# all manually without the aid of an XML parser
+class ConfUpdate():
+    def __init__(self):
+        # Find out where the confluence skin files are located
+        confAddon = xbmcaddon.Addon(id='skin.confluence')
+        self.confpath = xbmc.translatePath(confAddon.getAddonInfo('path'))
+        self.confpath = os_path_join(self.confpath, '720p')
+        log("Confluence Location: %s" % self.confpath)
+        # Create the timestamp centrally, as we want all files changed for a single
+        # run to have the same backup timestamp so it can be easily undone if the
+        # user wishes to switch it back
+        self.bak_timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        self.errorToLog = False
+
+    # Method to update all of the required Confluence files
+    def updateSkin(self):
+        # Update the files one at a time
+        self._updateDialogVideoInfo()
+
+        # Now either print the complete message or the "check log" message
+        if self.errorToLog:
+            xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32019), ADDON.getLocalizedString(32020))
+        else:
+            xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32021), ADDON.getLocalizedString(32022))
+
+    # Save the new contents, taking a backup of the old file
+    def _saveNewFile(self, dialogXml, dialogXmlStr):
+        log("SaveNewFile: New file content: %s" % dialogXmlStr)
+
+        # Now save the file to disk, start by backing up the old file
+        xbmcvfs.copy(dialogXml, "%s.recap-%s.bak" % (dialogXml, self.bak_timestamp))
+
+        # Now save the new file
+        dialogXmlFile = xbmcvfs.File(dialogXml, 'w')
+        dialogXmlFile.write(dialogXmlStr)
+        dialogXmlFile.close()
+
+    ##########################################################################
+    # UPDATES FOR DialogVideoInfo.xml
+    ##########################################################################
+    # Makes all the required changes to DialogVideoInfo.xml
+    def _updateDialogVideoInfo(self):
+        # Get the location of the information dialog XML file
+        dialogXml = os_path_join(self.confpath, 'DialogVideoInfo.xml')
+        log("DialogVideoInfo: Confluence dialog XML file: %s" % dialogXml)
+
+        # Make sure the file exists (It should always exist)
+        if not xbmcvfs.exists(dialogXml):
+            log("DialogVideoInfo: Unable to find the file DialogVideoInfo.xml, skipping file", xbmc.LOGERROR)
+            self.errorToLog = True
+            return
+
+        # Load the DialogVideoInfo.xml into a string
+        dialogXmlFile = xbmcvfs.File(dialogXml, 'r')
+        dialogXmlStr = dialogXmlFile.read()
+        dialogXmlFile.close()
+
+        # Now check to see if the skin file has already had the recap bits added
+        if 'script.recap' in dialogXmlStr:
+            # Already have recap referenced, so we do not want to do anything else
+            # to this file
+            log("DialogVideoInfo: Recap already referenced in %s, skipping file" % dialogXml, xbmc.LOGINFO)
+            self.errorToLog = True
+            return
+
+        # Now we need to add the button after the Final button
+        previousButton = '<label>13511</label>'
+
+        if previousButton not in dialogXmlStr:
+            # The file has had a standard component deleted, so quit
+            log("DialogVideoInfo: Could not find final button, skipping file", xbmc.LOGERROR)
+            self.errorToLog = True
+            return
+
+        # Check to make sure we use a unique ID value for the button
+        idOK = False
+        idval = 102
+        while not idOK:
+            idStr = "id=\"%d\"" % idval
+            if idStr not in dialogXmlStr:
+                idOK = True
+            else:
+                idval = idval + 1
+
+        # Now add the Recap button after the Final one
+        DIALOG_VIDEO_INFO_BUTTON = '''\n\t\t\t\t\t</control>\n\t\t\t\t\t<control type="button" id="%d">
+\t\t\t\t\t\t<description>Recap</description>
+\t\t\t\t\t\t<include>ButtonInfoDialogsCommonValues</include>
+\t\t\t\t\t\t<label>$ADDON[script.recap 32001]</label>
+\t\t\t\t\t\t<onclick>RunScript(script.recap)</onclick>
+\t\t\t\t\t\t<visible>System.HasAddon(script.recap) + [Container.Content(tvshows) | Container.Content(seasons) | Container.Content(episodes)]</visible>'''
+
+        insertTxt = previousButton + (DIALOG_VIDEO_INFO_BUTTON % idval)
+        dialogXmlStr = dialogXmlStr.replace(previousButton, insertTxt)
+
+        self._saveNewFile(dialogXml, dialogXmlStr)
+
+
+#########################
+# Main
+#########################
+if __name__ == '__main__':
+    log("Recap: Updating Confluence Skin (version %s)" % ADDON.getAddonInfo('version'))
+
+    doUpdate = xbmcgui.Dialog().yesno(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32017))
+
+    if doUpdate:
+        try:
+            confUp = ConfUpdate()
+            confUp.updateSkin()
+            del confUp
+        except:
+            log("Recap: %s" % traceback.format_exc(), xbmc.LOGERROR)
+            xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32018), ADDON.getLocalizedString(32020))
---
+++ script.recap/resources/lib/viewer.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+import xbmcgui
+import xbmcaddon
+
+# Import the common settings
+from settings import log
+from settings import Settings
+
+ADDON = xbmcaddon.Addon(id='script.recap')
+CWD = ADDON.getAddonInfo('path').decode("utf-8")
+
+
+# Handles the display window
+class Viewer(xbmcgui.WindowXMLDialog):
+    TEXT_BOX_ID = 202
+    TITLE_LABEL_ID = 201
+    IMAGE_ID = 204
+    CAPTION_ID = 205
+    CLOSE_BUTTON = 302
+    PREVIOUS_BUTTON = 305
+    NEXT_BUTTON = 304
+    CACHED_IMAGE_OFFSET = 400
+
+    def __init__(self, *args, **kwargs):
+        self.isClosedFlag = False
+        self.currentImage = 0
+        self.title = kwargs.get('title', '')
+        self.summary = kwargs.get('summary', '')
+        self.slideshow = kwargs.get('slideshow', '')
+        xbmcgui.WindowXMLDialog.__init__(self)
+
+    @staticmethod
+    def createViewer(title, summary, slideshow):
+        return Viewer("script-recap-window.xml", CWD, title=title, summary=summary, slideshow=slideshow)
+
+    # Called when setting up the window
+    def onInit(self):
+        # Update the dialog to show the correct data
+        labelControl = self.getControl(Viewer.TITLE_LABEL_ID)
+        labelControl.setLabel(self.title)
+
+        textControl = self.getControl(Viewer.TEXT_BOX_ID)
+        textControl.setText(self.summary)
+
+        # Set the initial image
+        self._setSlideshowImage(0)
+
+        # load all the images into the cache
+        for i in range(1, 51):
+            # If we have an image to cache
+            if i <= len(self.slideshow):
+                cacheImageControlId = Viewer.CACHED_IMAGE_OFFSET + i
+                imageControl = self.getControl(cacheImageControlId)
+                imageControl.setImage(self.slideshow[i - 1]["image"])
+
+        xbmcgui.WindowXMLDialog.onInit(self)
+
+    # Handle any activity on the screen
+    def onAction(self, action):
+        # actioncodes from https://github.com/xbmc/xbmc/blob/master/xbmc/input/Key.h
+        ACTION_PREVIOUS_MENU = 10
+        ACTION_NAV_BACK = 92
+        ACTION_PAGE_UP = 5
+        ACTION_PAGE_DOWN = 6
+
+        if (action == ACTION_PREVIOUS_MENU) or (action == ACTION_NAV_BACK):
+            log("Viewer: Close Action received: %s" % str(action.getId()))
+            self.close()
+        elif action == ACTION_PAGE_UP:
+            log("Viewer: Page Up Action received: %s" % str(action.getId()))
+            # Page up is going to the previous page
+            self.onClick(Viewer.PREVIOUS_BUTTON)
+        elif action == ACTION_PAGE_DOWN:
+            log("Viewer: Page Down Action received: %s" % str(action.getId()))
+            # Page down is going to the next page
+            self.onClick(Viewer.NEXT_BUTTON)
+
+    def onClick(self, controlID):
+        # Play button has been clicked
+        if controlID == Viewer.CLOSE_BUTTON:
+            log("Viewer: Close click action received: %d" % controlID)
+            self.close()
+        elif controlID == Viewer.NEXT_BUTTON:
+            log("Viewer: Request to view next image: %d" % controlID)
+            self._setSlideshowImage(self.currentImage + 1)
+        elif controlID == Viewer.PREVIOUS_BUTTON:
+            log("Viewer: Request to view previous image: %d" % controlID)
+            self._setSlideshowImage(self.currentImage - 1)
+
+    def close(self):
+        log("Viewer: Closing window")
+        self.isClosedFlag = True
+        xbmcgui.WindowXMLDialog.close(self)
+
+    def isClosed(self):
+        return self.isClosedFlag
+
+    def showNextSlideshowImage(self):
+        autoNextImage = self.currentImage + 1
+        # If we are already on the last image, check if we loop or not
+        if autoNextImage > (len(self.slideshow) - 1):
+            if Settings.isLoopSlideshow():
+                autoNextImage = 0
+            else:
+                return
+
+        # Move on to the next image in the slideshow
+        self._setSlideshowImage(autoNextImage)
+
+    def _setSlideshowImage(self, idx):
+        log("Viewer: Showing image %d" % idx)
+        if idx < 0:
+            if not Settings.isLoopSlideshow():
+                log("Viewer: Image %d out of range 0 to %d" % (idx, len(self.slideshow)))
+            idx = len(self.slideshow) - 1
+            log("Viewer: Image looped to last image %d" % idx)
+        elif idx > (len(self.slideshow) - 1):
+            if not Settings.isLoopSlideshow():
+                log("Viewer: Image %d out of range 0 to %d" % (idx, len(self.slideshow)))
+            idx = 0
+            log("Viewer: Image looped to first image %d" % idx)
+
+        self.currentImage = idx
+        imageControl = self.getControl(Viewer.IMAGE_ID)
+        imageControl.setImage(self.slideshow[idx]["image"])
+
+        if Settings.showImageCaptions():
+            captionControl = self.getControl(Viewer.CAPTION_ID)
+            captionControl.setText(self.slideshow[idx]["caption"])
+
+        previousControl = self.getControl(Viewer.PREVIOUS_BUTTON)
+        nextControl = self.getControl(Viewer.NEXT_BUTTON)
+        # For automated slideshows, do not show the next and previous buttons
+        if Settings.isAutoSlideshow():
+            previousControl.setVisible(False)
+            nextControl.setVisible(False)
+        # If we are looping then need to always show the next and previous buttons
+        elif Settings.isLoopSlideshow():
+            previousControl.setVisible(True)
+            nextControl.setVisible(True)
+        else:
+            if idx < 1:
+                previousControl.setVisible(False)
+            else:
+                previousControl.setVisible(True)
+
+            if idx >= (len(self.slideshow) - 1):
+                nextControl.setVisible(False)
+            else:
+                nextControl.setVisible(True)
+
+        # Hightlight the pre-loaded image to show some form of progress
+        # load all the images into the cache
+        for i in range(1, 51):
+            # Default colour is grey
+            colour = 'EE000000'
+            if i <= idx + 1:
+                # Highlight in blue
+                colour = 'EE000099'
+
+            # If we have an image to cache
+            if i <= len(self.slideshow):
+                cacheImageControlId = Viewer.CACHED_IMAGE_OFFSET + i
+                imageControl = self.getControl(cacheImageControlId)
+                imageControl.setColorDiffuse(colour)
