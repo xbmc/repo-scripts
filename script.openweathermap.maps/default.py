49c7d3dfa5b0700bb0356c6a62198cf2c97a2e6a
ronie <ronie@users.noreply.github.com>
2016-07-12 21:57:33 +0200

    script.openweathermap.maps 0.0.13

--- script.openweathermap.maps/default.py
+++ script.openweathermap.maps/default.py
@@ -1,22 +1,21 @@
-import os, sys, time, urllib2, threading, socket, hashlib, shutil, gzip
+import os, sys, time, urllib2, threading, socket, hashlib, shutil
 import xbmc, xbmcvfs, xbmcaddon
-from StringIO import StringIO
 from PIL import Image
 from PIL import ImageEnhance
 
 ADDON        = xbmcaddon.Addon()
-MAINADDON    = xbmcaddon.Addon('weather.openweathermap.extended')
 ADDONID      = ADDON.getAddonInfo('id')
 ADDONVERSION = ADDON.getAddonInfo('version')
 CWD          = ADDON.getAddonInfo('path').decode("utf-8")
 RESOURCE     = xbmc.translatePath( os.path.join( CWD, 'resources', 'lib' ).encode("utf-8") ).decode("utf-8")
 PROFILE      = xbmc.translatePath(ADDON.getAddonInfo('profile')).decode('utf-8')
+MAINADDON    = xbmcaddon.Addon('weather.openweathermap.extended')
 
 sys.path.append(RESOURCE)
 
 from utils import *
 
-ZOOM = int(MAINADDON.getSetting('Zoom')) + 3
+ZOOM = int(MAINADDON.getSetting('Zoom')) + 2
 
 socket.setdefaulttimeout(10)
 
@@ -24,6 +23,21 @@ class Main:
     def __init__(self):
         set_property('Map.IsFetched', 'true')
         lat, lon = self._parse_argv()
+        clear_property('Map.1.Area')
+        clear_property('Map.2.Area')
+        clear_property('Map.3.Area')
+        clear_property('Map.4.Area')
+        clear_property('Map.5.Area')
+        clear_property('Map.1.Layer')
+        clear_property('Map.2.Layer')
+        clear_property('Map.3.Layer')
+        clear_property('Map.4.Layer')
+        clear_property('Map.5.Layer')
+        clear_property('Map.1.Heading')
+        clear_property('Map.2.Heading')
+        clear_property('Map.3.Heading')
+        clear_property('Map.4.Heading')
+        clear_property('Map.5.Heading')
         self._get_maps(lat, lon)
 
     def _parse_argv(self):
@@ -47,19 +61,21 @@ class Main:
         clouds_url = 'http://undefined.tile.openweathermap.org/map/clouds/%i/%i/%i.png'
         temp_url = 'http://undefined.tile.openweathermap.org/map/temp/%i/%i/%i.png'
         wind_url = 'http://undefined.tile.openweathermap.org/map/wind/%i/%i/%i.png'
-        pressure_url = 'http://undefined.tile.openweathermap.org/map/pressure_cntr/%i/%i/%i.png'
-        streetmapdir = os.path.join(PROFILE, 'maps', 'streetmap-%s' % tag)
-        precipmapdir = os.path.join(PROFILE, 'maps', 'precipmap')
-        cloudsmapdir = os.path.join(PROFILE, 'maps', 'cloudsmap')
-        tempmapdir = os.path.join(PROFILE, 'maps', 'tempmap')
-        windmapdir = os.path.join(PROFILE, 'maps', 'windmap')
-        pressuremapdir = os.path.join(PROFILE, 'maps', 'pressuremap')
+        pressure_url = 'http://undefined.tile.openweathermap.org/map/pressure/%i/%i/%i.png'
+        pressurecntr_url = 'http://undefined.tile.openweathermap.org/map/pressure_cntr/%i/%i/%i.png'
+        streetmapdir = os.path.join(PROFILE, 'maps', 'streetmap-%s' % tag, '')
+        precipmapdir = os.path.join(PROFILE, 'maps', 'precipmap', '')
+        cloudsmapdir = os.path.join(PROFILE, 'maps', 'cloudsmap', '')
+        tempmapdir = os.path.join(PROFILE, 'maps', 'tempmap', '')
+        windmapdir = os.path.join(PROFILE, 'maps', 'windmap', '')
+        pressuremapdir = os.path.join(PROFILE, 'maps', 'pressuremap', '')
+        pressurecntrmapdir = os.path.join(PROFILE, 'maps', 'pressurecntrmap', '')
         lat = float(lat)
         lon = float(lon)
         x, y = GET_TILE(lat, lon, ZOOM)
         imgs = [[x-1,y-1], [x,y-1], [x+1,y-1], [x-1,y], [x,y], [x+1,y], [x-1,y+1], [x,y+1], [x+1,y+1]]
         # adjust for locations on the edge of the map
-        tile_max = 2**ZOOM
+        tile_max = 2**ZOOM - 1
         if x == 0:
             imgs = [[tile_max,y-1], [x,y-1], [x+1,y-1], [tile_max,y], [x,y], [x+1,y], [tile_max,y+1], [x,y+1], [x+1,y+1]]
         elif x == tile_max:
@@ -79,6 +95,11 @@ class Main:
             shutil.rmtree(windmapdir)
         if xbmcvfs.exists(pressuremapdir):
             shutil.rmtree(pressuremapdir)
+        if xbmcvfs.exists(pressurecntrmapdir):
+            shutil.rmtree(pressurecntrmapdir)
+        if xbmcvfs.exists(streetmapdir) and not xbmcvfs.exists(os.path.join(streetmapdir, 'streetmap.png')):
+            # we have an incomplete streetmap
+            shutil.rmtree(streetmapdir)
         if not xbmcvfs.exists(streetmapdir):
             xbmcvfs.mkdirs(streetmapdir)
         # download the streetmap once, unless location or zoom has changed
@@ -106,6 +127,10 @@ class Main:
             xbmcvfs.mkdirs(pressuremapdir)
         thread_pressure = get_tiles(pressuremapdir, 'pressuremap-%s.png', stamp, imgs, pressure_url)
         thread_pressure.start()
+        if not xbmcvfs.exists(pressurecntrmapdir):
+            xbmcvfs.mkdirs(pressurecntrmapdir)
+        thread_pressurecntr = get_tiles(pressurecntrmapdir, 'pressurecntrmap-%s.png', stamp, imgs, pressurecntr_url)
+        thread_pressurecntr.start()
         if streetthread_created:
             thread_street.join()
         thread_precip.join()
@@ -113,6 +138,14 @@ class Main:
         thread_temp.join()
         thread_wind.join()
         thread_pressure.join()
+        thread_pressurecntr.join()
+        psmap = os.path.join(PROFILE, 'maps', 'pressuremap', 'pressuremap-%s.png' % stamp)
+        pscntrmap = os.path.join(PROFILE, 'maps', 'pressurecntrmap', 'pressurecntrmap-%s.png' % stamp)
+        if xbmcvfs.exists(psmap) and xbmcvfs.exists(pscntrmap):
+            background = Image.open(psmap)
+            foreground = Image.open(pscntrmap)
+            background.paste(foreground, (0, 0), foreground)
+            background.save(psmap)
         set_property('Map.1.Area', os.path.join(PROFILE, 'maps', 'streetmap-%s' % tag, 'streetmap.png'))
         set_property('Map.2.Area', os.path.join(PROFILE, 'maps', 'streetmap-%s' % tag, 'streetmap.png'))
         set_property('Map.3.Area', os.path.join(PROFILE, 'maps', 'streetmap-%s' % tag, 'streetmap.png'))
@@ -143,7 +176,7 @@ class Main:
             set_property('Map.4.Legend' , xbmc.translatePath(os.path.join(CWD, 'resources', 'graphics', 'wind-bft.png')))
         else:
             set_property('Map.4.Legend' , xbmc.translatePath(os.path.join(CWD, 'resources', 'graphics', 'wind-kmh.png')))
-        set_property('Map.5.Legend' , '')
+        set_property('Map.5.Legend' , xbmc.translatePath(os.path.join(CWD, 'resources', 'graphics', 'press.png')))
 
 
 class get_tiles(threading.Thread):
@@ -162,28 +195,36 @@ class get_tiles(threading.Thread):
 
     def fetch_tiles(self, imgs, mapdir):
         count = 1
+        success = True
         failed = []
         for img in imgs:
+            data = []
+            query = self.url % (ZOOM, img[0], img[1])
+            req = urllib2.Request(query)
             try:
-                query = self.url % (ZOOM, img[0], img[1])
-                req = urllib2.Request(query)
-                req.add_header('Accept-encoding', 'gzip')
                 response = urllib2.urlopen(req, timeout=10)
-                if response.info().get('Content-Encoding') == 'gzip':
-                    buf = StringIO(response.read())
-                    compr = gzip.GzipFile(fileobj=buf)
-                    data = compr.read()
-                else:
-                    data = response.read()
+                try:
+                    while True:
+                        if MONITOR.abortRequested():
+                            return
+                        bytes = response.read(4096)
+                        if not bytes:
+                            log('image downloaded')
+                            break
+                        data.append(bytes)
+                except:
+                    log('image download failed, retry')
+                    success = False
                 response.close()
-                log('image downloaded')
             except:
-                data = ''
-                log('image download failed, retry')
+                log('failed to connect, retry')
+                success = False
+            if not success:
+                data = []
                 if len(img) == 2:
                     img.append(str(count))
                 failed.append(img)
-            if data != '':
+            if data != []:
                 if len(img) == 3:
                     num = img[2]
                 else:
@@ -191,7 +232,7 @@ class get_tiles(threading.Thread):
                 tilefile = xbmc.translatePath(os.path.join(mapdir, num + '.png')).decode("utf-8")
                 try:
                     tmpmap = open(tilefile, 'wb')
-                    tmpmap.write(data)
+                    tmpmap.write(''.join(data))
                     tmpmap.close()
                 except:
                     log('failed to save image')
@@ -221,7 +262,7 @@ class get_tiles(threading.Thread):
                 out.paste( tile, (imx, imy), tile.convert('RGBA') )
                 imx += 256
             imy += 256
-        if self.mapfile[0:6] == 'precip' or self.mapfile[0:6] == 'clouds':
+        if self.mapfile[0:6] == 'clouds' or self.mapfile[0:9] == 'pressurec':
             enhancer = ImageEnhance.Brightness(out)
             out = enhancer.enhance(0.3)
         if not self.mapfile == 'streetmap.png':
--- script.openweathermap.maps/resources/lib/utils.py
+++ script.openweathermap.maps/resources/lib/utils.py
@@ -9,6 +9,7 @@ WEATHER_WINDOW = xbmcgui.Window(12600)
 DEBUG          = MAINADDON.getSetting('Debug')
 TEMPUNIT       = unicode(xbmc.getRegion('tempunit'),encoding='utf-8')
 SPEEDUNIT      = xbmc.getRegion('speedunit')
+
 if SPEEDUNIT in ('ft/s', 'ft/min', 'ft/h', 'inch/s', 'yard/s', 'kts', ):
     SPEEDUNIT = 'mph'
 
@@ -22,11 +23,14 @@ def log(txt):
 def set_property(name, value):
     WEATHER_WINDOW.setProperty(name, value)
 
+def clear_property(name):
+    WEATHER_WINDOW.clearProperty(name)
+
 # http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Lon..2Flat._to_tile_numbers
 def GET_TILE(lat_deg, lon_deg, zoom):
-  lat_rad = math.radians(lat_deg)
-  n = 2.0 ** zoom
-  xtile = int((lon_deg + 180.0) / 360.0 * n)
-  ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
-  return (xtile, ytile)
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
+    return (xtile, ytile)
 
