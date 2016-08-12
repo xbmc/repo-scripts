523b5e2bc60fb5277011138ac846fd4fe1d16cd1
CaTz-Laptop <catzil@gmail.com>
2016-08-12 10:29:19 +0300

    [service.subtitles.subscenter] 4.0.1

--- service.subtitles.subscenter/resources/lib/SUBUtilities.py
+++ service.subtitles.subscenter/resources/lib/SUBUtilities.py
@@ -30,7 +30,7 @@ __language__ = __addon__.getLocalizedString
 __profile__ = unicode(xbmc.translatePath(__addon__.getAddonInfo('profile')), 'utf-8')
 __temp__ = unicode(xbmc.translatePath(os.path.join(__profile__, 'temp', '')), 'utf-8')
 
-cache = StorageServer.StorageServer(__scriptname__, int(24 * 364 / 2))  # 6 months
+store = StorageServer.StorageServer(__scriptname__, int(24 * 364 / 2))  # 6 months
 regexHelper = re.compile('\W+', re.UNICODE)
 
 
@@ -66,8 +66,8 @@ def clean_title(item):
     item["title"] = unicode(item["title"], "utf-8")
     item["tvshow"] = unicode(item["tvshow"], "utf-8")
     # Removes country identifier at the end
-    item["title"] = re.sub(r'\([^\)]+\)\W*$', '', item["title"])
-    item["tvshow"] = re.sub(r'\([^\)]+\)\W*$', '', item["tvshow"])
+    item["title"] = re.sub(r'\([^\)]+\)\W*$', '', item["title"]).strip()
+    item["tvshow"] = re.sub(r'\([^\)]+\)\W*$', '', item["tvshow"]).strip()
 
 
 def parse_rls_title(item):
@@ -86,7 +86,7 @@ def parse_rls_title(item):
         item["tvshow"] = regexHelper.sub(' ', title).strip()
         item["season"] = str(int(season))
         item["episode"] = str(int(episode))
-        log(__scriptname__, "TV Parsed Item: %s" % (item,))
+        log("TV Parsed Item: %s" % (item,))
 
     else:
         groups = re.findall(r"(.*?)(\d{4})", item["title"], re.I)
@@ -95,25 +95,29 @@ def parse_rls_title(item):
             item["title"] = regexHelper.sub(' ', title).strip()
             item["year"] = groups[0][1] if len(groups[0]) == 2 else item["year"]
 
-            log(__scriptname__, "MOVIE Parsed Item: %s" % (item,))
+            log("MOVIE Parsed Item: %s" % (item,))
 
 
-def clear_cache():
-    cache.delete("tv-show%")
-    xbmc.executebuiltin((u'Notification(%s,%s)' % (__scriptname__, __language__(32004))).encode('utf-8'))
+def clear_store():
+    store.delete("%")
+    notify(32004)
 
 
-def get_cache_key(prefix="", str=""):
+def get_store_key(prefix="", str=""):
     str = re.sub(r'[\'\(\)\.\-\]\[ ]+', '_', str).lower()
     return prefix + str
 
 
-def log(module, msg):
-    xbmc.log((u"### [%s] - %s" % (module, msg,)).encode('utf-8'), level=xbmc.LOGDEBUG)
+def log(msg):
+    xbmc.log((u"### [%s] - %s" % (__scriptname__, msg,)).encode('utf-8'), level=xbmc.LOGDEBUG)
+
+
+def notify(msg_id):
+    xbmc.executebuiltin((u'Notification(%s,%s)' % (__scriptname__, __language__(msg_id))).encode('utf-8'))
 
 
 class SubscenterHelper:
-    BASE_URL = "http://www.subscenter.org"
+    BASE_URL = "http://www.subscenter.org/he/api/"
 
     def __init__(self):
         self.urlHandler = URLHandler()
@@ -129,98 +133,95 @@ class SubscenterHelper:
         results = []
 
         search_string = re.split(r'\s\(\w+\)$', item["tvshow"])[0] if item["tvshow"] else item["title"]
+        user_token = self.get_user_token()
 
+        query = {"q": search_string.encode("utf-8"), "user": user_token["user"], "token": user_token["token"]}
         if item["tvshow"]:
-            cache_key = get_cache_key("tv-show_", search_string)
-            results = cache.get(cache_key)
-            if results:
-                results = eval(results)
+            query["type"] = "series"
+            query["season"] = item["season"]
+            query["episode"] = item["episode"]
+        else:
+            query["type"] = "movies"
+            query["year_start"] = int(item["year"])-1
+            query["year_end"] = int(item["year"])
 
-        if not results:
-            query = {"q": search_string.encode("utf-8").lower() + "'"}  # hack to prevent redirection in hebrew search
-            search_result = self.urlHandler.request(self.BASE_URL + "/he/subtitle/search/?" + urllib.urlencode(query))
-            if search_result is None:
-                return results  # return empty set
+        search_result = self.urlHandler.request(self.BASE_URL + "search/", query)
 
-            urls = re.findall(u'<a href=".*/he/subtitle/(movie|series)/([^/]+)/">(.*) / ([^<]+)</a>', search_result)
-            years = re.findall(u'<span class="special">[^:]+: </span>(\d{4}).<br />', search_result)
-            for i, url in enumerate(urls):
-                year = years[i] if len(years) > i else ''
-                urls[i] += (year,)
+        if search_result is not None and search_result["result"] == "failed":
+            # Update cached token
+            user_token = self.get_user_token(True)
+            query["token"] = user_token["token"]
+            search_result = self.urlHandler.request(self.BASE_URL + "search/", query)
 
-            results = self._filter_urls(urls, search_string, item)
+        if search_result is not None and search_result["result"] == "failed":
+            notify(32009)
+            return results
 
-            if item["tvshow"] and results:
-                cache.set(cache_key, repr(results))
+        log("Results: %s" % search_result)
+
+        if search_result is None or search_result["result"] != "success" or search_result["count"] < 1:
+            return results  # return empty set
+
+        results = self._filter_results(search_result["data"], search_string, item)
+        log("Filtered: %s" % results)
 
         return results
 
-    def _filter_urls(self, urls, search_string, item):
+    def _filter_results(self, results, search_string, item):
         filtered = []
         search_string = regexHelper.sub('', search_string.lower())
 
-        h = HTMLParser.HTMLParser()
-
-        log(__scriptname__, "urls: %s" % urls)
-
-        for i, (content_type, slug, heb_name, eng_name, year) in enumerate(urls):
-            eng_name = unicode(eng_name, 'utf-8')
-            heb_name = unicode(heb_name, 'utf-8')
-
-            eng_name = h.unescape(eng_name).replace(' ...', '').lower()
-            heb_name = h.unescape(heb_name).replace(' ...', '')
+        for result in results:
+            eng_name = result["name_en"].strip().lower()
+            heb_name = result["name_he"].strip()
 
             eng_name = regexHelper.sub(' ', eng_name)
             eng_name_tmp = regexHelper.sub('', eng_name)
             heb_name = regexHelper.sub('', heb_name)
 
-            if ((content_type == "movie" and not item["tvshow"]) or
-                    (content_type == "series" and item["tvshow"])) and \
-                    (search_string.startswith(eng_name_tmp) or eng_name_tmp.startswith(search_string) or
-                         search_string.startswith(heb_name) or heb_name.startswith(search_string)) and \
+            if (search_string.startswith(eng_name_tmp) or eng_name_tmp.startswith(search_string) or
+                    search_string.startswith(heb_name) or heb_name.startswith(search_string)) and \
                     (item["year"] == '' or
-                             year == '' or
-                                 (int(year) - 1) <= int(item["year"]) <= (int(year) + 1) or
-                                 (int(item["year"]) - 1) <= int(year) <= (int(item["year"]) + 1)):
-                filtered.append({"type": content_type, "name": eng_name, "slug": slug, "year": year})
-        log(__scriptname__, "filtered: %s" % filtered)
+                             result["year"] == '' or
+                                 (int(result["year"]) - 1) <= int(item["year"]) <= (int(result["year"]) + 1) or
+                                 (int(item["year"]) - 1) <= int(result["year"]) <= (int(item["year"]) + 1)):
+                filtered.append({"name": eng_name, "year": result["year"], "subs": result["subtitles"]})
         return filtered
 
     def _build_subtitle_list(self, search_results, item):
         ret = []
-        total_downloads = 0
         for result in search_results:
-            url = self.BASE_URL + "/he/cinemast/data/" + result["type"] + "/sb/" + result["slug"]
-            url += "/" + item["season"] + "/" + item["episode"] + "/" if result["type"] == "series" else "/"
-            subs_list = self.urlHandler.request(url)
+            total_downloads = 0
+            counter = 0
+            subs_list = result["subs"]
 
             if subs_list is not None:
-                subs_list = json.loads(subs_list, encoding="utf-8")
                 for language in subs_list.keys():
                     if xbmc.convertLanguage(language, xbmc.ISO_639_2) in item["3let_language"]:
-                        for translator_group in subs_list[language]:
-                            for quality in subs_list[language][translator_group]:
-                                for index in subs_list[language][translator_group][quality]:
-                                    current = subs_list[language][translator_group][quality][index]
-                                    title = current["subtitle_version"]
-                                    subtitle_rate = self._calc_rating(title, item["file_original_path"])
-                                    total_downloads += current["downloaded"]
-                                    ret.append(
-                                            {'lang_index': item["3let_language"].index(xbmc.convertLanguage(language, xbmc.ISO_639_2)),
-                                                'filename': title,
-                                                'link': current["key"],
-                                                'language_name': xbmc.convertLanguage(language, xbmc.ENGLISH_NAME),
-                                                'language_flag': language,
-                                                'id': current["id"],
-                                                'rating': str(current["downloaded"]),
-                                                'sync': subtitle_rate >= 3.8,
-                                                'hearing_imp': current["hearing_impaired"] > 0,
-                                                'is_preferred': xbmc.convertLanguage(language, xbmc.ISO_639_2) == item['preferredlanguage']
-                                            })
-        # Fix the rating
-        if total_downloads:
-            for it in ret:
-                it["rating"] = str(int(round(float(it["rating"]) / float(total_downloads), 1) * 5))
+                        for current in subs_list[language]:
+                            counter += 1
+                            title = current["version"]
+                            subtitle_rate = self._calc_rating(title, item["file_original_path"])
+                            total_downloads += int(current["downloads"])
+                            ret.append(
+                                {'lang_index': item["3let_language"].index(
+                                    xbmc.convertLanguage(language, xbmc.ISO_639_2)),
+                                    'filename': title,
+                                    'link': current["key"],
+                                    'language_name': xbmc.convertLanguage(language, xbmc.ENGLISH_NAME),
+                                    'language_flag': language,
+                                    'id': current["id"],
+                                    'rating': current["downloads"],
+                                    'sync': subtitle_rate >= 3.8,
+                                    'hearing_imp': False,
+                                    'is_preferred':
+                                        xbmc.convertLanguage(language, xbmc.ISO_639_2) == item[
+                                            'preferredlanguage']
+                                })
+            # Fix the rating
+            if total_downloads:
+                for it in ret[-1 * counter:]:
+                    it["rating"] = str(min(int(round(float(it["rating"]) / float(total_downloads), 1) * 8), 5))
 
         return sorted(ret, key=lambda x: (x['is_preferred'], x['lang_index'], x['sync'], x['rating']), reverse=True)
 
@@ -231,7 +232,7 @@ class SubscenterHelper:
         subsfile = re.sub(r'\W+', '.', subsfile).lower()
         file_name = re.sub(r'\W+', '.', file_name).lower()
         folder_name = re.sub(r'\W+', '.', folder_name).lower()
-        log(__scriptname__, "# Comparing Releases:\n [subtitle-rls] %s \n [filename-rls] %s \n [folder-rls] %s" % (
+        log("# Comparing Releases:\n [subtitle-rls] %s \n [filename-rls] %s \n [folder-rls] %s" % (
             subsfile, file_name, folder_name))
 
         subsfile = subsfile.split('.')
@@ -245,8 +246,7 @@ class SubscenterHelper:
             diff_folder = list(set(folder_name) - set(subsfile))
             rating = (1 - (len(diff_folder) / float(len(folder_name)))) * 5
 
-        log(__scriptname__,
-            "\n rating: %f (by %s)" % (round(rating, 1), "file" if len(file_name) > len(folder_name) else "folder"))
+        log("\n rating: %f (by %s)" % (round(rating, 1), "file" if len(file_name) > len(folder_name) else "folder"))
 
         return round(rating, 1)
 
@@ -258,9 +258,14 @@ class SubscenterHelper:
         xbmcvfs.mkdirs(__temp__)
 
         query = {"v": filename,
-                 "key": key}
-        url = self.BASE_URL + "/he/subtitle/download/" + language + "/" + str(id) + "/?" + urllib.urlencode(query)
-        f = self.urlHandler.request(url)
+                 "key": key,
+                 "sub_id": id}
+
+        user_token = self.get_user_token()
+
+        url = self.BASE_URL + "subtitle/download/" + language + "/?" + urllib.urlencode(query)
+
+        f = self.urlHandler.request(url, user_token)
 
         with open(zip_filename, "wb") as subFile:
             subFile.write(f)
@@ -268,6 +273,35 @@ class SubscenterHelper:
         xbmc.sleep(500)
 
         xbmc.executebuiltin(('XBMC.Extract("%s","%s")' % (zip_filename, __temp__,)).encode('utf-8'), True)
+
+    def login(self, isFromSettings=False):
+        email = __addon__.getSetting("Email")
+        password = __addon__.getSetting("Password")
+        post_data = {'username': email, 'password': password}
+        content = self.urlHandler.request(self.BASE_URL + "login/", post_data)
+
+        if content['result'] == 'success':
+            if isFromSettings:
+                notify(32010)
+
+            del content["result"]
+            return content
+        else:
+            notify(32009)
+            return None
+
+    def get_user_token(self, force_update=False):
+        if force_update:
+            store.delete('credentials')
+
+        results = store.get('credentials')
+        if results:
+            results = json.loads(results)
+        else:
+            results = self.login()
+            store.set('credentials', json.dumps(results))
+
+        return results
 
 
 class URLHandler():
@@ -278,20 +312,24 @@ class URLHandler():
                                   ('Pragma', 'no-cache'),
                                   ('Cache-Control', 'no-cache'),
                                   ('User-Agent',
-                                   'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0')]
+                                   'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36')]
 
-    def request(self, url, data=None, ajax=False, referer=None, cookie=None):
+    def request(self, url, data=None, query_string=None, ajax=False, referrer=None, cookie=None):
         if data is not None:
             data = urllib.urlencode(data)
+        if query_string is not None:
+            url += '?' + urllib.urlencode(query_string)
         if ajax:
             self.opener.addheaders += [('X-Requested-With', 'XMLHttpRequest')]
-        if referer is not None:
-            self.opener.addheaders += [('Referer', referer)]
+        if referrer is not None:
+            self.opener.addheaders += [('Referrer', referrer)]
         if cookie is not None:
             self.opener.addheaders += [('Cookie', cookie)]
 
         content = None
-        log(__scriptname__, "Getting url: %s" % (url))
+        log("Getting url: %s" % (url))
+        if data is not None and 'password' not in data:
+            log("Post Data: %s" % (data))
         try:
             response = self.opener.open(url, data)
             content = None if response.code != 200 else response.read()
@@ -302,8 +340,11 @@ class URLHandler():
                 except zlib.error:
                     pass
 
+            if response.headers.get('content-type', '') == 'application/json':
+                content = json.loads(content, encoding="utf-8")
+
             response.close()
         except Exception as e:
-            log(__scriptname__, "Failed to get url: %s\n%s" % (url, e))
+            log("Failed to get url: %s\n%s" % (url, e))
             # Second parameter is the filename
         return content
