75540e59200f625cb8b0b3f5476ebadc609014e8
robwebset <rob@deadlyduck.com>
2016-08-20 10:57:52 +0100

    [script.sleep] 1.1.1

--- script.sleep/default.py
+++ script.sleep/default.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 import xbmcgui
 
-# Import the common settings
-from resources.lib.settings import log
-
-
 #########################
 # Main
 #########################
 if __name__ == '__main__':
-    log("Sleep: Started")
 
-    # The service will be waiting for the variable to be set, and when set
-    # will display the sleep dialog
-    xbmcgui.Window(10000).setProperty("SleepPrompt", "true")
+    msg = 'The Sleep Addon has been removed from the Official Repo'
+    msg2 = 'Sleep is now located in the robwebset repository.'
+    msg3 = 'See the forum for more information'
+    makeRequest = xbmcgui.Dialog().ok('Sleep Has Moved', msg, msg2, msg3)
