e428ad9028c272f1451d141eb656647690e870a4
robwebset <rob@deadlyduck.com>
2016-08-20 08:31:22 +0100

    [script.game.filmwise] 1.0.4

--- script.game.filmwise/default.py
+++ script.game.filmwise/default.py
@@ -1,57 +1,12 @@
 # -*- coding: utf-8 -*-
-import xbmc
 import xbmcgui
-import xbmcaddon
-
-# Import the common settings
-from resources.lib.settings import log
-from resources.lib.settings import Settings
-
-from resources.lib.core import FilmWiseCore
-from resources.lib.viewer import FilmWiseViewer
-
-ADDON = xbmcaddon.Addon(id='script.game.filmwise')
-
 
 #########################
 # Main
 #########################
 if __name__ == '__main__':
-    log("FilmWise: Started")
-    xbmc.executebuiltin("ActivateWindow(busydialog)")
 
-    filmWise = FilmWiseCore()
-    # get the list of available quizzes
-    quizList = filmWise.getQuizList()
-
-    # Now the system has been loaded, we should update the last viewed setting
-    if len(quizList) > 0:
-        Settings.setLastViewed(quizList[0]['link'])
-
-    displayList = []
-
-    for quiz in quizList:
-        displayName = "%s %s" % (quiz['date'], quiz['name'])
-        displayList.append(displayName)
-
-    xbmc.executebuiltin("Dialog.Close(busydialog)")
-
-    # Show the list to the user
-    select = xbmcgui.Dialog().select(ADDON.getLocalizedString(32001), displayList)
-    if select < 0:
-        log("FilmWise: Cancelled by user")
-    else:
-        xbmc.executebuiltin("ActivateWindow(busydialog)")
-        quiz = quizList[select]
-        log("FilmWise: Selected quiz: %s (%s)" % (displayList[select], quiz['link']))
-
-        # Now get the details of the selected quiz
-        quizDetails = filmWise.getQuizData(quiz['link'])
-
-        viewer = FilmWiseViewer.createFilmWiseViewer(quiz['number'], quiz['name'], quizDetails, quiz['solution'])
-        xbmc.executebuiltin("Dialog.Close(busydialog)")
-
-        viewer.doModal()
-
-    del filmWise
-    log("FilmWise: Ended")
+    msg = 'The FilmWise Addon has been removed from the Official Repo'
+    msg2 = 'FilmWise is now located in the robwebset repository.'
+    msg3 = 'See the forum for more information'
+    makeRequest = xbmcgui.Dialog().ok('FilmWise Has Moved', msg, msg2, msg3)
