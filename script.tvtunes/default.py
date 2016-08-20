1dd88278fc1e2d1c6820347b1a80f5613f33c7f1
robwebset <rob@deadlyduck.com>
2016-08-20 11:17:09 +0100

    [script.tvtunes] 6.3.7

--- script.tvtunes/default.py
+++ script.tvtunes/default.py
@@ -1,42 +1,12 @@
 # -*- coding: utf-8 -*-
-import sys
-import xbmcaddon
-import xbmc
-
-# Import the common settings
-from resources.lib.settings import log
-from resources.lib.scraper import TvTunesScraper
-
-ADDON = xbmcaddon.Addon(id='script.tvtunes')
-
+import xbmcgui
 
 #########################
 # Main
 #########################
 if __name__ == '__main__':
-    log('script version %s started' % ADDON.getAddonInfo('version'))
 
-    try:
-        # parse sys.argv for params
-        try:
-            params = dict(arg.split("=") for arg in sys.argv[1].split("&"))
-        except:
-            params = dict(sys.argv[1].split("="))
-    except:
-        # no params passed
-        params = {}
-
-    log("params %s" % params)
-
-    if params.get("backend", False):
-        log("TvTunes: Ignoring manual call to run backend")
-
-    elif params.get("mode", False) == "solo":
-        themeScraper = TvTunesScraper()
-        del themeScraper
-    else:
-        # Close any open dialogs
-        xbmc.executebuiltin("Dialog.Close(all, true)", True)
-
-        log("TvTunes: Running as Addon/Plugin")
-        xbmc.executebuiltin("RunAddon(script.tvtunes)")
+    msg = 'The TvTunes Addon has been removed from the Official Repo'
+    msg2 = 'TvTunes is now located in the robwebset repository.'
+    msg3 = 'See the forum for more information'
+    makeRequest = xbmcgui.Dialog().ok('TvTunes Has Moved', msg, msg2, msg3)
--- script.tvtunes/service.py
+++ script.tvtunes/service.py
@@ -1,70 +1,13 @@
 # -*- coding: utf-8 -*-
-import os
-import xbmc
-import xbmcaddon
-
-# Import the common settings
-from resources.lib.settings import log
-from resources.lib.settings import Settings
-from resources.lib.settings import WindowShowing
-from resources.lib.backend import TunesBackend
-
-ADDON = xbmcaddon.Addon(id='script.tvtunes')
-CWD = ADDON.getAddonInfo('path').decode("utf-8")
-LIB_DIR = xbmc.translatePath(os.path.join(CWD, 'resources', 'lib').encode("utf-8")).decode("utf-8")
+import xbmcgui
 
 
-# Class to detect when something in the system has changed
-class TvTunesMonitor(xbmc.Monitor):
-    def onSettingsChanged(self):
-        log("TvTunesMonitor: Notification of settings change received")
-        Settings.reloadSettings()
-
-
-##################################
+###################################
 # Main of the TvTunes Service
-##################################
+###################################
 if __name__ == '__main__':
-    log("Starting TvTunes Service %s" % ADDON.getAddonInfo('version'))
 
-    # Make sure we have recorded this machines Id
-    Settings.setTvTunesId()
-
-    # Check if the settings mean we want to reset the volume on startup
-    startupVol = Settings.getStartupVolume()
-
-    if startupVol < 0:
-        log("TvTunesService: No Volume Change Required")
-    else:
-        log("TvTunesService: Setting volume to %s" % startupVol)
-        xbmc.executebuiltin('SetVolume(%d)' % startupVol, True)
-
-    # Check if the video info button should be hidden, we do this here as this will be
-    # called when the system is loaded, it can then be read by the skin
-    # when it comes to draw the button
-    WindowShowing.updateHideVideoInfoButton()
-    WindowShowing.updateShowOnContextMenu()
-
-    # Make sure the user wants to play themes
-    if Settings.isThemePlayingEnabled():
-        log("TvTunesService: Theme playing enabled")
-
-        if Settings.isUploadEnabled():
-            log("TvTunesService: Launching uploader")
-            xbmc.executebuiltin('RunScript(%s)' % os.path.join(LIB_DIR, "upload.py"), False)
-        else:
-            log("TvTunesService: Uploader not enabled")
-
-        # Create a monitor so we can reload the settings if they change
-        systemMonitor = TvTunesMonitor()
-
-        # Start looping to perform the TvTune theme operations
-        main = TunesBackend()
-
-        # Start the themes running
-        main.runAsAService()
-
-        del main
-        del systemMonitor
-    else:
-        log("TvTunesService: Theme playing disabled")
+    msg = 'The TvTunes Addon has been removed from the Official Repo'
+    msg2 = 'TvTunes is now located in the robwebset repository.'
+    msg3 = 'See the forum for more information'
+    makeRequest = xbmcgui.Dialog().ok('TvTunes Has Moved', msg, msg2, msg3)
