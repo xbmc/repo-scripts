1f5abcd17a26817b53a064418d90da038f87b96e
robwebset <rob@deadlyduck.com>
2016-08-20 11:10:57 +0100

    [script.suitability] 1.1.3

--- script.suitability/default.py
+++ script.suitability/default.py
@@ -1,62 +1,12 @@
 # -*- coding: utf-8 -*-
-import xbmc
-import xbmcaddon
 import xbmcgui
-
-# Import the common settings
-from resources.lib.settings import log
-from resources.lib.core import SuitabilityCore
-
-ADDON = xbmcaddon.Addon(id='script.suitability')
-
-
-def getIsTvShow():
-    if xbmc.getCondVisibility("Container.Content(tvshows)"):
-        return True
-    if xbmc.getCondVisibility("Container.Content(Seasons)"):
-        return True
-    if xbmc.getCondVisibility("Container.Content(Episodes)"):
-        return True
-    if xbmc.getInfoLabel("container.folderpath") == "videodb://tvshows/titles/":
-        return True  # TvShowTitles
-
-    return False
-
 
 #########################
 # Main
 #########################
 if __name__ == '__main__':
-    log("Suitability: Started")
 
-    videoName = None
-    isTvShow = getIsTvShow()
-
-    # First check to see if we have a TV Show of a Movie
-    if isTvShow:
-        videoName = xbmc.getInfoLabel("ListItem.TVShowTitle")
-
-    # If we do not have the title yet, get the default title
-    if videoName in [None, ""]:
-        videoName = xbmc.getInfoLabel("ListItem.Title")
-
-    # If there is no video name available prompt for it
-    if videoName in [None, ""]:
-        # Prompt the user for the new name
-        keyboard = xbmc.Keyboard('', ADDON.getLocalizedString(32032), False)
-        keyboard.doModal()
-
-        if keyboard.isConfirmed():
-            try:
-                videoName = keyboard.getText().decode("utf-8")
-            except:
-                videoName = keyboard.getText()
-
-    if videoName not in [None, ""]:
-        log("Suitability: Video detected %s" % videoName)
-        SuitabilityCore.runForVideo(videoName, isTvShow)
-    else:
-        log("Suitability: Failed to detect selected video")
-        xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32011))
-
-    log("Suitability: Ended")
+    msg = 'The Suitability Addon has been removed from the Official Repo'
+    msg2 = 'Suitability is now located in the robwebset repository.'
+    msg3 = 'See the forum for more information'
+    makeRequest = xbmcgui.Dialog().ok('Suitability Has Moved', msg, msg2, msg3)
--- script.suitability/service.py
+++ script.suitability/service.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 import xbmcgui
 
-# Import the common settings
-from resources.lib.settings import Settings
-from resources.lib.settings import log
-
 
 ###################################
 # Main of the Suitability Service
 ###################################
 if __name__ == '__main__':
-    log("Suitability: Starting service")
 
-    # Record if the Context menu should be displayed
-    if not Settings.showOnContextMenu():
-        log("Suitability: Hiding context menu")
-        xbmcgui.Window(10025).setProperty("SuitabilityHideContextMenu", "true")
-    else:
-        log("Suitability: Showing context menu")
-        xbmcgui.Window(10025).clearProperty("SuitabilityHideContextMenu")
+    msg = 'The Suitability Addon has been removed from the Official Repo'
+    msg2 = 'Suitability is now located in the robwebset repository.'
+    msg3 = 'See the forum for more information'
+    makeRequest = xbmcgui.Dialog().ok('Suitability Has Moved', msg, msg2, msg3)
