e72e5d5225062a9145975cc942e9ddcad947a813
phil65 <philipptemminghoff@gmx.de>
2016-08-31 21:25:26 +0200

    script.module.t9.search 1.1.0

---
+++ script.module.t9.search/lib/T9Utils.py
@@ -0,0 +1,185 @@
+# -*- coding: utf8 -*-
+
+# Copyright (C) 2015 - Philipp Temminghoff <phil65@kodi.tv>
+# This program is Free Software see LICENSE file for details
+
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcvfs
+import urllib2
+import os
+import time
+import hashlib
+import json
+import threading
+from functools import wraps
+
+ADDON = xbmcaddon.Addon()
+ADDON_ID = ADDON.getAddonInfo('id')
+ADDON_ICON = ADDON.getAddonInfo('icon')
+ADDON_NAME = ADDON.getAddonInfo('name')
+ADDON_PATH = ADDON.getAddonInfo('path').decode("utf-8")
+ADDON_VERSION = ADDON.getAddonInfo('version')
+ADDON_DATA_PATH = xbmc.translatePath("special://profile/addon_data/%s" % ADDON_ID).decode("utf-8")
+HOME = xbmcgui.Window(10000)
+SETTING = ADDON.getSetting
+
+
+def LANG(label_id):
+    if 31000 <= label_id <= 33000:
+        return ADDON.getLocalizedString(label_id)
+    else:
+        return xbmc.getLocalizedString(label_id)
+
+
+def run_async(func):
+    """
+    Decorator to run a function in a separate thread
+    """
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        func_hl = threading.Thread(target=func,
+                                   args=args,
+                                   kwargs=kwargs)
+        func_hl.start()
+        return func_hl
+
+    return async_func
+
+
+def get_http(url=None, headers=False):
+    """
+    fetches data from *url, returns it as a string
+    """
+    succeed = 0
+    if not headers:
+        headers = {'User-agent': 'XBMC/14.0 ( phil65@kodi.tv )'}
+    request = urllib2.Request(url)
+    for (key, value) in headers.iteritems():
+        request.add_header(key, value)
+    while (succeed < 2) and (not xbmc.abortRequested):
+        try:
+            response = urllib2.urlopen(request, timeout=3)
+            data = response.read()
+            return data
+        except Exception:
+            log("get_http: could not get data from %s" % url)
+            xbmc.sleep(1000)
+            succeed += 1
+    return None
+
+
+def get_JSON_response(url="", cache_days=7.0, folder=False, headers=False):
+    """
+    get JSON response for *url, makes use of prop and file cache.
+    """
+    now = time.time()
+    hashed_url = hashlib.md5(url).hexdigest()
+    if folder:
+        cache_path = xbmc.translatePath(os.path.join(ADDON_DATA_PATH, folder))
+    else:
+        cache_path = xbmc.translatePath(os.path.join(ADDON_DATA_PATH))
+    path = os.path.join(cache_path, hashed_url + ".txt")
+    cache_seconds = int(cache_days * 86400.0)
+    prop_time = HOME.getProperty(hashed_url + "_timestamp")
+    if prop_time and now - float(prop_time) < cache_seconds:
+        try:
+            prop = json.loads(HOME.getProperty(hashed_url))
+            log("prop load for %s. time: %f" % (url, time.time() - now))
+            if prop:
+                return prop
+        except Exception:
+            log("could not load prop data for %s" % url)
+    if xbmcvfs.exists(path) and ((now - os.path.getmtime(path)) < cache_seconds):
+        results = read_from_file(path)
+        log("loaded file for %s. time: %f" % (url, time.time() - now))
+    else:
+        response = get_http(url, headers)
+        try:
+            results = json.loads(response)
+            log("download %s. time: %f" % (url, time.time() - now))
+            save_to_file(results, hashed_url, cache_path)
+        except Exception:
+            log("Exception: Could not get new JSON data from %s. Tryin to fallback to cache" % url)
+            log(response)
+            if xbmcvfs.exists(path):
+                results = read_from_file(path)
+            else:
+                results = []
+    if results:
+        HOME.setProperty(hashed_url + "_timestamp", str(now))
+        HOME.setProperty(hashed_url, json.dumps(results))
+        return results
+    else:
+        return []
+
+
+def log(txt):
+    if isinstance(txt, str):
+        txt = txt.decode("utf-8", 'ignore')
+    message = u'%s: %s' % (ADDON_ID, txt)
+    xbmc.log(msg=message.encode("utf-8", 'ignore'),
+             level=xbmc.LOGDEBUG)
+
+
+def save_to_file(content, filename, path=""):
+    """
+    dump json and save to *filename in *path
+    """
+    if not xbmcvfs.exists(path):
+        xbmcvfs.mkdirs(path)
+    text_file_path = os.path.join(path, filename + ".txt")
+    now = time.time()
+    text_file = xbmcvfs.File(text_file_path, "w")
+    json.dump(content, text_file)
+    text_file.close()
+    log("saved textfile %s. Time: %f" % (text_file_path, time.time() - now))
+    return True
+
+
+def read_from_file(path="", raw=False):
+    """
+    return data from file with *path
+    """
+    if not xbmcvfs.exists(path):
+        return False
+    try:
+        with open(path) as f:
+            log("opened textfile %s." % (path))
+            if not raw:
+                result = json.load(f)
+            else:
+                result = f.read()
+        return result
+    except Exception:
+        log("failed to load textfile: " + path)
+        return False
+
+
+def reset_color(item):
+    label = item.getLabel2()
+    label = label.replace("[COLOR=FFFF3333]", "").replace("[/COLOR]", "")
+    item.setLabel2(label)
+
+
+def create_listitems(data=None):
+    if not data:
+        return []
+    itemlist = []
+    for (count, result) in enumerate(data):
+        listitem = xbmcgui.ListItem('%s' % (str(count)))
+        for (key, value) in result.iteritems():
+            if not value:
+                continue
+            value = unicode(value)
+            if key.lower() in ["name", "label"]:
+                listitem.setLabel(value)
+            elif key.lower() in ["label2"]:
+                listitem.setLabel2(value)
+            elif key.lower() in ["path"]:
+                listitem.setPath(path=value)
+            listitem.setProperty('%s' % (key), value)
+        listitem.setProperty("index", str(count))
+        itemlist.append(listitem)
+    return itemlist
