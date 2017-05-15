c4121bdf63a66cb2ff0fee2148599281546918c9
b-jesch <birger.jesch@gmail.com>
2017-05-15 08:53:02 +0200

    [service.kn.switchtimer] v2.0.3

--- service.kn.switchtimer/addtimer.py
+++ service.kn.switchtimer/addtimer.py
@@ -6,7 +6,7 @@ import handler
 
 if __name__ ==  '__main__':
 
-    handler.notifyLog('Parameter handler called: add Timer')
+    handler.notifyLog('Parameter handler called: add timer')
     args = {'channel': xbmc.getInfoLabel('ListItem.ChannelName'),
             'icon': xbmc.getInfoLabel('ListItem.Icon'),
             'date': xbmc.getInfoLabel('ListItem.Date'),
--- service.kn.switchtimer/deltimer.py
+++ service.kn.switchtimer/deltimer.py
@@ -5,5 +5,5 @@ import handler
 
 if __name__ ==  '__main__':
 
-    handler.notifyLog('Parameter handler called: Delete Timerlist')
+    handler.notifyLog('Parameter handler called: delete timerlist')
     handler.clearTimerProperties()
