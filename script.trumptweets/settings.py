6cd7363c254a063dacda980efa953d23da64d80c
Lunatixz <pseudotvlive@gmail.com>
2017-06-15 19:41:01 -0400

    [script.trumptweets] 1.0.2

---
+++ script.trumptweets/settings.py
@@ -0,0 +1,23 @@
+#   Copyright (C) 2017 Lunatixz
+#
+#
+# This file is part of Trump Tweets.
+#
+# Trump Tweets is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Trump Tweets is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Trump Tweets.  If not, see <http://www.gnu.org/licenses/>.
+
+import xbmcaddon
+
+# Plugin Info
+REAL_SETTINGS = xbmcaddon.Addon()
+REAL_SETTINGS.openSettings() 
