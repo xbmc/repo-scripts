db82b9252032432318be9d460acf7e24814196fa
Ichika <e@kleop.com>
2017-07-09 02:14:11 +0300

    [script.simkl] 2.0.0

--- script.simkl/service.py
+++ script.simkl/service.py
@@ -5,55 +5,28 @@ NOT LICENSED YET
 Creator: David Davó <david@ddavo.me>
 '''
 
-import sys, os
-import xbmcaddon
+import sys
 import xbmc
 
-from resources.lib import utils
-utils.systemLock("SimklTrackerRun")
-utils.systemUnlockDelay("SimklTrackerRun", 5)
+from resources.lib.utils import log
+from resources.lib.utils import system_lock
 
-from resources.lib import interface, engine
-from resources.lib import simklapi as API
-
-__addon__ = xbmcaddon.Addon()
-interface.__addon__ = __addon__
-autoscrobble = __addon__.getSetting("autoscrobble")
-def getstr(strid): return __addon__.getLocalizedString(strid)
-
-try:
-    compdatefile = os.path.join(__addon__.getAddonInfo("path").decode("utf-8"), "resources", "data", "compdate.txt")
-    with open(xbmc.translatePath(compdatefile), "r") as f:
-        __compdate__ = f.read()
-except:
-    __compdate__ = "ERROR: No such file or directory"
+from resources.lib import engine
+from resources.lib import api_simkl
+from resources.lib import events
 
 if __name__ == "__main__":
-    xbmc.log("Simkl dir: " + str(xbmc.translatePath("special://home")))
-    xbmc.log("Simkl | Python Version: " + str(sys.version))
-    xbmc.log("Simkl | "+ str(sys.argv), level=xbmc.LOGDEBUG)
-    xbmc.log("Simkl | compdate: {0}".format(__compdate__))
-    monitor = xbmc.Monitor()
+    system_lock("SimklTrackerRun", 5)
+    log("dir = " + str(xbmc.translatePath("special://home")))
+    log("Python Version = " + str(sys.version))
+    log("args = " + str(sys.argv))
 
-    player  = engine.Player()
-    player.addon = __addon__
-    eng     = engine.Engine(API.api, player)
-    #Remember: if getTime() is more than x% scrobble file
+    api = api_simkl.Simkl()
+    monitor = events.Monitor(api=api)
+    player = engine.Player(api=api)
 
-    #Testing:
-    #API.api.login()
-
-    if API.api.internet == False:
-        interface.notify(getstr(32027))
-    elif not API.api.is_user_logged():
-        API.api.login() #Add "remind me tomorrow button"
-        #interface.notify(getstr(32026))
-    else:
-        interface.notify(getstr(32025).format(API.api.USERSETTINGS["user"]["name"]))
-
-    #__addon__.openSettings()
     while not monitor.abortRequested():
         if monitor.waitForAbort(90):
             break
-        elif player.isPlaying():
-            player.onPlayBackStopped()
+
+sys.exit(0)
