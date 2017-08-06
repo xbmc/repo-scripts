e00134fa7716e13c29140b0abc70792500a32864
Kolja Lampe <razzeee@gmail.com>
2017-08-06 22:12:21 +0200

    [script.trakt] 3.1.12

---
+++ script.trakt/tests/test_kodiUtilities.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+
+import pytest
+import mock
+import sys
+
+xbmc_mock = mock.Mock()
+sys.modules['xbmc'] = xbmc_mock
+xbmcgui_mock = mock.Mock()
+sys.modules['xbmcgui'] = xbmcgui_mock
+xbmcaddon_mock = mock.Mock()
+sys.modules['xbmcaddon'] = xbmcaddon_mock
+from resources.lib import kodiUtilities
+
+
+def test_notification():
+    assert not xbmc_mock.executebuiltin.called
+    kodiUtilities.notification('header', 'message')
+    assert xbmc_mock.executebuiltin.called
+
+
+def test_showSettings():
+    assert not xbmcaddon_mock.Addon().openSettings.called
+    kodiUtilities.showSettings()
+    assert xbmcaddon_mock.Addon().openSettings.called
