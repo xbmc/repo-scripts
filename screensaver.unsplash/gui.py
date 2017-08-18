87d53465a55ff23ae92c07e46033733dd19c5a39
Lunatixz <Lunatixz@users.noreply.github.com>
2017-08-18 16:31:42 -0400

    [screensaver.unsplash] 1.0.0 (#586)

---
+++ screensaver.unsplash/gui.py
@@ -0,0 +1,82 @@
+#   Copyright (C) 2017 Lunatixz
+#
+#
+# This file is part of Unsplash Photo ScreenSaver.
+#
+# Unsplash Photo ScreenSaver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Unsplash Photo ScreenSaver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Unsplash Photo ScreenSaver.  If not, see <http://www.gnu.org/licenses/>.
+
+import urllib, urllib2, socket, random
+import xbmc, xbmcaddon, xbmcvfs, xbmcgui
+
+# Plugin Info
+ADDON_ID       = 'screensaver.unsplash'
+REAL_SETTINGS  = xbmcaddon.Addon(id=ADDON_ID)
+ADDON_NAME     = REAL_SETTINGS.getAddonInfo('name')
+ADDON_VERSION  = REAL_SETTINGS.getAddonInfo('version')
+ADDON_PATH     = (REAL_SETTINGS.getAddonInfo('path').decode('utf-8'))
+SETTINGS_LOC   = REAL_SETTINGS.getAddonInfo('profile').decode('utf-8')
+ENABLE_KEYS    = REAL_SETTINGS.getSetting("Enable_Keys") == 'true'
+KEYWORDS       = urllib.quote_plus(REAL_SETTINGS.getSetting("Keywords").encode("utf-8"))
+PHOTO_TYPE     = ['featured','random'][int(REAL_SETTINGS.getSetting("PhotoType"))]
+BASE_URL       = 'https://source.unsplash.com'
+URL_PARAMS     = '/%s/1920x1200'%PHOTO_TYPE
+KEY_PARAMS     = '/?%s'%KEYWORDS
+IMAGE_URL      = BASE_URL + URL_PARAMS + KEY_PARAMS if ENABLE_KEYS else BASE_URL + URL_PARAMS
+KODI_MONITOR   = xbmc.Monitor()
+TIMER          = [30,60,120,240][int(REAL_SETTINGS.getSetting("RotateTime"))]
+ANIMATION      = 'okay' if REAL_SETTINGS.getSetting("Animate") == 'true' else 'nope'
+TIME           = REAL_SETTINGS.getSetting("Time") == 'true'
+
+class GUI(xbmcgui.WindowXMLDialog):
+    def __init__( self, *args, **kwargs ):
+        self.isExiting = False
+        
+    def log(self, msg, level=xbmc.LOGDEBUG):
+        xbmc.log(ADDON_ID + '-' + ADDON_VERSION + '-' + msg, level)
+            
+            
+    def onInit( self ):
+        self.winid = xbmcgui.Window(xbmcgui.getCurrentWindowDialogId())
+        self.winid.setProperty('unsplash_animation', ANIMATION)
+        self.imageControl = self.getControl(30000)
+        self.timeControl  = self.getControl(30001)
+        self.timeControl.setVisible(TIME)
+        self.startRotation()
+        
+        
+    def startRotation(self):
+        while not KODI_MONITOR.abortRequested():
+            self.imageControl.setImage(self.openURL(IMAGE_URL))
+            if KODI_MONITOR.waitForAbort(TIMER) == True or self.isExiting == True:
+                break
+        
+        
+    def onAction( self, action ):
+        self.isExiting = True
+        self.close()
+        
+        
+    def openURL(self, url):
+        try:
+            request = urllib2.Request(url)
+            request.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
+            page = urllib2.urlopen(request, timeout = 15)
+            url = page.geturl()
+            self.log("openURL return url = " + url)
+            return url
+        except urllib2.URLError, e:
+            self.log("openURL Failed! " + str(e), xbmc.LOGERROR)
+        except socket.timeout, e:
+            self.log("openURL Failed! " + str(e), xbmc.LOGERROR)
+        return ''
