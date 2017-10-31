80b060b5b4ed423d10d373e37c2e8ba74dfcf68d
prof-membrane <pr0lckr@googlemail.com>
2017-10-31 22:38:58 +0100

    [script.module.libmediathek3] 1.5.0

--- script.module.libmediathek3/lib/libmediathek3.py
+++ script.module.libmediathek3/lib/libmediathek3.py
@@ -13,10 +13,72 @@ from libmediathek3premadedirs import *
 from libmediathek3dialogs import *
 	
 #translation = xbmcaddon.Addon(id='script.module.libmediathek3').getLocalizedString
+"""
+prefererdLang = ['de','en','fr']
+prefererdSub = ['de','en','fr']
+subtitleenabled = False
+subtitleenabled = True
+"""
 
+def _chooseBitrate(l):
+	bitrate = 0
+	url = False
+	streamType = False
+	for item in l:
+		if item.get('stream','').lower() == 'hls':#prefer hls
+			url = item['url']
+			streamType = 'HLS'
+			break
+		if item.get('stream','').lower() == 'dash':
+			url = item['url']
+			streamType = 'DASH'
+		if item.get('stream','').lower() == 'mp4' and item.get('bitrate',0) >= bitrate:
+			bitrate = item.get('bitrate',0)
+			url = item['url']
+			streamType = 'MP4'
+	listitem = xbmcgui.ListItem(path=url)
+	if streamType == 'DASH':
+		listitem.setProperty('inputstreamaddon', 'inputstream.adaptive')
+		listitem.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+	return listitem,url
 
 def play(d,external=False):
-	listitem = xbmcgui.ListItem(path=d['media'][0]['url'])
+	"""
+	if 'lang' in d['media'][0]:
+		url = _chooseBitrate(d['media'])
+		#""
+		languageFound = False
+		l = []
+		lsubtitles = []
+		for lang in prefererdLang:
+			for item in d['media']:
+				if lang == item['lang']:
+					if 'subtitlelang' in item:
+						lsubtitles.append(item)
+					else:
+						l.append(item)
+					languageFound = True
+			if languageFound:
+				log('###############')
+				if subtitleenabled and len(lsubtitles) > 0:
+					url = _chooseBitrate(lsubtitles)
+				elif len(l) == 0 and len(lsubtitles) > 0:
+					url = _chooseBitrate(lsubtitles)
+				elif lang != prefererdLang and len(lsubtitles) > 0:
+					url = _chooseBitrate(lsubtitles)
+				else:
+					url = _chooseBitrate(l)
+				break
+		#""
+		if not languageFound:
+			url = _chooseBitrate(d['media'])
+	else:
+		url = _chooseBitrate(d['media'])
+	"""	
+			
+	#listitem = xbmcgui.ListItem(path=url)
+	listitem,url = _chooseBitrate(d['media'])	
+	
 	subs = []
 	i = 0
 	if 'subtitle' in d:
@@ -34,13 +96,22 @@ def play(d,external=False):
 				log('Subtitle format not supported: ' + subtitle['type'])
 	
 	if 'metadata' in d:
+		ilabels = {}
 		if 'plot' in d['metadata']:
-			listitem.setInfo( type="Video", infoLabels={'Plot':d['metadata']['plot']})
-	
+			ilabels['Plot'] = d['metadata']['plot']
+		if 'name' in d['metadata']:
+			ilabels['Title'] = d['metadata']['name']
+		listitem.setInfo( type="Video", infoLabels=ilabels)
+		
+		art = {}
+		if 'thumb' in d['metadata']:
+			art['thumb'] = d['metadata']['thumb']
+		listitem.setArt(art)
 	listitem.setSubtitles(subs)
 	
+	
 	if external:
-		xbmc.Player().play(d['media'][0]['url'], listitem)
+		xbmc.Player().play(url, listitem)
 	else:
 		pluginhandle = int(sys.argv[1])
 		xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
--- script.module.libmediathek3/lib/libmediathek3utils.py
+++ script.module.libmediathek3/lib/libmediathek3utils.py
@@ -3,32 +3,53 @@ import urllib
 import urllib2
 import socket
 import xbmc
+import xbmcplugin
 import xbmcaddon
 import xbmcvfs
 import re
+import sys
 from StringIO import StringIO
 import gzip
 temp = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')+'temp').decode('utf-8')
 dict = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')+'dict.py').decode('utf-8')
 
+socket.setdefaulttimeout(30)
+
+try:
+    from urllib.request import Request, urlopen, build_opener, BaseHandler
+except ImportError:
+    from urllib2 import Request, urlopen, build_opener, BaseHandler
+
 
 def log(msg):
 	xbmc.log(msg)
 
-def getTranslation(id):
-	return xbmcaddon.Addon(id='script.module.libmediathek3').getLocalizedString(id)
+def getTranslation(id,addonid='script.module.libmediathek3'):
+	return xbmcaddon.Addon(id=addonid).getLocalizedString(id)
 	
-def getUrl(url,headers=False):
+def getUrl(url,headers=False,post=False):
+	log(url)
+	return _request(url,headers,post)
 	try:
-		return _request(url,headers)
+		return _request(url,headers,post)
 	except:#fast retry hack
-		return _request(url,headers)
-def _request(url,headers):
-	xbmc.log(url)
-	req = urllib2.Request(url)
+		return _request(url,headers,post)
+def _request(url,headers,post):
+	log(url)
+	if post:
+		#log(urllib.urlencode(post))
+		#log(urllib.quote(post))
+		#log(post)
+		#req = urllib2.Request(url,urllib.urlencode(post))
+		#req = urllib2.Request(url,urllib.quote(post))
+		req = urllib2.Request(url,post)
+	else:
+		req = urllib2.Request(url)
 	if headers:
 		for key in headers:
 			req.add_header(key, headers[key])
+		#req.add_header('Content-Type','application/json')
+		req.has_header = lambda header_name: (True if header_name == 'Content-Length' else Request.has_header(req, header_name))
 	else:
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0')
 		req.add_header('Accept-Encoding','gzip, deflate')
@@ -52,11 +73,16 @@ def clearString(s):
 	s = s.replace("&#43;","\"")
 	s = s.strip()
 	return s
+	
 def pathUserdata(path):
 	special = xbmcaddon.Addon().getAddonInfo('profile')+path
 	special = special.replace('//','/').replace('special:/','special://')
 	return special
-	#return xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('profile')+path).decode('utf-8')
+	
+def pathAddon(path):
+	special = xbmc.validatePath(xbmcaddon.Addon().getAddonInfo('path').replace('\\','/')+path.replace('\\','/'))
+	special = special.replace('//','/').replace('special:/','special://')
+	return special
 	
 def f_open(path):
 	try:
@@ -69,6 +95,7 @@ def f_open(path):
 
 def f_write(path,data):
 	try:
+		#f_mkdir(path)
 		f = xbmcvfs.File(path, 'w')
 		result = f.write(data)
 	except: pass
@@ -100,3 +127,10 @@ def searchWorkaroundExists():
 def searchWorkaroundRemove():
 	log('###Krypton workaround: removing lock...')
 	f_remove(pathUserdata('/search.lock'))
+
+def setSetting(k,v):
+	return xbmcplugin.setSetting(int(sys.argv[1]), k, v)
+	
+def getSetting(k):
+	return xbmcplugin.getSetting(int(sys.argv[1]), id=k)
+	
