1a90d5024f2a11426246a4ef0f70f086bdf7a8bd
Moritz Schappler (MSPC2011) <moritz-schappler@web.de>
2017-11-26 16:51:46 +0100

    [service.watchedlist] 1.2.1

---
+++ service.watchedlist/default.py
@@ -0,0 +1,29 @@
+"""
+This file is entry point for automatic start via Kodi
+"""
+
+import lib.watchedlist.utils as utils
+from lib.watchedlist.watchedlist import WatchedList
+import xbmc
+
+__remotedebug__ = False
+# Append pydev remote debugger
+if __remotedebug__:
+    utils.log("Initialize remote debugging.")
+    # Make pydev debugger works for auto reload.
+    try:
+        import pydevd
+        pydevd.settrace('localhost', port=60678, stdoutToServer=True, stderrToServer=True)
+    except ImportError:
+        sys.stderr.write("Error: " +
+            "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
+        utils.showNotification('WatchedList Error', 'remote debug could not be imported.')
+        sys.exit(1)
+    except:
+        utils.showNotification('WatchedList Error', 'remote debug in pydev is activated, but remote server not responding.')
+        sys.exit(1)
+
+# Run the program
+xbmc.sleep(1500) # wait 1.5 seconds to prevent import-errors
+utils.log("WatchedList Database Service starting...")
+WatchedList().runProgram()
