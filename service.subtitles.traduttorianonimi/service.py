9b60feca74b89a6195afd40afa974d52733398c3
ShellAddicted <ShellAddicted@gmail.com>
2018-04-17 19:52:07 +0200

    [service.subtitles.traduttorianonimi] 2.2.1

--- service.subtitles.traduttorianonimi/service.py
+++ service.subtitles.traduttorianonimi/service.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-#
-#  service.py
-#  
-#  Copyright 2017 ShellAddicted <shelladdicted@gmail.com<>
+#  Copyright 2018 ShellAddicted <shelladdicted@gmail.com>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,14 +15,14 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #  
-#
 __author__ = "ShellAddicted"
-__copyright__ = "Copyright 2017, ShellAddicted"
+__copyright__ = "Copyright 2018, ShellAddicted"
 __license__ = "GPL"
-__version__ = "2.1.1"
+__version__ = "2.2.1"
 __maintainer__ = "ShellAddicted"
 __email__ = "shelladdicted@gmail.com"
 __status__ = "Development"
+
 import xbmc
 import xbmcvfs
 import xbmcaddon
@@ -34,16 +31,21 @@ import xbmcplugin
 
 import os
 import sys
+import logging
+
 try:
     from urllib import unquote
 except (ImportError, AttributeError):
     from urllib.parse import unquote
 
+try:
+    import StringIO
+except ImportError:
+    from io import StringIO
+
 import requests
 import zipfile
-import StringIO
 import shutil
-import logging
 
 addon = xbmcaddon.Addon()
 cwd = xbmc.translatePath(addon.getAddonInfo("path")).decode("utf-8")
@@ -58,84 +60,60 @@ version = addon.getAddonInfo("version")
 language = addon.getLocalizedString
 main_url = "http://traduttorianonimi.it"
 
-NotifyLogo = os.path.join(cwd, "likelogo.png")
+NotifyLogo = xbmc.translatePath(os.path.join(cwd, "resources", "likelogo.png")).decode("utf-8")
 
 if not os.path.isfile(NotifyLogo):
     NotifyLogo = None
 
-HEADERS = {"user-agent": "Kodi-SubtitleService-TraduttoriAnonimi"}
-
-
 def notify(message, header=scriptname, icon=NotifyLogo, time=5000):
     dialog.notification(header, message, icon=icon, time=time)
 
+class KodiLogHandler(logging.StreamHandler):
+    _levels = {
+        logging.CRITICAL: xbmc.LOGFATAL,
+        logging.ERROR: xbmc.LOGERROR,
+        logging.WARNING: xbmc.LOGWARNING,
+        logging.INFO: xbmc.LOGINFO,
+        logging.DEBUG: xbmc.LOGDEBUG,
+        logging.NOTSET: xbmc.LOGNONE,
+    }
 
-class LogStream(object):
-    def write(self, data):
-        xbmc.log("*** [service.subtiles.traduttorianonimi] -> {0}".format(data.encode('utf-8', "ignore")),
-                 level=xbmc.LOGNOTICE)
+    def __init__(self):
+        logging.StreamHandler.__init__(self)
+        addon_id = xbmcaddon.Addon().getAddonInfo('id')
+        formatter = logging.Formatter(b'[{}]'.format(addon_id) + b'{%(levelname)s} %(name)s.%(funcName)s() -->> %(message)s')
+        self.setFormatter(formatter)
 
+    def emit(self, record):
+        try:
+            xbmc.log(self.format(record), self._levels[record.levelno])
+        except UnicodeEncodeError:
+            xbmc.log(self.format(record).encode(
+                'utf-8', 'ignore'), self._levels[record.levelno])
 
-log = logging.getLogger("TraduttoriAnonimi")
-log.setLevel(logging.DEBUG)
-style = logging.Formatter("{%(levelname)s} %(name)s.%(funcName)s() -->> %(message)s")
-consoleHandler = logging.StreamHandler(LogStream())
-consoleHandler.setFormatter(style)
-log.addHandler(consoleHandler)
+    def flush(self):pass
 
-
-def magicUnicode(data):
-    """
-    Return Unicode (utf-8) text encoded or exactly the input data (if already encoded)
-    :param data: data (text) to encode
-    :return:
-    """
-    # unicode is not defined in python3.x
-    if type(data) != bytes:
-        return str(data).encode("utf-8")
-    else:
-        return data
-
-
-def retriveURL(url, headers=HEADERS):
-    try:
-        log.debug("GET Request => HEADERS={0} ; URL={1}".format(headers, url))
-        q = requests.get(url, headers=headers)
-        log.debug("GET Request <= Response HEADERS={0}".format(q.headers))
-        return q
-    except:
-        log.error("An Error is occurred", exc_info=True)
-        notify(language(30001), time=3000)  # Network Error. Check your Connection
-        return None
-
-def overrideRetriveURL(self, url, headers=HEADERS):
-    return retriveURL(url, headers)
-
-def ovverideLogging(self):
-    self.log = log
-
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        KodiLogHandler()
+    ]
+)
+log = logging.getLogger("TraduttoriAnonimiKodiService")
 
 sys.path.append(resource)
 import TraduttoriAnonimi
 
-# Overwrite _retriveURL() with the retriveURL() to show Error Message (Notify) On Connection Error
-TraduttoriAnonimi.TraduttoriAnonimi._retriveURL = overrideRetriveURL
+core = TraduttoriAnonimi.TraduttoriAnonimi()
 
-# Overwrite _initLogger() to force to use the current logger (connected to kodi/xbmc logging system)
-TraduttoriAnonimi.TraduttoriAnonimi._initLogger = ovverideLogging
-
-# TraduttoriAnonimi.TraduttoriAnonimi._magicUnicode = magicUnicode
-
-
-def CleanJunk():
+def cleanup():
     # cleanup junk files
     for x in [profile, temp]:
         if xbmcvfs.exists(x):
             shutil.rmtree(x)
         xbmcvfs.mkdirs(x)
 
-
-def GetParams():
+def getParams():
     param = {}
     if len(sys.argv[2]) >= 2:
         for pairsofparam in sys.argv[2].replace("?", "").split("&"):
@@ -144,25 +122,22 @@ def GetParams():
                 param[tmp[0]] = tmp[1]
     return param
 
-def CheckSync(fnvideo,fnsub):
-    fnvideo=fnvideo.lower()
-    fnsub=fnsub.lower()
-    if "720p" in fnvideo and "hdtv" in fnvideo and "720p" in fnsub:
-        return True
+def checkSync(fnvideo, fnsub):
+    #TODO: implement this
+    return True
 
 def search(item):
-    CleanJunk()
+    cleanup()
     if "ita" in item["languages"]:
         if item["tvshow"]:
-            x = TraduttoriAnonimi.TraduttoriAnonimi()
-            results = x.getSubtitles(item["tvshow"], int(item["season"]), int(item["episode"]))
+            results = core.getSubtitles(item["tvshow"], int(item["season"]), int(item["episode"]))
             if results is not None:
                 for result in results:
                     subs = download(result["URL"])
                     for sub in subs:
                         # sub["Name"]
                         listitem = xbmcgui.ListItem(label='Italian', label2=os.path.basename(sub),thumbnailImage='it')
-                        listitem.setProperty('sync', 'true' if CheckSync(item["file_original_path"],os.path.basename(sub)) else 'false')
+                        listitem.setProperty('sync', 'true' if checkSync(item["file_original_path"],os.path.basename(sub)) else 'false')
                         xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),
                                                     url="plugin://{0}/?action=download&url={1}".format(scriptid, sub),
                                                     listitem=listitem, isFolder=False)  # sub["URL"]
@@ -170,7 +145,7 @@ def search(item):
             else:
                 log.info("NO RESULTS")
         else:
-            # The Subtitles are available only for TV Shows,MOVIES will be supported in future
+            # The Subtitles are available only for TV Shows, MOVIES will be supported in future
             notify(language(30002), time=18000)
             log.info('TraduttoriAnonimi only works with tv shows. Skipped')
     else:
@@ -181,14 +156,12 @@ def search(item):
 
 
 def download(url):
-    log.debug("Downloading => {0}".format(url))
     if os.path.isfile(url):
-        log.info("url is a local path")
         return [url]
     exts = ["srt"]
     out = []
 
-    r = retriveURL(url)
+    r = core._retriveURL(url)
     if r is not None:
         content = r.content
         tmp = StringIO.StringIO(content)  # "with" can't be used because StringIO has not __exit__
@@ -211,29 +184,21 @@ def download(url):
 
 
 def main():
-    log.info("Application version: {0}".format(version))
+    log.info("Traduttori Anonimi Kodi Service version: {0}".format(version))
     if xbmc.Player().isPlayingVideo():
-        params = GetParams()
+        params = getParams()
 
         if params["action"] == "search" or params["action"] == "manualsearch":
-            item = {"mansearch": params["action"] == "manualsearch"}
+            item = {}
 
-            if item['mansearch']:
-                notify(language(30004), time=7000)  # Manual Search is not supported yet, but it will be in the future.
+            if params["action"] == "manualsearch": #ignore manual search is not supported
+                notify(language(30004), time=5000)  # Manual Search is not supported yet, but it will be in the future.
 
-            # Season
             item["season"] = str(xbmc.getInfoLabel("VideoPlayer.Season"))
-            # Episode
             item["episode"] = str(xbmc.getInfoLabel("VideoPlayer.Episode")).zfill(2)
-            # Show Name
-            item["tvshow"] = magicUnicode(xbmc.getInfoLabel("VideoPlayer.TVshowtitle"))
-            # FullPath
+            item["tvshow"] = xbmc.getInfoLabel("VideoPlayer.TVshowtitle")
             item["file_original_path"] = xbmc.Player().getPlayingFile().decode("utf-8")
             item["languages"] = []
-
-            if "searchstring" in params:
-                item["mansearch"] = True
-                item["mansearchstr"] = unquote(params["searchstring"])
 
             for lang in unquote(params["languages"]).decode("utf-8").split(","):
                 item["languages"].append(xbmc.convertLanguage(lang, xbmc.ISO_639_2))
@@ -250,5 +215,5 @@ def main():
                 xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=sub, listitem=listitem, isFolder=False)
             xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
-
-main()
+if __name__ == "__main__":
+    main()
