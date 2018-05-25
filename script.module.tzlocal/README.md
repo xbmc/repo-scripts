783567496daeb31ad588eaf55c0d0bd432de6788
Lunatixz <Kgraer@gmail.com>
2018-05-24 22:51:05 -0400

    [script.module.tzlocal] 1.5.2

---
+++ script.module.tzlocal/README.md
@@ -0,0 +1,7 @@
+script.module.tzlocal
+======================
+
+Python tzlocal library packed for Kodi.
+
+See https://github.com/regebro/tzlocal.git
+
---
+++ script.module.tzlocal/lib/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '1.5.2.dev0'
+
+setup(name='tzlocal',
+      version=version,
+      description="tzinfo object for the local timezone",
+      long_description=open('README.rst', 'rt').read() + '\n\n' + open('CHANGES.txt', 'rt').read(),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: Unix',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='timezone pytz',
+      author='Lennart Regebro',
+      author_email='regebro@gmail.com',
+      url='https://github.com/regebro/tzlocal',
+      license="MIT",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'pytz',
+      ],
+      tests_require=[
+          'mock',
+      ],
+      test_suite='tzlocal.tests',
+      )
