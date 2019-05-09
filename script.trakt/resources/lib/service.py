99753d71be51aec43142f0a650934b4539ef3d31
Kolja Lampe <razzeee@gmail.com>
2019-05-09 21:10:27 +0200

    [script.trakt] 3.2.2

--- script.trakt/resources/lib/service.py
+++ script.trakt/resources/lib/service.py
@@ -2,17 +2,16 @@
 import threading
 import logging
 import xbmc
-from resources.lib import globals
-from resources.lib import sqlitequeue
-from resources.lib import utilities
-from resources.lib import kodiUtilities
 import time
 import xbmcgui
 import json
 import re
-import AddonSignals
 import urllib
 
+from resources.lib import globals
+from resources.lib import sqlitequeue
+from resources.lib import utilities
+from resources.lib import kodiUtilities
 from resources.lib.rating import rateMedia
 from resources.lib.scrobbler import Scrobbler
 from resources.lib.sync import Sync
@@ -106,8 +105,6 @@ class traktService:
 
         # init scrobbler class
         self.scrobbler = Scrobbler(globals.traktapi)
-
-        AddonSignals.registerSlot('service.nextup.notification', 'NEXTUPWATCHEDSIGNAL', self.callback)
 
         # start loop for events
         while not self.Monitor.abortRequested():
@@ -315,10 +312,6 @@ class traktService:
         self.syncThread = syncThread(manual, silent, library)
         self.syncThread.start()
 
-    def callback(self, data):
-        logger.debug('Callback received - Nextup skipped to the next episode')
-        self.scrobbler.playbackEnded()
-
 class syncThread(threading.Thread):
 
     _isManual = False
@@ -341,6 +334,14 @@ class traktMonitor(xbmc.Monitor):
         # xbmc.getCondVisibility('Library.IsScanningVideo') returns false when cleaning during update...
         self.scanning_video = False
         logger.debug("[traktMonitor] Initalized.")
+
+    def onNotification(self, sender, method, data):
+        if method.split('.')[1].upper() != 'NEXTUPWATCHEDSIGNAL': # method looks like Other.NEXTUPWATCHEDSIGNAL
+            return
+
+        logger.debug('Callback received - Upnext skipped to the next episode')
+        data = {'action': 'ended'}
+        self.action(data)
 
     # called when database gets updated and return video or music to indicate which DB has been changed
     def onScanFinished(self, database):
@@ -382,7 +383,7 @@ class traktPlayer(xbmc.Player):
             waitFor = 10
             waitedFor = 0
             # check each 10 seconds if we can abort or proceed
-            while not xbmc.abortRequested and scrobbleStartOffset > waitedFor:
+            while scrobbleStartOffset > waitedFor:
                 waitedFor += waitFor
                 time.sleep(waitFor)
                 if not self.isPlayingVideo():
@@ -549,7 +550,7 @@ class traktPlayer(xbmc.Player):
                                 return
                         foundShowAndEpInfo = splitLabel[0]
                         logger.debug("[traktPlayer] onAVStarted() - show plus episode info: %s" % foundShowAndEpInfo)
-                        splitShowAndEpInfo = re.split(' (s\d\de\d\d)? ?\((\d\d\d\d)\) ',foundShowAndEpInfo, 1)
+                        splitShowAndEpInfo = re.split(r' (s\d\de\d\d)? ?\((\d\d\d\d)\) ', foundShowAndEpInfo, 1)
                         logger.debug("[traktPlayer] onAVStarted() - Post-split of show plus episode info: %s " % splitShowAndEpInfo)
                         if len(splitShowAndEpInfo) != 4:
                             logger.debug("[traktPlayer] onAVStarted() - Show plus episode info doesn't have the ShowName sXXeYY (year) EpisodeName format that was expected. Giving up.")
@@ -603,8 +604,8 @@ class traktPlayer(xbmc.Player):
                             data['video_ids'] = showKeys
                             # For some reason, the Trakt search call returns the season and episode as an array in the pk field.
                             # You'd think individual episode and season fields would be better, but whatever.
-                            data['season'] = rightResp.pk[0];
-                            data['episode'] = rightResp.pk[1];
+                            data['season'] = rightResp.pk[0]
+                            data['episode'] = rightResp.pk[1]
                     # At this point if we haven't found the episode data yet, the episode-title-text-search method
                     # didn't work.
                     if (not data['season']):
@@ -631,7 +632,7 @@ class traktPlayer(xbmc.Player):
                                 if not epQueryResp:
                                     # Nothing returned. Giving up.
                                     logger.debug("[traktPlayer] onAVStarted() - No response received")
-                                    break;
+                                    break
                                 else:
                                     # Got the list back. Go through each season.
                                     logger.debug("[traktPlayer] onAVStarted() - Got response with seasons: %s" % str(epQueryResp))
@@ -656,7 +657,7 @@ class traktPlayer(xbmc.Player):
                                                 break
                                         # If we already found our data, no need to go through the rest of the seasons.
                                         if (data['season']):
-                                            break;
+                                            break
                     # Now we've done all we can.
                     if (data['season']):
                         # OK, that's everything. Data should be all set for scrobbling.
@@ -664,7 +665,7 @@ class traktPlayer(xbmc.Player):
                     else:
                         # Still no data? Too bad, have to give up.
                         logger.debug("[traktPlayer] onAVStarted() - Did our best, but couldn't get info for this show and episode. Skipping.")
-                        return;
+                        return
                 else:
                     logger.debug("[traktPlayer] onAVStarted() - Video type '%s' unrecognized, skipping." % self.type)
                     return
