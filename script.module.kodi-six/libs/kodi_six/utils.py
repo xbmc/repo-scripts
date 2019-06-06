60d81dd5f4412ec56e9c20cf6b3268b7056d7908
romanvm <roman1972@gmail.com>
2019-06-06 04:41:09 +0000

    [script.module.kodi-six] 0.0.4

--- script.module.kodi-six/libs/kodi_six/utils.py
+++ script.module.kodi-six/libs/kodi_six/utils.py
@@ -8,7 +8,14 @@ Utility functions for string conversion depending on Python version
 import sys
 import inspect
 
-__all__ = ['PY2', 'py2_encode', 'py2_decode', 'patch_module', 'encode_decode']
+__all__ = [
+    'PY2',
+    'py2_encode',
+    'py2_decode',
+    'encode_decode',
+    'patch_object',
+    'ModuleWrapper',
+]
 
 PY2 = sys.version_info[0] == 2  #: ``True`` for Python 2
 
@@ -49,7 +56,7 @@ def encode_decode(func):
     """
     if PY2:
         def wrapper(*args, **kwargs):
-            mod_args = tuple((py2_encode(item) for item in args))
+            mod_args = tuple(py2_encode(item) for item in args)
             mod_kwargs = {key: py2_encode(value) for key, value
                           in kwargs.iteritems()}
             return py2_decode(func(*mod_args, **mod_kwargs))
@@ -65,23 +72,44 @@ def _wrap_class(cls):
     return ClassWrapper
 
 
-def patch_module(mod):
+def patch_object(obj):
     """
-    Applies :func:`encode_decode` decorator to all
-    functions and classes in a module
+    Applies func:`encode_decode` decorator to an object
 
-    :param mod: module for patching
-    :type mod: types.ModuleType
+    :param obj: object for patching
+    :return: patched object
     """
-    for name, obj in inspect.getmembers(mod):
-        if inspect.isbuiltin(obj):
-            setattr(mod, name, encode_decode(obj))
-        elif inspect.isclass(obj):
-            # We cannot patch methods of Kodi classes directly.
-            cls = _wrap_class(obj)
-            for memb_name, member in inspect.getmembers(cls):
-                # Do not patch special methods!
-                if (inspect.ismethoddescriptor(member) and
-                        not memb_name.endswith('__')):
-                    setattr(cls, memb_name, encode_decode(member))
-            setattr(mod, name, cls)
+    if inspect.isbuiltin(obj):
+        obj = encode_decode(obj)
+    elif inspect.isclass(obj):
+        # We cannot patch methods of Kodi classes directly.
+        obj = _wrap_class(obj)
+        for memb_name, member in inspect.getmembers(obj):
+            # Do not patch special methods!
+            if (inspect.ismethoddescriptor(member) and
+                    not memb_name.endswith('__')):
+                setattr(obj, memb_name, encode_decode(member))
+    return obj
+
+
+class ModuleWrapper(object):
+    """
+    Implements lazy patching of Kodi Python API modules
+
+    This class applies func:`encode_decode` decorator to Kodi API functions
+    and classes on demand when a function or a class is first used.
+    """
+    def __init__(self, base_module):
+        self._base_module = base_module
+
+    def __getattr__(self, item):
+        if not hasattr(self._base_module, item):
+            raise AttributeError(
+                'Module {0} does not have attribute "{1}"!'.format(
+                    self._base_module, item
+                )
+            )
+        obj = getattr(self._base_module, item)
+        obj = patch_object(obj)
+        setattr(self, item, obj)
+        return obj
--- script.module.kodi-six/libs/kodi_six/xbmc.py
+++ script.module.kodi-six/libs/kodi_six/xbmc.py
@@ -7,8 +7,11 @@ General classes and functions for interacting with Kodi
 
 from __future__ import absolute_import
 import sys as _sys
-from xbmc import *
-from .utils import PY2 as _PY2, patch_module as _patch_module
+from .utils import PY2 as _PY2, ModuleWrapper as _ModuleWrapper
 
 if _PY2:
-    _patch_module(_sys.modules[__name__])
+    import xbmc as _xbmc
+    _wrapped_xbmc = _ModuleWrapper(_xbmc)
+    _sys.modules[__name__] = _wrapped_xbmc
+else:
+    from xbmc import *
