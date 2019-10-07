dd255bb6f2c036913c8f9d7d0e29631583915705
sualfred <su4lfred@gmail.com>
2019-10-07 09:44:35 +0200

    [script.embuary.helper] 1.2.28

--- script.embuary.helper/resources/lib/image.py
+++ script.embuary.helper/resources/lib/image.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python
-#Based on script.toolbox by phil65 - https://github.com/phil65/script.toolbox/
+# coding: utf-8
 
 #################################################################################################
+
 from __future__ import division
 
 import xbmc
 import xbmcaddon
 import xbmcvfs
-import hashlib
 import os
 from PIL import ImageFilter,Image,ImageOps
-
-''' Python 2<->3 compatibility
-'''
-try:
-    import urllib2 as urllib
-except ImportError:
-    import urllib.request as urllib
 
 from resources.lib.helper import *
 
@@ -29,176 +22,239 @@ OLD_IMAGE = ''
 #################################################################################################
 
 
+''' create imgage storage folders
+'''
 if not os.path.exists(ADDON_DATA_IMG_PATH):
-    log('Create missing image folder', force=True)
     os.makedirs(ADDON_DATA_IMG_PATH)
 
-
-def image_filter(prop='listitem',file=None,radius=BLUR_RADIUS):
-    global OLD_IMAGE
-    image = file if file is not None else xbmc.getInfoLabel('Control.GetLabel(%s)' % BLUR_CONTAINER)
-
-    try:
-        radius = int(radius)
-    except ValueError:
-        log('No valid radius defined for blurring')
-        return
-
-    if image:
-        if image == OLD_IMAGE:
-            log('Image blurring: Image has not changed. Skip %s.' % image, DEBUG)
-        else:
-            log('Image blurring: Image changed. Blur %s.' % image, DEBUG)
-            OLD_IMAGE = image
-            blurred_image, imagecolor = image_blur(image,radius)
-            winprop(prop + '_blurred', blurred_image)
-            winprop(prop + '_color', imagecolor)
-            winprop(prop + '_color_noalpha', imagecolor[2:])
+if not os.path.exists(ADDON_DATA_IMG_TEMP_PATH):
+    os.makedirs(ADDON_DATA_IMG_TEMP_PATH)
 
 
-def image_blur(image,radius):
-    md5 = hashlib.md5(image).hexdigest()
-    filename = md5 + str(radius) + '.png'
-    targetfile = os.path.join(ADDON_DATA_IMG_PATH, filename)
-    cachedthumb = xbmc.getCacheThumbName(image)
-    xbmc_vid_cache_file = os.path.join('special://profile/Thumbnails/Video', cachedthumb[0], cachedthumb)
-    xbmc_cache_file = os.path.join('special://profile/Thumbnails/', cachedthumb[0], cachedthumb[:-4] + '.jpg')
+''' blur image and store result in addon data folder
+'''
+class ImageBlur():
+    def __init__(self,prop='listitem',file=None,radius=None):
+        global OLD_IMAGE
+        self.image = file if file is not None else xbmc.getInfoLabel('Control.GetLabel(%s)' % BLUR_CONTAINER)
+        self.radius = int(radius) if radius is not None else int(BLUR_RADIUS)
 
-    if not xbmcvfs.exists(targetfile):
-        img = None
+        if self.image:
+            if self.image == OLD_IMAGE:
+                log('Image blurring: Image has not changed. Skip %s.' % self.image, DEBUG)
 
-        for i in range(1, 4):
-            try:
-                if xbmcvfs.exists(xbmc_cache_file):
-                    img = Image.open(xbmc.translatePath(xbmc_cache_file))
-                    break
-                elif xbmcvfs.exists(xbmc_vid_cache_file):
-                    img = Image.open(xbmc.translatePath(xbmc_vid_cache_file))
-                    break
-                else:
-                    image = urllib.unquote(image.replace('image://', ''))
-                    if image.endswith('/'):
-                        image = image[:-1]
-                    log('Copy image from source: ' + image, DEBUG)
-                    xbmcvfs.copy(image, targetfile)
-                    img = Image.open(targetfile)
-                    break
-            except Exception:
-                log('Could not get image for %s (try %i)' % (image, i))
-                xbmc.sleep(500)
+            else:
+                log('Image blurring: Image changed. Blur %s.' % self.image, DEBUG)
+                OLD_IMAGE = self.image
 
-        if not img:
-            return '', ''
+                self.filepath = self.blur()
+                self.avgcolor = self.color()
 
-        img.thumbnail((200, 200), Image.ANTIALIAS)
-        img = img.convert('RGB')
-        imgfilter = BlurImage(radius=radius)
-        img = img.filter(imgfilter)
-        img.save(targetfile)
+                winprop(prop + '_blurred', self.filepath)
+                winprop(prop + '_color', self.avgcolor)
+                winprop(prop + '_color_noalpha', self.avgcolor[2:])
 
-    else:
-        log('Blurred img already created: ' + targetfile, DEBUG)
-        img = Image.open(targetfile)
+    def __str__(self):
+        return self.filepath, self.avgcolor
 
-    imagecolor = get_colors(img)
+    def blur(self):
+        filename = md5hash(self.image) + str(self.radius) + '.png'
+        targetfile = os.path.join(ADDON_DATA_IMG_PATH, filename)
 
-    return targetfile, imagecolor
-
-
-def image_info(image):
-    cachedthumb = xbmc.getCacheThumbName(image)
-    xbmc_vid_cache_file = os.path.join('special://profile/Thumbnails/Video', cachedthumb[0], cachedthumb)
-    xbmc_cache_jpg_file = os.path.join('special://profile/Thumbnails/', cachedthumb[0], cachedthumb[:-4] + '.jpg')
-    xbmc_cache_png_file = os.path.join('special://profile/Thumbnails/', cachedthumb[0], cachedthumb[:-4] + '.png')
-
-    for i in range(1, 10):
         try:
-            if xbmcvfs.exists(xbmc_cache_jpg_file):
-                img = Image.open(xbmc.translatePath(xbmc_cache_jpg_file))
-            elif xbmcvfs.exists(xbmc_cache_png_file):
-                img = Image.open(xbmc.translatePath(xbmc_cache_png_file))
-            elif xbmcvfs.exists(xbmc_vid_cache_file):
-                img = Image.open(xbmc.translatePath(xbmc_vid_cache_file))
+            if xbmcvfs.exists(targetfile):
+                touch_file(targetfile)
+            else:
+                img = _openimage(self.image,ADDON_DATA_IMG_PATH,filename)
+                img.thumbnail((200, 200), Image.ANTIALIAS)
+                img = img.convert('RGB')
+                img = img.filter(ImageFilter.GaussianBlur(self.radius))
+                img.save(targetfile)
+
+            return targetfile
+
+        except Exception:
+            return ''
+
+    ''' get average image color
+    '''
+    def color(self):
+        imagecolor = 'FFF0F0F0'
+
+        try:
+            img = Image.open(self.filepath)
+            width, height = img.size
+            pixels = img.load()
+
+            data = []
+            for x in range(int(width / 2)):
+                for y in range(int(height / 2)):
+                    cpixel = pixels[x * 2, y * 2]
+                    data.append(cpixel)
+
+            counter, r, g, b = 0, 0, 0, 0
+            for x in range(len(data)):
+                brightness = data[x][0] + data[x][1] + data[x][2]
+                if brightness > 150 and brightness < 720:
+                    r += data[x][0]
+                    g += data[x][1]
+                    b += data[x][2]
+                    counter += 1
+
+            if counter > 0:
+                rAvg = int(r / counter)
+                gAvg = int(g / counter)
+                bAvg = int(b / counter)
+                Avg = int((rAvg + gAvg + bAvg) / 3)
+                minBrightness = 130
+
+                if Avg < minBrightness:
+                    Diff = minBrightness - Avg
+
+                    if rAvg <= (255 - Diff):
+                        rAvg += Diff
+                    else:
+                        rAvg = 255
+                    if gAvg <= (255 - Diff):
+                        gAvg += Diff
+                    else:
+                        gAvg = 255
+                    if bAvg <= (255 - Diff):
+                        bAvg += Diff
+                    else:
+                        bAvg = 255
+
+                imagecolor = 'FF%s%s%s' % (format(rAvg, '02x'), format(gAvg, '02x'), format(bAvg, '02x'))
+                log('Average color: ' + imagecolor, DEBUG)
+
             else:
                 raise Exception
 
-            width,height = img.size
-            ar = round(width / height,2)
-            return width,height,ar
+        except Exception as error:
+            log('Use fallback average color: ' + imagecolor, DEBUG)
+            pass
+
+        return imagecolor
+
+
+''' generate genre thumb and store result in addon data folder
+'''
+class CreateGenreThumb():
+    def __init__(self,genre,images):
+        self.images = images
+        self.genre = genre
+        self.filename = genre + '_' + md5hash(images) + '.jpg'
+        self.filepath = os.path.join(ADDON_DATA_IMG_PATH, self.filename)
+
+        if xbmcvfs.exists(self.filepath):
+            self.thumb = self.filepath
+            touch_file(self.filepath)
+        else:
+            self.temp_files = self.copy_files()
+            self.thumb = self.create_thumb()
+
+    def __str__(self):
+        return self.thumb
+
+    def copy_files(self):
+        ''' copy source posters to addon_data/img/tmp
+        '''
+        posters = list()
+        for poster in self.images:
+            posterfile = self.images.get(poster)
+            temp_filename = md5hash(posterfile) + '.jpg'
+            image = _openimage(posterfile,ADDON_DATA_IMG_TEMP_PATH,temp_filename)
+
+            if image:
+                posters.append(image)
+
+        return posters
+
+    def create_thumb(self):
+        ''' create collage with copied posteres
+        '''
+        width, height = 500, 750
+        cols, rows = 2, 2
+        thumbnail_width = int(width / cols)
+        thumbnail_height = int(height / rows)
+        size = thumbnail_width, thumbnail_height
+
+        try:
+            collage_images = []
+            for poster in self.temp_files:
+                image = ImageOps.fit(poster, (size), method=Image.ANTIALIAS, bleed=0.0, centering=(0.5, 0.5))
+                collage_images.append(image)
+
+            collage = Image.new('RGB', (width, height), (5,5,5))
+            i, x, y = 0, 0 ,0
+            for row in range(rows):
+                for col in range(cols):
+                    try:
+                        collage.paste(collage_images[i],(int(x), int(y)))
+                    except Exception:
+                        pass
+                    i += 1
+                    x += thumbnail_width
+                y += thumbnail_height
+                x = 0
+
+            collage.save(self.filepath,optimize=True,quality=75)
+
+            return self.filepath
 
         except Exception:
-            xbmc.sleep(500)
-
-    log('Image info: Could not get cached image for %s' % image)
-    return '','',''
+            return ''
 
 
-def get_colors(img):
-    width, height = img.size
-    imagecolor = 'FFF0F0F0'
-
+''' get image dimension and aspect ratio
+'''
+def image_info(image):
     try:
-        pixels = img.load()
+        filename = md5hash(image) + '.jpg'
+        img = _openimage(image,ADDON_DATA_IMG_TEMP_PATH,filename)
+        width,height = img.size
+        ar = round(width / height,2)
 
-        data = []
-        for x in range(width / 2):
-            for y in range(height / 2):
-                cpixel = pixels[x * 2, y * 2]
-                data.append(cpixel)
-
-        r = 0
-        g = 0
-        b = 0
-        counter = 0
-        for x in range(len(data)):
-            brightness = data[x][0] + data[x][1] + data[x][2]
-            if brightness > 150 and brightness < 720:
-                r += data[x][0]
-                g += data[x][1]
-                b += data[x][2]
-                counter += 1
-
-        if counter > 0:
-            rAvg = int(r / counter)
-            gAvg = int(g / counter)
-            bAvg = int(b / counter)
-            Avg = (rAvg + gAvg + bAvg) / 3
-            minBrightness = 130
-
-            if Avg < minBrightness:
-                Diff = minBrightness - Avg
-
-                if rAvg <= (255 - Diff):
-                    rAvg += Diff
-                else:
-                    rAvg = 255
-                if gAvg <= (255 - Diff):
-                    gAvg += Diff
-                else:
-                    gAvg = 255
-                if bAvg <= (255 - Diff):
-                    bAvg += Diff
-                else:
-                    bAvg = 255
-
-            imagecolor = 'FF%s%s%s' % (format(rAvg, '02x'), format(gAvg, '02x'), format(bAvg, '02x'))
-            log('average color: ' + imagecolor, DEBUG)
-
-        else:
-            raise Exception
+        return width, height, ar
 
     except Exception:
-        log('Use fallback average color: ' + imagecolor, DEBUG)
-        pass
-
-    return imagecolor
+        return '', '', ''
 
 
-class BlurImage(ImageFilter.Filter):
-    NAME = "GaussianBlur"
+''' get cached images or copy to temp if file has not been cached yet
+'''
+def _openimage(image,targetpath,filename):
+    cached_files = list()
+    cachedthumb = xbmc.getCacheThumbName(image)
+    cached_files.append(os.path.join('special://profile/Thumbnails/', cachedthumb[0], cachedthumb[:-4] + '.jpg'))
+    cached_files.append(os.path.join('special://profile/Thumbnails/', cachedthumb[0], cachedthumb[:-4] + '.png'))
+    cached_files.append(os.path.join('special://profile/Thumbnails/Video', cachedthumb[0], cachedthumb))
 
-    def __init__(self,radius):
-        self.radius = radius
+    for i in range(1, 4):
+        try:
+            for cache in cached_files:
+                if xbmcvfs.exists(cache):
+                    try:
+                        img = Image.open(xbmc.translatePath(cache))
+                        return img
+                    except Exception as error:
+                        log('Image error: Could not open cached image --> %s' % error, WARNING)
+                        pass
 
-    def filter(self,image):
-        return image.gaussian_blur(self.radius)
+            targetfile = os.path.join(targetpath, filename)
+            if not xbmcvfs.exists(targetfile):
+                image = url_unquote(image.replace('image://', ''))
+                if image.endswith('/'):
+                    image = image[:-1]
+                xbmcvfs.copy(image, targetfile)
+
+            img = Image.open(targetfile)
+
+            return img
+
+        except Exception as error:
+            log('Image error: Could not get image for %s (try %d) -> %s' % (image, i, error), ERROR)
+            xbmc.sleep(500)
+            pass
+
+    return ''
--- script.embuary.helper/resources/lib/plugin_listing.py
+++ script.embuary.helper/resources/lib/plugin_listing.py
@@ -18,109 +18,149 @@ from resources.lib.helper import *
 
 ########################
 
-index = [
-        {'name': ADDON.getLocalizedString(32011), 'action': 'folder', 'type': 'mixed'},
-        {'name': xbmc.getLocalizedString(342), 'action': 'folder', 'type': 'movies'},
-        {'name': xbmc.getLocalizedString(20343), 'action': 'folder', 'type': 'tvshows'}
-        ]
-
-mixed = [
-        {'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
-        {'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'}
-        ]
-
-movies = [
-        {'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
-        {'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
-        {'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
-        {'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
-        {'name': xbmc.getLocalizedString(135), 'action': 'getgenre'}
-        ]
-
-tvshows = [
-        {'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
-        {'name': ADDON.getLocalizedString(32008), 'action': 'getnextup'},
-        {'name': ADDON.getLocalizedString(32015), 'action': 'getnewshows'},
-        {'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows', 'showall': 'true'},
-        {'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
-        {'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
-        {'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
-        {'name': xbmc.getLocalizedString(135), 'action': 'getgenre'}
-        ]
-
-emby_movies = [
-        {'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
-        {'name': xbmc.getLocalizedString(20382), 'action': 'getbyargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort': '{"order": "descending", "method": "dateadded"}'},
-        {'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
-        {'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
-        {'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
-        {'name': ADDON.getLocalizedString(32012), 'action': 'getbyargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}'},
-        {'name': xbmc.getLocalizedString(16101), 'action': 'getbyargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
-        {'name': xbmc.getLocalizedString(590), 'action': 'getbyargs', 'sort': '{"method": "random"}', 'limit': '50'},
-        ]
-
-emby_tvshows = [
-        {'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
-        {'name': ADDON.getLocalizedString(32008), 'action': 'getnextup'},
-        {'name': ADDON.getLocalizedString(32015), 'action': 'getnewshows'},
-        {'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows', 'showall': 'true'},
-        {'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
-        {'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
-        {'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
-        {'name': ADDON.getLocalizedString(32012), 'action': 'getbyargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}'},
-        {'name': xbmc.getLocalizedString(16101), 'action': 'getrbyargs', 'filter': '{"field": "numwatched", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
-        {'name': xbmc.getLocalizedString(590), 'action': 'getbyargs', 'sort': '{"method": "random"}', 'limit': '50'},
-        ]
+LISTING = {
+          'index': [
+              {'name': ADDON.getLocalizedString(32011), 'browse': 'folder', 'content': 'mixed'},
+              {'name': xbmc.getLocalizedString(342), 'browse': 'folder', 'content': 'movie'},
+              {'name': xbmc.getLocalizedString(20343), 'browse': 'folder', 'content': 'tvshow'}
+              ],
+          'mixed': [
+              {'name': ADDON.getLocalizedString(32013), 'info': 'getinprogress'},
+              {'name': ADDON.getLocalizedString(32009), 'info': 'getbygenre'}
+              ],
+          'movie': [
+              {'name': ADDON.getLocalizedString(32013), 'info': 'getinprogress'},
+              {'name': xbmc.getLocalizedString(20382), 'info': 'getbyargs', 'filter_args': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort_args': '{"order": "descending", "method": "dateadded"}'},
+              {'name': ADDON.getLocalizedString(32007), 'info': 'getsimilar'},
+              {'name': ADDON.getLocalizedString(32014), 'info': 'getsimilar', 'pos': '0'},
+              {'name': ADDON.getLocalizedString(32009), 'info': 'getbygenre'},
+              {'name': xbmc.getLocalizedString(135), 'info': 'getgenre'},
+              {'name': ADDON.getLocalizedString(32012), 'info': 'getbyargs', 'limit': '50', 'sort_args': '{"order": "descending", "method": "rating"}'},
+              {'name': xbmc.getLocalizedString(16101), 'info': 'getbyargs', 'filter_args': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'sort_args': '{"order": "ascending", "method": "title"}'},
+              {'name': xbmc.getLocalizedString(590), 'info': 'getbyargs', 'sort_args': '{"method": "random"}', 'limit': '50'}
+              ],
+          'tvshow': [
+              {'name': ADDON.getLocalizedString(32013), 'info': 'getinprogress'},
+              {'name': ADDON.getLocalizedString(32008), 'info': 'getnextup'},
+              {'name': ADDON.getLocalizedString(32015), 'info': 'getnewshows'},
+              {'name': ADDON.getLocalizedString(32010), 'info': 'getnewshows', 'showall': 'true'},
+              {'name': ADDON.getLocalizedString(32007), 'info': 'getsimilar'},
+              {'name': ADDON.getLocalizedString(32014), 'info': 'getsimilar', 'pos': '0'},
+              {'name': ADDON.getLocalizedString(32009), 'info': 'getbygenre'},
+              {'name': xbmc.getLocalizedString(135), 'info': 'getgenre'},
+              {'name': ADDON.getLocalizedString(32012), 'info': 'getbyargs', 'limit': '50', 'sort_args': '{"order": "descending", "method": "rating"}'},
+              {'name': xbmc.getLocalizedString(16101), 'info': 'getbyargs', 'filter_args': '{"field": "numwatched", "operator": "lessthan", "value": "1"}', 'sort_args': '{"order": "ascending", "method": "title"}'},
+              {'name': xbmc.getLocalizedString(590), 'info': 'getbyargs', 'sort_args': '{"method": "random"}', 'limit': '50'}
+              ]
+          }
 
 ########################
 
 class PluginListing(object):
-
     def __init__(self,params,li):
         self.li = li
-        self.folder = params.get('folder','')
-        self.tag = params.get('tag','')
-        self.cat_type = None
+        self.folder = params.get('folder')
+        self.browse = params.get('browse')
+        self.tag = params.get('tag')
+        self.available_tags = params.get('available_tags')
 
-        if self.folder:
-            if 'tvshows' in self.folder:
-                self.cat_type = 'tvshow'
-            elif 'movies' in self.folder:
-                self.cat_type = 'movie'
-
-            self.widgets()
-
+        if self.browse == 'widgets':
+            self.list_widgets()
+        elif self.browse == 'folder':
+            self.list_folder()
         else:
-            self.folders()
+            self.list_index()
 
 
-    def folders(self):
-        for folder in index:
-            url = self._encode_url(folder=folder['type'], plugincat=encode_string(folder['name']))
-            self._add_item(folder['name'],url)
+    def list_index(self):
+        tags_movies = self._get_tags('movie')
+        tags_tvshows = self._get_tags('tvshow')
 
-        if visible('System.HasAddon(plugin.video.emby'):
-            i = 0
-            for prop in range(30):
-                tag = winprop('emby.wnodes.%s.cleantitle' % i)
-                database = winprop('emby.wnodes.%s.type' % i)
+        for item in LISTING['index']:
+            content = item.get('content')
+            browse = 'widgets'
+            folder = None
+            available_tags = None
 
-                if database == 'movies' or database == 'tvshows':
-                    label = 'Emby: %s' % winprop('emby.wnodes.%s.title' % i)
-                    url = self._encode_url(folder='emby_%s' % database,tag=tag, plugincat=encode_string(label))
-                    self._add_item(label,url)
+            if content == 'movie' and tags_movies:
+                browse = 'folder'
+                available_tags = tags_movies
 
-                i += 1
+            elif content == 'tvshow' and tags_tvshows:
+                browse = 'folder'
+                available_tags = tags_tvshows
 
-
-    def widgets(self):
-        for widget in globals()[self.folder]:
-            url = self._get_url(widget)
-            self._add_item(widget['name'],url)
+            elif content == 'mixed' and (tags_movies or tags_tvshows):
+                browse = 'folder'
+                available_tags = tags_movies + tags_tvshows
 
 
-    def _get_url(self,widget):
-        return self._encode_url(info=widget['action'], type=self.cat_type, tag=self.tag, pos=widget.get('pos',''), filter_args=widget.get('filter',''), sort_args=widget.get('sort',''), limit=widget.get('limit',''), showall=widget.get('showall',''), plugincat=encode_string(widget['name']))
+            url = self._encode_url(browse=browse,
+                                   folder=content,
+                                   available_tags=available_tags,
+                                   plugincat=encode_string(item['name'])
+                                   )
+
+            self._add_item(item['name'],url)
+
+
+    def list_folder(self):
+        folders = self._generate_subfolder()
+
+        for item in folders:
+            url = self._encode_url(browse='widgets',
+                                   folder=self.folder,
+                                   tag=item.get('tag'),
+                                   plugincat=encode_string(item['name'])
+                                   )
+
+            self._add_item(item['name'],url)
+
+
+    def list_widgets(self):
+        content_type = self.folder if self.folder in ['tvshow','movie'] else None
+
+        for item in LISTING[self.folder]:
+            url = self._encode_url(info=item.get('info'),
+                                   type=content_type,
+                                   tag=self.tag,
+                                   pos=item.get('pos'),
+                                   filter_args=item.get('filter_args'),
+                                   sort_args=item.get('sort_args'),
+                                   limit=item.get('limit'),
+                                   showall=item.get('showall'),
+                                   plugincat=encode_string(item.get('name'))
+                                   )
+
+            self._add_item(item['name'],url)
+
+
+    def _generate_subfolder(self):
+        items = [{'name': ADDON.getLocalizedString(32022), 'browse': 'widgets'}]
+
+        duplicate_handler = []
+        for item in eval(self.available_tags):
+            if item not in duplicate_handler:
+                duplicate_handler.append(item)
+                items.append({'name': '"' + item + '" ' + ADDON.getLocalizedString(32023), 'browse': 'widgets', 'tag': item})
+
+        return items
+
+
+    def _get_tags(self,library):
+        tags = []
+        json_query = json_call('VideoLibrary.GetTags',
+                                properties=['title'],
+                                params={'type': library}
+                                )
+
+        try:
+            for tag in json_query['result']['tags']:
+                tags.append(tag.get('label'))
+        except KeyError:
+            pass
+
+        return tags
 
 
     def _encode_url(self,**kwargs):
@@ -132,12 +172,7 @@ class PluginListing(object):
 
 
     def _add_item(self,label,url):
-        icon = 'special://home/addons/script.embuary.helper/resources/icon.png'
-        source = self.folder or url
-
-        if 'emby_' in source:
-            icon = 'special://home/addons/plugin.video.emby/icon.png'
-
+        icon = 'special://home/addons/' + ADDON_ID + '/resources/icon.png'
         list_item = xbmcgui.ListItem(label=label)
         list_item.setInfo('video', {'title': label, 'mediatype': 'video'})
         list_item.setArt({'icon': 'DefaultFolder.png','thumb': icon})
