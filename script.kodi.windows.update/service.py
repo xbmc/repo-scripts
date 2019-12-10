bb07c1ae5fcd569b588013d66ba9e2b169122e7d
Lunatixz <Kgraer@gmail.com>
2019-12-10 14:00:08 -0500

    [script.kodi.windows.update] 1.1.1

--- script.kodi.windows.update/service.py
+++ script.kodi.windows.update/service.py
@@ -1,4 +1,4 @@
-#     Copyright (C) 2019 Team-Kodi
+#     Copyright (C) 2020 Team-Kodi
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 #
 # -*- coding: utf-8 -*-
 
-import platform, traceback
+import platform, traceback, json, sys
 import xbmc, xbmcaddon, xbmcgui, xbmcvfs
 
 # Plugin Info
@@ -30,6 +30,7 @@ LANGUAGE      = REAL_SETTINGS.getLocalizedString
 DEBUG         = REAL_SETTINGS.getSetting('Enable_Debugging') == 'true'
 CLEAN         = REAL_SETTINGS.getSetting('Disable_Maintenance') == 'false'
 CACHE         = REAL_SETTINGS.getSetting('Disable_Cache') == 'false'
+VER_QUERY     = '{"jsonrpc":"2.0","method":"Application.GetProperties","params":{"properties":["version"]},"id":1}'
 
 def log(msg, level=xbmc.LOGDEBUG):
     if DEBUG == False and level != xbmc.LOGERROR: return
@@ -38,38 +39,53 @@ def log(msg, level=xbmc.LOGDEBUG):
 
 class Service(object):
     def __init__(self):
-        try:
-            self.getBuild()
-            self.getVersion()
-            self.lastPath = REAL_SETTINGS.getSetting("LastPath") # CACHE = Keep last download, CLEAN = Remove all downloads
-            if not CACHE and CLEAN and xbmcvfs.exists(self.lastPath):
-                log('Service, removing last download')
-                xbmcvfs.delete(self.lastPath)
-                xbmcgui.Dialog().notification(ADDON_NAME, LANGUAGE(30007), ICON, 4000)
-        except Exception as e: log("Service Failed! " + str(e), xbmc.LOGERROR)
-                
+        self.myMonitor = xbmc.Monitor()
+        self.setSettings()
+        lastPath = REAL_SETTINGS.getSetting("LastPath") # CACHE = Keep last download, CLEAN = Remove all downloads
+        if not CACHE and CLEAN and xbmcvfs.exists(lastPath): self.deleteLast(lastPath)
         
-    def getBuild(self): 
+                
+    def deleteLast(self, lastPath):
+        log('deleteLast')
+        #some file systems don't release the file lock instantly.
+        for count in range(3):
+            if self.myMonitor.waitForAbort(1): return 
+            try: 
+                if xbmcvfs.delete(lastPath): return
+            except: pass
+                
+
+    def setSettings(self):
+        log('setSettings')
+        [func() for func in [self.getBuild,self.getPlatform,self.getVersion]]
+              
+              
+    def getBuild(self):
+        log('getBuild')
+        REAL_SETTINGS.setSetting("Build",json.dumps(json.loads(xbmc.executeJSONRPC(VER_QUERY) or '').get('result',{}).get('version',{})))
+        
+        
+    def getPlatform(self): 
+        log('getPlatform')
         count = 0
-        build = ''
         try:
-            while not xbmc.Monitor().abortRequested() and count < 3:
-                count += 1
-                build = platform.machine()
-                if len(str(build)) > 0: return REAL_SETTINGS.setSetting("Platform",str(build))
-        except Exception as e: log("getBuild Failed! " + str(e), xbmc.LOGERROR)
-             
-             
-    def getVersion(self):
-        count = 0
-        build = ''
-        try:
-            while not xbmc.Monitor().abortRequested() and count < 3:
+            while not self.myMonitor.abortRequested() and count < 15:
                 count += 1 
-                build = xbmc.getInfoLabel('System.OSVersionInfo')
-                if build.lower() != 'busy': return REAL_SETTINGS.setSetting("Version",str(build))
-                if xbmc.Monitor().waitForAbort(1): return
+                if self.myMonitor.waitForAbort(1): return
+                build = platform.machine()
+                if len(build) > 0: return REAL_SETTINGS.setSetting("Platform",build)
         except Exception as e: log("getVersion Failed! " + str(e), xbmc.LOGERROR)
         
+        
+    def getVersion(self):
+        log('getVersion')
+        count = 0
+        try:
+            while not self.myMonitor.abortRequested() and count < 15:
+                count += 1 
+                if self.myMonitor.waitForAbort(1): return
+                build = (xbmc.getInfoLabel('System.OSVersionInfo') or 'busy')
+                if build.lower() != 'busy': return REAL_SETTINGS.setSetting("Version",str(build))
+        except Exception as e: log("getVersion Failed! " + str(e), xbmc.LOGERROR)
               
 if __name__ == '__main__': Service()
