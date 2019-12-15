19521ff23d31a520e4e7403bb64b769dd00bf670
Rick Phillips <ruuk@kodi.tv>
2019-12-14 17:52:57 -0800

    [script.module.addon.signals] 0.0.5

--- script.module.addon.signals/README.md
+++ script.module.addon.signals/README.md
@@ -1,4 +1,10 @@
+[![GitHub release](https://img.shields.io/github/release/ruuk/script.module.addon.signals.svg)](https://github.com/ruuk/script.module.addon.signals/releases)
+[![Build Status](https://travis-ci.org/ruuk/script.module.addon.signals.svg?branch=master)](https://travis-ci.org/ruuk/script.module.addon.signals)
+[![License: LGPL-2.1](https://img.shields.io/badge/license-lgpl__2__1-blue)](https://opensource.org/licenses/LGPL-2.1)
+[![Contributors](https://img.shields.io/github/contributors/ruuk/script.module.addon.signals.svg)](https://github.com/ruuk/script.module.addon.signals/graphs/contributors)
+
 # script.module.addon.signals
+
 Provides signal/slot mechanism for inter-addon communication in Kodi
 
 ```python
@@ -7,7 +13,7 @@ import AddonSignals
 ​
 def callback(data):
 	# Do something with data
-	
+
 AddonSignals.registerSlot('sender.addon.id', 'signal_name', callback)
 ​
 ​
--- script.module.addon.signals/lib/AddonSignals.py
+++ script.module.addon.signals/lib/AddonSignals.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
+
 import sys
-import xbmc
-import xbmcaddon
 import base64
 import json
+import xbmc
+import xbmcaddon
 
 RECEIVER = None
 
 
 def _getReceiver():
-    global RECEIVER
+    global RECEIVER  # pylint: disable=global-statement
     if not RECEIVER:
         RECEIVER = SignalReceiver()
     return RECEIVER
 
 
 def _decodeData(data):
-    data = json.loads(data)
-    if data:
-        return json.loads(base64.b64decode(data[0]))
+    encoded_data = json.loads(data)
+    if encoded_data:
+        json_data = base64.b64decode(encoded_data[0])
+        # NOTE: With Python 3.5 and older json.loads() does not support bytes or bytearray
+        if isinstance(json_data, bytes):
+            json_data = json_data.decode('utf-8')
+        return json.loads(json_data)
+
+    return None
 
 
 def _encodeData(data):
@@ -28,11 +35,20 @@ def _encodeData(data):
     encoded_data = base64.b64encode(json_data)
     if sys.version_info[0] > 2:
         encoded_data = encoded_data.decode('ascii')
-    return '\\"[\\"{0}\\"]\\"'.format(encoded_data)
+    return encoded_data
+
+
+def _jsonrpc(**kwargs):
+    ''' Perform JSONRPC calls '''
+    if 'id' not in kwargs:
+        kwargs.update(id=1)
+    if 'jsonrpc' not in kwargs:
+        kwargs.update(jsonrpc='2.0')
+    return json.loads(xbmc.executeJSONRPC(json.dumps(kwargs)))
 
 
 class SignalReceiver(xbmc.Monitor):
-    def __init__(self):
+    def __init__(self):  # pylint: disable=super-init-not-called
         self._slots = {}
 
     def registerSlot(self, signaler_id, signal, callback):
@@ -99,8 +115,12 @@ def sendSignal(signal, data=None, source_id=None, sourceID=None):
     if sourceID:
         xbmc.log('++++==== script.module.addon.signals: sourceID keyword is DEPRECATED - use source_id ====++++', xbmc.LOGNOTICE)
     source_id = source_id or sourceID or xbmcaddon.Addon().getAddonInfo('id')
-    command = 'XBMC.NotifyAll({0}.SIGNAL,{1},{2})'.format(source_id, signal,_encodeData(data))
-    xbmc.executebuiltin(command)
+
+    _jsonrpc(method='JSONRPC.NotifyAll', params=dict(
+        sender='%s.SIGNAL' % source_id,
+        message=signal,
+        data=[_encodeData(data)],
+    ))
 
 
 def registerCall(signaler_id, signal, callback):
