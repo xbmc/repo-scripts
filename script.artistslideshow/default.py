15afcf148f53ba1aeb05437cad2f9eb643325362
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-02-25 04:11:43 -1000

    [script.artistslideshow] 3.2.0 (#1308)
    * [script.artistslideshow] 3.2.0
    
    v.3.2.0
    - added option to pause slideshow when playback pauses
    - logic change to load first slideshow image sooner
    - override slideshow no longer resets at each song change
    - fix for potential runaway loop
    - fix for slow slideshow change when artist changes
    - fix for AS not reloading settings properly when in daemon mode
    - fixes for miscellaneous UTF8 errors
    - internal change to the way the main thread talks to the slideshow thread
    
    * fixes for invalid xml

--- script.artistslideshow/default.py
+++ script.artistslideshow/default.py
@@ -3,4 +3,8 @@ import resources.lib.artistslideshow as ArtistSlideshow
 
 if ( __name__ == "__main__" ):
     slideshow = ArtistSlideshow.Main()
+    if slideshow.RunFromSettings():
+        slideshow.DoSettingsRoutines()
+    elif not slideshow.SlideshowRunning():
+        slideshow.Start()
 xbmc.log( '[Artist Slideshow] script stopped', xbmc.LOGNOTICE )
