d328a80e7cd317b9d2aae1bbb721fdd266a28153
anxdpanic <anxdpanic@users.noreply.github.com>
2020-02-25 10:38:58 -0500

    [script.common.plugin.cache] 2.6.1.1 (#1302)

---
+++ script.common.plugin.cache/resources/lib/entry_point.py
@@ -0,0 +1,16 @@
+"""
+    Cache service for Kodi
+    Version 0.8
+
+    Copyright (C) 2010-2011 Tobias Ussing And Henrik Mosgaard Jensen
+    Copyright (C) 2019 anxdpanic
+
+    This file is part of script.common.plugin.cache
+
+    SPDX-License-Identifier: GPL-3.0-only
+    See LICENSES/GPL-3.0-only.txt for more information.
+"""
+
+import service
+
+service.run()
