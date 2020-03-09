db27d0877bfff9db056e2388489c5514fe98dd6c
mikesilvo164 <mikesilvo164@gmail.com>
2020-03-09 13:20:41 -0400

    [script.skinshortcuts] v1.1.0

---
+++ script.skinshortcuts/resources/lib/unicodeutils.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+def try_decode(text, encoding="utf-8"):
+    if not isinstance(text, str):
+        try:
+            return text.decode(encoding)
+        except:
+            pass
+    return text
