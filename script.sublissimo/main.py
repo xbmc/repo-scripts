95b18f6cd678747edbc01eb6a28e22053dbe65e3
Allert van Middendorp <drinkblacklabel@gmail.com>
2020-03-10 12:27:47 +0100

    [script.sublissimo] 0.0.1

---
+++ script.sublissimo/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from resources.lib import script
+
+import logging
+import xbmcaddon
+import xbmcplugin
+import sys
+
+ADDON = xbmcaddon.Addon()
+
+script.show_dialog()
+
+
