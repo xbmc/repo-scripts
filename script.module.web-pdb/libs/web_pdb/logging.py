201a204ed7eeb49d0e1bac4043a8c398c50f284e
Roman Miroshnychenko <roman1972@gmail.com>
2020-04-04 03:19:56 +0300

    [script.module.web-pdb] 1.5.3+matrix.1 (#1374)

---
+++ script.module.web-pdb/libs/web_pdb/logging.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+"""Mimics built-in logging module"""
+
+from __future__ import unicode_literals
+import sys
+from traceback import format_exc
+import xbmc
+
+__all__ = ['Logger', 'getLogger']
+
+PY2 = sys.version_info[0] == 2
+
+
+def encode(string):
+    if PY2 and isinstance(string, unicode):
+        string = string.encode('utf-8')
+    return string
+
+
+class Logger(object):
+    def __init__(self, name=''):
+        self._name = name
+
+    def info(self, msg):
+        xbmc.log(encode('{}: {}'.format(self._name, msg)), xbmc.LOGNOTICE)
+
+    def error(self, msg):
+        xbmc.log(encode('{}: {}'.format(self._name, msg)), xbmc.LOGERROR)
+
+    def exception(self, msg):
+        self.error(msg)
+        xbmc.log(format_exc(), xbmc.LOGERROR)
+
+    def debug(self, msg):
+        xbmc.log(encode('{}: {}'.format(self._name, msg)), xbmc.LOGDEBUG)
+
+    def critical(self, msg):
+        xbmc.log(encode('{}: {}'.format(self._name, msg)), xbmc.LOGSEVERE)
+
+
+def getLogger(name):
+    return Logger(name)
