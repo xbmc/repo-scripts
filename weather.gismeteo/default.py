be876906cf44f0ab9a09cd88da36b046c76057cc
vlmaksime <vl.maksime@gmail.com>
2020-05-26 19:53:11 +0000

    [weather.gismeteo] 0.4.2+matrix.1

--- weather.gismeteo/default.py
+++ weather.gismeteo/default.py
@@ -401,11 +401,22 @@ def location(params):
 
 @weather.mem_cached(30)
 def _location_forecast(lang, _id):
-    return Gismeteo(lang).forecast(_id)
+
+    gismeteo = Gismeteo(lang)
+
+    params = {'city_id': _id,
+              }
+
+    return _call_method(gismeteo.forecast, params)
+
+
 
 @weather.mem_cached(10)
 def _ip_locations(lang):
-    return Gismeteo(lang).cities_ip()
+
+    gismeteo = Gismeteo(lang)
+
+    return _call_method(gismeteo.cities_ip)
 
 def get_location(loc_id):
 
@@ -444,6 +455,22 @@ def get_location(loc_id):
         return Location(data)
 
     return Location()
+
+
+def _call_method(func, params=None):
+    params = params or {}
+
+    retry = 0
+    monitor = xbmc.Monitor()
+    while not monitor.abortRequested():
+        try:
+            return func(**params)
+        except (GismeteoError, WebClientError, ImportError) as e:
+            if retry >= 10:
+                raise e
+        finally:
+            retry += 1
+            monitor.waitForAbort(1)
 
 
 if __name__ == '__main__':
