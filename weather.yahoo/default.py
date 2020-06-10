6b96e5f4efeeb24b8059f46f9b974c2246e8ac57
ronie <ronie@users.noreply.github.com>
2020-06-10 11:12:08 +0200

    [weather.yahoo] 5.0.7 (#1572)
    Co-authored-by: ronie <ronie@kodi.tv>

--- weather.yahoo/default.py
+++ weather.yahoo/default.py
@@ -183,9 +183,10 @@ def properties(response, loc, locid):
         set_property('Current.Visibility'    , str(round(data['observation']['visibility'],2)) + ' mi')
         set_property('Current.Pressure'      , str(round(data['observation']['barometricPressure'],2)) + ' inHg')
     else:
-        set_property('Current.Visibility'        , str(round(1.60934 * data['observation']['visibility'],2)) + ' km')
+        set_property('Current.Visibility'    , str(round(1.60934 * data['observation']['visibility'],2)) + ' km')
         set_property('Current.Pressure'      , str(int(round((33.864 * data['observation']['barometricPressure'])))) + ' mbar')
-    set_property('Current.Precipitation'     , str(data['observation']['precipitationProbability']) + '%')
+    if 'precipitationProbability' in data['observation']:
+        set_property('Current.Precipitation' , str(data['observation']['precipitationProbability']) + '%')
 #forecast - extended
     set_property('Forecast.City'            , data['location']['displayName'])
     set_property('Forecast.Country'         , data['location']['countryName'])
