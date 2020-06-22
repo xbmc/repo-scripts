6db2d46a80457a10a02f1e8fec7dbcad609f0682
enen92 <92enen@gmail.com>
2020-06-22 19:46:04 +0000

    [screensaver.kaster] 1.3.3

--- screensaver.kaster/resources/lib/screensaverutils.py
+++ screensaver.kaster/resources/lib/screensaverutils.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-    screensaver.kaster
-    Copyright (C) 2017 enen92
+  Copyright (C) 2017-2020 enen92
+  This file is part of kaster
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  SPDX-License-Identifier: GPL-2.0-or-later
+  See LICENSE for more information.
 """
 from . import kodiutils
 import xbmcvfs
