d8b1db5b8d2a4b2fc9cc60d7e1f266ac60a5a945
dagwieers <dag.wieers@gmail.com>
2020-10-10 16:57:34 +0000

    [script.speedtester] 1.1.1

---
+++ script.speedtester/resources/lib/addon_entry.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""This is the actual Speed Tester add-on entry point"""
+
+from __future__ import absolute_import, division, unicode_literals
+
+if __name__ == '__main__':
+    from addon import run
+    run()
