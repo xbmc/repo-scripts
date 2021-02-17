914d6cb8678153ef955b2a3ca70ecad6f1031b40
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-02-16 16:41:32 -1000

    [script.artistslideshow]3.2.4+matrix.1
     fix for similar and album infolabels not being cleared property at artist change
    - updated language files
    - changed method for loading Kodi addon variables
    - code reformatting for consistent capitalization meaning in variables
    - updated read/write functions to use Python 3 features when running in Python 3
    
    fix for change in version number schema

--- script.artistslideshow/resources/lib/fileops.py
+++ script.artistslideshow/resources/lib/fileops.py
@@ -1,10 +1,6 @@
-# v.0.7.0
+# v.0.12.4
 
-import os, re
-try:
-    _range = range
-except NameError:
-    _range = xrange
+import os, re, sys
 try:
     from kodi_six import xbmcvfs
     isXBMC = True
@@ -12,20 +8,22 @@ except ImportError:
     isXBMC= False
 
 if isXBMC:
-    _mkdirs = xbmcvfs.mkdirs
-    _rmdir  = xbmcvfs.rmdir
-    _exists = xbmcvfs.exists
-    _delete = xbmcvfs.delete
-    _copy   = xbmcvfs.copy
-    _open   = xbmcvfs.File
+    _mkdirs  = xbmcvfs.mkdirs
+    _rmdir   = xbmcvfs.rmdir
+    _exists  = xbmcvfs.exists
+    _delete  = xbmcvfs.delete
+    _copy    = xbmcvfs.copy
+    _open    = xbmcvfs.File
+    _rename  = xbmcvfs.rename
 else:
     import shutil
-    _mkdirs = os.makedirs
-    _rmdir  = os.rmdir
-    _exists = os.path.exists
-    _delete = os.remove
-    _copy   = shutil.copyfile
-    _open   = open
+    _mkdirs  = os.makedirs
+    _rmdir   = os.rmdir
+    _exists  = os.path.exists
+    _delete  = os.remove
+    _copy    = shutil.copyfile
+    _open    = open
+    _rename  = os.rename
 
 
 def checkPath( thepath, createdir=True ):
@@ -46,8 +44,8 @@ def checkPath( thepath, createdir=True ):
 def copyFile( thesource, thedest ):
     log_lines = []
     if _exists( thesource ):
+        log_lines.append( 'copying file %s to %s' % (thesource, thedest) )
         try:
-            log_lines.append( 'copying file %s to %s' % (thesource, thedest) )
             _copy( thesource, thedest )
         except IOError:
             log_lines.append( 'unable to copy %s to %s' % (thesource, thedest) )
@@ -81,8 +79,8 @@ def deleteFolder( thesource, thetype='folder' ):
             _action = _rmdir
         else:
             _action = _delete
+        log_lines.append( 'deleting %s %s' % (thetype, thesource) )
         try:
-            log_lines.append( 'deleting %s %s' % (thetype, thesource) )
             if isXBMC:
                 if not _action( thesource ):
                     raise IOError( 'unable to delete item' )
@@ -99,6 +97,45 @@ def deleteFolder( thesource, thetype='folder' ):
     else:
         log_lines.append( '%s does not exist' % thesource )
         return False, log_lines
+
+
+def listDirectory( thesource, thefilter='all' ):
+    log_lines = []
+    log_lines.append( 'getting contents of folder %s' % thesource )
+    if isXBMC:
+        try:
+            dirs, files = xbmcvfs.listdir( thesource )
+        except OSError:
+            log_lines.append( 'OSError getting directory list' )
+            return [], log_lines
+        except Exception as e:
+            log_lines.append( 'unexpected error getting directory list' )
+            log_lines.append( e )
+            return [], log_lines
+        if thefilter == 'files':
+            log_lines.append( 'returning files from %s' % thesource )
+            log_lines.append( files )
+            return files, log_lines
+        elif thefilter =='folders':
+            log_lines.append( 'returning folders from %s' % thesource )
+            log_lines.append( dirs )
+            return dirs, log_lines
+        else:
+            log_lines.append( 'returning files and folders from %s' % thesource )
+            log_lines.append( files + dirs )
+            return files + dirs, log_lines
+    else:
+        try:
+            contents = os.listdir( thesource )
+        except OSError:
+            log_lines.append( 'OSError getting directory list' )
+            return [], log_lines
+        except Exception as e:
+            log_lines.append( 'unexpected error getting directory list' )
+            log_lines.append( e )
+            return [], log_lines
+        log_lines.append( 'returning files and folders from %s' % thesource )
+        return contents, log_lines
 
 
 def moveFile( thesource, thedest ):
@@ -118,25 +155,34 @@ def moveFile( thesource, thedest ):
     return success, log_lines + cp_loglines + dl_loglines
 
 
-def atoi( text ):
+def _atoi( text ):
     return int(text) if text.isdigit() else text
 
 
-def naturalKeys( text ):
-    '''
-    alist.sort( key=naturalKeys ) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    '''
-    return [ atoi( c ) for c in re.split( r'(\d+)', text ) ]
+def naturalKeys( thelist ):
+    # from http://nedbatchelder.com/blog/200712/human_sorting.html
+    return [ _atoi( c ) for c in re.split( r'(\d+)', thelist ) ]
+
+
+def osPathFromString( spath, sep='/' ):
+    pathlist = spath.split( sep )
+    if spath.startswith( sep ):
+        pathlist.insert( 0, os.sep )
+        pathlist[2] = pathlist[2] + os.sep
+    return os.path.join(*pathlist)
 
 
 def readFile( filename ):
     log_lines = []
     if _exists( filename ):
-        thefile = _open( filename, 'r' )
         try:
-            data = thefile.read()
-            thefile.close()
+            if sys.version_info >= (3, 0):
+                with _open( filename, 'r') as thefile:
+                    thedata = thefile.read()
+            else:
+                thefile = _open( filename, 'r' )
+                thedata = thefile.read()
+                thefile.close()
         except IOError:
             log_lines.append( 'unable to read data from ' + filename )
             return log_lines, ''
@@ -144,24 +190,63 @@ def readFile( filename ):
             log_lines.append( 'unknown error while reading data from ' + filename )
             log_lines.append( e )
             return log_lines, ''
-        return log_lines, data
+        return log_lines, thedata
     else:
         log_lines.append( '%s does not exist' % filename )
         return log_lines, ''
 
 
 def renameFile ( thesource, thedest ):
-    return moveFile( thesource, thedest )
+    log_lines = []
+    log_lines.append( 'renaming file %s to %s' % (thesource, thedest) )
+    try:
+        _rename( thesource, thedest )
+    except IOError:
+        log_lines.append( 'unable to rename %s to %s' % (thesource, thedest) )
+        return False, log_lines
+    except Exception as e:
+        log_lines.append( 'unknown error while attempting to rename %s to %s' % (thesource, thedest) )
+        log_lines.append( e )
+        return False, log_lines
+    return True, log_lines
+
+
+def _remove_trailing_dot( thename, endreplace='' ):
+    if thename[-1] == '.' and len( thename ) > 1 and endreplace != '.':
+        return _remove_trailing_dot( thename[:-1] + endreplace )
+    else:
+        return thename
+
+
+def setSafeName( thename, illegalchars='<>:"/\|?*', illegalreplace='_', endreplace='' ):
+    loglines = []
+    if not thename:
+        loglines.append( 'name is empty, returning it' )
+        return thename, loglines
+    s_name = ''
+    loglines.append( 'started with %s' % thename )
+    loglines.append( 'the illegal characters are %s and the replacement is %s' % (illegalchars, illegalreplace) )
+    for c in list( _remove_trailing_dot( thename, endreplace=endreplace ) ):
+        if c in illegalchars:
+            s_name = s_name + illegalreplace
+        else:
+            s_name = s_name + c
+    loglines.append( 'finished with %s' % s_name )
+    return s_name, loglines
 
 
 def writeFile( data, filename, wtype='wb' ):
     log_lines = []
     if type(data).__name__=='unicode':
         data = data.encode('utf-8')
-    thefile = _open( filename, wtype )
     try:
-        thefile.write( data )
-        thefile.close()
+        if sys.version_info >= (3, 0):
+            with _open( filename, wtype) as thefile:
+                thefile.write( data )
+        else:
+            thefile = _open( filename, wtype )
+            thefile.write( data )
+            thefile.close()
     except IOError as e:
         log_lines.append( 'unable to write data to ' + filename )
         log_lines.append( e )
