27b94e96abb228484587c82cfe5be8894dd2c6e1
phate89 <phates89@gmail.com>
2021-02-21 16:06:43 +0000

    [script.module.phate89] 1.2.1

--- script.module.phate89/lib/phate89lib/kodiutils.py
+++ script.module.phate89/lib/phate89lib/kodiutils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from kodi_six import xbmc, xbmcaddon, xbmcplugin, xbmcgui, utils
 import os
-import sys
 import re
+import sys
+from kodi_six import xbmc, xbmcaddon, xbmcplugin, xbmcgui, utils  # pylint: disable=import-error
 try:
     from urllib.parse import urlencode
 except ImportError:
@@ -26,49 +26,60 @@ IMAGE_PATH_T = os.path.join(PATH_T, 'resources', 'media', "")
 LANGUAGE = ADDON.getLocalizedString
 KODILANGUAGE = xbmc.getLocalizedString
 
-HANDLE=int(sys.argv[1])
+HANDLE = int(sys.argv[1])
 
-def executebuiltin(func,block=False):
-    xbmc.executebuiltin(func,block)
+
+def executebuiltin(func, block=False):
+    xbmc.executebuiltin(func, block)
+
 
 def notify(msg):
     message = 'Notification(%s,%s)' % (ID, msg)
     xbmc.executebuiltin(message)
+
 
 def log(msg, level=2):
     message = u'%s: %s' % (ID, msg)
     if level > 1:
         xbmc.log(msg=message, level=xbmc.LOGDEBUG)
     else:
-        xbmc.log(msg=message, level=xbmc.LOGNOTICE)
+        xbmc.log(msg=message, level=xbmc.LOGINFO)
         if level == 0:
             notify(msg)
+
 
 def py2_decode(s):
     return utils.py2_decode(s)
 
+
 def py2_encode(s):
     return utils.py2_encode(s)
+
 
 def getSetting(setting):
     return ADDON.getSetting(setting).strip()
 
+
 def getSettingAsBool(setting):
     return getSetting(setting).lower() == "true"
 
+
 def getSettingAsNum(setting):
-    num=0
+    num = 0
     try:
-        num=float(getSetting(setting))
-    except:
+        num = float(getSetting(setting))
+    except ValueError:
         pass
     return num
 
-def setSetting(setting,value):
+
+def setSetting(setting, value):
     ADDON.setSetting(id=setting, value=str(value))
+
 
 def getKeyboard():
     return xbmc.Keyboard()
+
 
 def getKeyboardText(heading, default='', hidden=False):
     kb = xbmc.Keyboard(default, heading)
@@ -76,106 +87,139 @@ def getKeyboardText(heading, default='', hidden=False):
     kb.doModal()
     if (kb.isConfirmed()):
         return kb.getText()
-    else:
-        False
+    return False
+
 
 def showOkDialog(heading, line):
     xbmcgui.Dialog().ok(heading, line)
 
-def addListItem(label="", params={}, label2=None, thumb=None, fanart=None, poster=None, arts={},
-                videoInfo={}, properties={}, isFolder=True):
-    item=xbmcgui.ListItem(label,label2)
-    if thumb: arts['thumb'] = thumb
-    if fanart: arts['fanart'] = fanart
-    if poster: arts['poster'] = poster
+
+def addListItem(label="", params=None, label2=None, thumb=None, fanart=None, poster=None, arts=None,
+                videoInfo=None, properties=None, isFolder=True):
+    if arts is None:
+        arts = {}
+    if properties is None:
+        properties = {}
+    item = xbmcgui.ListItem(label, label2)
+    if thumb:
+        arts['thumb'] = thumb
+    if fanart:
+        arts['fanart'] = fanart
+    if poster:
+        arts['poster'] = poster
     item.setArt(arts)
-    item.setInfo( 'video', videoInfo)
-    if not isFolder: properties['IsPlayable']='true'
-    if isinstance(params,dict):
-        url=staticutils.parameters(params)
+    item.setInfo('video', videoInfo)
+    if not isFolder:
+        properties['IsPlayable'] = 'true'
+    if isinstance(params, dict):
+        url = staticutils.parameters(params)
     else:
         url = params
     for key, value in list(properties.items()):
         item.setProperty(key, value)
     return xbmcplugin.addDirectoryItem(handle=HANDLE, url=url, listitem=item, isFolder=isFolder)
 
-def setResolvedUrl(url="", solved=True, subs=[], headers=None, ins=None, insdata=None):
-    headerUrl=""
+
+def setResolvedUrl(url="", solved=True, subs=None, headers=None, ins=None, insdata=None):
+    headerUrl = ""
     if headers:
-            headerUrl = urlencode(headers)
-    item = xbmcgui.ListItem(path = url + "|" + headerUrl)
-    item.setSubtitles(subs)
+        headerUrl = urlencode(headers)
+    item = xbmcgui.ListItem(path=url + "|" + headerUrl)
+    if subs is not None:
+        item.setSubtitles(subs)
     if ins:
         item.setProperty('inputstreamaddon', ins)
+        item.setProperty('inputstream', ins)
         if insdata:
             for key, value in list(insdata.items()):
                 item.setProperty(ins + '.' + key, value)
     xbmcplugin.setResolvedUrl(HANDLE, solved, item)
     sys.exit()
 
+
 def append_subtitle(sUrl, subtitlename, sync=False, provider=None):
     #listitem = createListItem({'label': 'Italian', 'label2': subtitlename, 'thumbnailImage': 'it'})
     if not provider:
-        tUrl = {'action': 'download', 'subid':sUrl}
+        tUrl = {'action': 'download', 'subid': sUrl}
     elif provider == 'ItalianSubs':
-        p=re.search('subtitle_id=(?P<SUBID>[0-9]+)',sUrl,re.IGNORECASE)
+        p = re.search('subtitle_id=(?P<SUBID>[0-9]+)', sUrl, re.IGNORECASE)
         if not p:
             return False
-        tUrl = "plugin://service.subtitles.itasa/?action=download&subid={subid}".format(subid=p.group('SUBID'))
+        tUrl = "plugin://service.subtitles.itasa/?action=download&subid={subid}".format(
+            subid=p.group('SUBID'))
     else:
-        tUrl = {'action': 'download', 'url':sUrl}
-    log("aggiungo il sottotitolo '" + subtitlename + "' alla lista",3)
-    return addListItem(label="Italian", label2=subtitlename, params=tUrl, thumb="it", 
-                       properties={"sync": 'true' if sync else 'false', "hearing_imp": "false"}, isFolder=False)
+        tUrl = {'action': 'download', 'url': sUrl}
+    log("aggiungo il sottotitolo '" + subtitlename + "' alla lista", 3)
+    return addListItem(label="Italian", label2=subtitlename, params=tUrl, thumb="it",
+                       properties={"sync": 'true' if sync else 'false', "hearing_imp": "false"},
+                       isFolder=False)
 
-def setContent(type='videos'):
-    xbmcplugin.setContent(HANDLE, type)
 
-def endScript(message=None,loglevel=2, closedir=True):
+def setContent(ctype='videos'):
+    xbmcplugin.setContent(HANDLE, ctype)
+
+
+def endScript(message=None, loglevel=2, closedir=True):
     if message:
-        log(message,loglevel)
+        log(message, loglevel)
     if closedir:
         xbmcplugin.endOfDirectory(handle=HANDLE, succeeded=True)
     sys.exit()
+
 
 def createAddonFolder():
     if not os.path.isdir(DATA_PATH_T):
         log("Creating the addon data folder")
         os.makedirs(DATA_PATH_T)
 
+
 def getShowID():
-    json_query = xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Player.GetItem","params":{"playerid":1,"properties":["tvshowid"]},"id":1}' )
-    json_player_getitem = json.loads(utils.py2_decode(json_query, 'utf-8', errors='ignore'))
-    if 'result' in json_player_getitem and json_player_getitem['result']['item']['type'] == 'episode':
-        json_query = xbmc.executeJSONRPC('{"jsonrpc":"2.0","id":1,"method":"VideoLibrary.GetTVShowDetails","params":{"tvshowid":%s, "properties": ["imdbnumber"]}}' % (json_player_getitem['result']['item']['tvshowid']) )
-        json_getepisodedetails = json.loads(utils.py2_decode(json_query, 'utf-8', errors='ignore'))
-        if 'result' in json_getepisodedetails and json_getepisodedetails['result']['tvshowdetails']['imdbnumber']!='':
-            return str(json_getepisodedetails['result']['tvshowdetails']['imdbnumber'])
+    json_query = xbmc.executeJSONRPC((
+        '{"jsonrpc":"2.0","method":"Player.GetItem","params":'
+        '{"playerid":1,"properties":["tvshowid"]},"id":1}'))
+    jsn_player_item = json.loads(utils.py2_decode(json_query, 'utf-8', errors='ignore'))
+    if 'result' in jsn_player_item and jsn_player_item['result']['item']['type'] == 'episode':
+        json_query = xbmc.executeJSONRPC((
+            '{"jsonrpc":"2.0","id":1,"method":"VideoLibrary.GetTVShowDetails","params":'
+            '{"tvshowid":%s, "properties": ["imdbnumber"]}}') % (
+                jsn_player_item['result']['item']['tvshowid']))
+        jsn_ep_det = json.loads(utils.py2_decode(json_query, 'utf-8', errors='ignore'))
+        if 'result' in jsn_ep_det and jsn_ep_det['result']['tvshowdetails']['imdbnumber'] != '':
+            return str(jsn_ep_det['result']['tvshowdetails']['imdbnumber'])
     return False
 
-def containsLanguage(strlang,langs):
+
+def containsLanguage(strlang, langs):
     for lang in strlang.split(','):
         if xbmc.convertLanguage(lang, xbmc.ISO_639_2) in langs:
             return True
     return False
 
+
 def isPlayingVideo():
     return xbmc.Player().isPlayingVideo()
+
 
 def getInfoLabel(lbl):
     return xbmc.getInfoLabel(lbl)
 
-def getRegion(id):
-    return xbmc.getRegion(id)
+
+def getRegion(r):
+    return xbmc.getRegion(r)
+
 
 def getEpisodeInfo():
     episode = {}
-    episode['tvshow'] = staticutils.normalizeString(xbmc.getInfoLabel('VideoPlayer.TVshowtitle'))    # Show
-    episode['season'] = xbmc.getInfoLabel('VideoPlayer.Season')                                    # Season
-    episode['episode'] = xbmc.getInfoLabel('VideoPlayer.Episode')                                  # Episode
-    file_original_path = xbmc.Player().getPlayingFile()                                            # Full path
-    
-    if str(episode['episode']).lower().find('s') > -1:                                             # Check if season is "Special"
+    episode['tvshow'] = staticutils.normalizeString(
+        xbmc.getInfoLabel('VideoPlayer.TVshowtitle'))    # Show
+    episode['season'] = xbmc.getInfoLabel(
+        'VideoPlayer.Season')                            # Season
+    episode['episode'] = xbmc.getInfoLabel(
+        'VideoPlayer.Episode')                           # Episode
+    file_original_path = xbmc.Player().getPlayingFile()  # Full path
+
+    # Check if season is "Special"
+    if str(episode['episode']).lower().find('s') > -1:
         episode['season'] = 0
         episode['episode'] = int(str(episode['episode'])[-1:])
 
@@ -188,5 +232,13 @@ def getEpisodeInfo():
     episode['filename'] = os.path.splitext(os.path.basename(file_original_path))[0]
 
     return episode
+
+
+def getFormattedDate(dt):
+    fmt = getRegion('datelong')
+    fmt = fmt.replace("%A", KODILANGUAGE(dt.weekday() + 11))
+    fmt = fmt.replace("%B", KODILANGUAGE(dt.month + 20))
+    return dt.strftime(py2_encode(fmt))
+
 
 log("Starting module '%s' version '%s' with command '%s'" % (NAME, VERSION, sys.argv[2]), 1)
--- script.module.phate89/lib/phate89lib/rutils.py
+++ script.module.phate89/lib/phate89lib/rutils.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
+import os
+import shutil
 import zipfile
 import requests
-import re
-import shutil
-import os
-from . import staticutils
 from bs4 import BeautifulSoup
-from kodi_six.utils import py2_encode
+from . import staticutils
+try:
+    from json.decoder import JSONDecodeError
+except ImportError:
+    JSONDecodeError = ValueError
+
 
 class RUtils(object):
     SESSION = requests.Session()
     USERAGENT = 'phate89 utility module'
-    DEFPARAMS={}
-    LOGLEVEL=5
+    DEFPARAMS = {}
+    LOGLEVEL = 5
 
     def __init__(self):
         self.setUserAgent(self.USERAGENT)
@@ -24,76 +28,80 @@ class RUtils(object):
     def setHeader(self, voice, value):
         self.SESSION.headers.update({voice: value})
 
-    def log(self,msg, level=2):
-        if level<=self.LOGLEVEL:
-            print (u"#### {name}: {text} ####".format(name=os.path.basename(__file__),text=msg))
+    def log(self, msg, level=2):
+        if level <= self.LOGLEVEL:
+            print(u"#### {name}: {text} ####".format(name=os.path.basename(__file__), text=msg))
 
-    def createRequest(self,url,params={},post={},stream=False,addDefault=True, **kwargs):
+    def createRequest(self, url, params=None, post=None, stream=False, addDefault=True, **kwargs):
+        if params is None:
+            params = {}
         if addDefault:
             params.update(self.DEFPARAMS)
-        if post:
-            r = self.SESSION.post(url,params=params,data=post,stream=stream, **kwargs)
+        if post is not None:
+            r = self.SESSION.post(url, params=params, data=post,
+                                  stream=stream, **kwargs)
         else:
-            r = self.SESSION.get(url,params=params,stream=stream, **kwargs)
-        self.log("Opening url %s" % r.url,2)
-        if r.status_code == requests.codes.ok or r.status_code == 302:
+            r = self.SESSION.get(url, params=params, stream=stream, **kwargs)
+        self.log("Opening url %s" % r.url, 2)
+        if r.ok:
             return r
-        elif r.status_code < 500:
-            self.log("Error opening url. Client error")
+        if r.status_code < 500:
+            self.log("Error opening url. Client error " + str(r.status_code))
         else:
-            self.log("Error opening url. Server error")
+            self.log("Error opening url. Server error " + str(r.status_code))
         return False
 
     def newSession(self):
         self.SESSION = requests.Session()
         self.setUserAgent(self.USERAGENT)
 
-    def getJson(self,url,params={},post={}, **kwargs):
+    def getJson(self, url, params=None, post=None, **kwargs):
         r = self.createRequest(url, params, post, **kwargs)
         if r:
             try:
                 return r.json()
-            except:
+            except (requests.HTTPError, JSONDecodeError):
                 self.log("Error serializing json")
-                return False
+        return None
 
-    def getSoup(self,url,params={},post={},parser="html.parser", **kwargs):
+    def getSoup(self, url, params=None, post=None, parser="html.parser", **kwargs):
         r = self.createRequest(url, params, post)
         if r:
             return BeautifulSoup(r.text, parser, **kwargs)
         return False
 
-    def getSoupFromRes(self,res,parser="html.parser", **kwargs):
+    def getSoupFromRes(self, res, parser="html.parser", **kwargs):
         if res:
             return BeautifulSoup(res.text, parser, **kwargs)
         return False
 
-    def getText(self,url,params={},post={}, **kwargs):
-        r = self.createRequest(url, params, post)
+    def getText(self, url, params=None, post=None, **kwargs):
+        r = self.createRequest(url, params, post, **kwargs)
         if r:
-            return py2_encode(r.text)
+            return r.text
         return False
 
-    def getFileExtracted(self,url,params={},post={},dataPath='',index=0):
+    def getFileExtracted(self, url, params=None, post=None, dataPath='', index=0):
         if not dataPath:
             return False
-        data=self.createRequest(url,params,post,stream=True)
+        data = self.createRequest(url, params, post, stream=True)
         if not data:
-            self.log(sUrl + " file read failed",4)
+            self.log(url + " file read failed", 4)
             return False
 
         if os.path.isdir(dataPath):
             shutil.rmtree(dataPath)
         os.makedirs(dataPath)
+        chunk = ''
         for chunk in data.iter_content(1):
-            ext='srt'
-            if chunk=='P':
-                ext='zip'
-            elif chunk=='R':
-                ext='rar'
+            ext = 'srt'
+            if chunk == 'P':
+                ext = 'zip'
+            elif chunk == 'R':
+                ext = 'rar'
             break
 
-        TEMPFILE=os.path.join(dataPath, 'itasa.' + ext)
+        TEMPFILE = os.path.join(dataPath, 'itasa.' + ext)
         if os.path.exists(TEMPFILE):
             os.remove(TEMPFILE)
         try:
@@ -101,54 +109,54 @@ class RUtils(object):
                 fd.write(chunk)
                 for chunk in data.iter_content(chunk_size=1024):
                     fd.write(chunk)
-        except:
+        except EnvironmentError:
             self.log("Error writing file")
             return False
-        
-        if ext=='srt':
+
+        if ext == 'srt':
             outName = TEMPFILE
             index = 0
-        elif ext=='zip':
-            zf=zipfile.ZipFile(TEMPFILE,'r')
+        elif ext == 'zip':
+            zf = zipfile.ZipFile(TEMPFILE, 'r')
             exts = ['.srt', '.sub', '.txt', '.smi', '.ssa', '.ass']
             subs = [x for x in zf.namelist() if os.path.splitext(x)[1] in exts]
-            if index>=len(subs):
-                index=0
-            binData=zf.read(subs[index])
+            if index >= len(subs):
+                index = 0
+            binData = zf.read(subs[index])
             zf.close()
-            outName=os.path.join(dataPath,subs[index])
-            
+            outName = os.path.join(dataPath, subs[index])
+
             if binData:
-                try:       
-                    fp=open(outName,'wb')                 
+                try:
+                    fp = open(outName, 'wb')
                     fp.write(binData)
                     fp.close()
-                except:
+                except EnvironmentError:
                     self.log("Error writing text subtitle file")
                     return False
             else:
-                self.log('subtitle not found in zip file',1)
+                self.log('subtitle not found in zip file', 1)
                 fp.close()
                 return False
-        elif ext=='rar':
+        elif ext == 'rar':
             try:
-                __import__(xbmc)
+                from kodi_six import xbmc
             except ImportError:
-                self.log('rar extraction not supported',1)
+                self.log('rar extraction not supported', 1)
                 return False
-            TEMPFOLDER=os.path.join(dataPath,'temp', '')
+            TEMPFOLDER = os.path.join(dataPath, 'temp', '')
             if os.path.isdir(TEMPFOLDER):
                 shutil.rmtree(TEMPFOLDER)
             os.makedirs(TEMPFOLDER)
             xbmc.executebuiltin('XBMC.Extract(%s,%s)' % (TEMPFILE, TEMPFOLDER), True)
             exts = ['.srt', '.sub', '.txt', '.smi', '.ssa', '.ass']
             subs = [os.path.join(root, name)
-                 for root, dirs, files in os.walk(TEMPFOLDER)
-                 for name in files
-                 if os.path.splitext(name)[1] in exts]
-            if len(subs)<=0:
+                    for root, dirs, files in os.walk(TEMPFOLDER)
+                    for name in files
+                    if os.path.splitext(name)[1] in exts]
+            if not subs:
                 return False
-            if index>=len(subs):
+            if index >= len(subs):
                 index = 0
             outName = subs[0]
         return index, outName
