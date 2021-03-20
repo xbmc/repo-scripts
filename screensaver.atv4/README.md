4fcd9ccba9ccf467239246a22cc124ab05315fa9
enen92 <92enen@gmail.com>
2021-03-20 17:00:14 +0000

    [screensaver.atv4] 3.0.2

--- screensaver.atv4/README.md
+++ screensaver.atv4/README.md
@@ -2,13 +2,44 @@
 
 # screensaver.atv4
 
-## Apple Aerial screensavers for Kodi
+## Apple Aerial screensavers for Kodi 19 (Matrix)
 
-This addon lets you add the apple tv 4 screensavers to Kodi Entertainment Center
+This addon adds the Apple Aerial screensavers to Kodi Entertainment Center. It can be installed via the Kodi official repository.
 
-## Supported versions
+## Plugin Features
 
-Aerial is available for Kodi Matrix and above. It can be installed via the Kodi official repository.
+- JSON-based video playlist fetching and playback
+  - Custom JSON by default to include all scenes Apple ever published
+  - Configurable to download the latest Apple JSON on each run
+- Display Power Management Signaling (DPMS) configurable
+  - When the display is supposed to go to sleep, pause/stop the Aerials video and turn the display off or put it into standby via HDMI CEC
+- Choose from playback of:
+  - HEVC H.265 or AVC H.264 codec (HEVC default)
+  - 4K or 1080p resolution (4K default)
+  - High Dynamic Range (HDR) Dolby Vision or Standard Dynamic Range (SDR default)
+- Filtering of videos by location/scene
+- Offline caching of selected video quality
+  - Download scene by scene or all at once
+  - Full offline mode to prevent all network calls, using only local videos and JSON
+  - Checksum validation to prevent unnecessary re-downloading of cached videos
+- Custom JSON file count and gigabytes per quality level:
+  - H.264 1080P SDR: 71 files, 17.3GB
+  - H.265 1080P SDR: 90 files, 15.5GB
+  - H.265 1080P HDR: 90 files, 19.9GB
+  - H.265 4K SDR: 90 files, 31.0GB
+  - H.265 4K HDR: 90 files, 39.5GB
+
+## Aerials History
+- When the Apple TV first came out with Aerials screensavers, Apple published a [V1 JSON manifest](http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json) with all the different videos. Locations featured San Francisco, New York, China, Hong Kong, Greenland, Dubai, Los Angeles, and others. They were published in 1080p H.264 format
+  - This JSON also included a `timeOfDay` key indicating if the video was shot during the day or night 
+  - Later on, Apple changed the URL to a [V2 JSON manifest](http://a1.v2.phobos.apple.com.edgesuite.net/us/r1000/000/Features/atv/AutumnResources/videos/entries.json). As of March 2021, V1 and V2 contents are identical
+- Later on in October 2018, Apple published a large refresh of Aerials in 4K resolution with HDR color space and in a more modern H.265 "HEVC" codec. 
+  - While this included re-colored, extended versions of some of the original set of videos, it also retired others.
+- Through the rest of 2018 and up until the beginning of 2020, Apple continued to release new 4K HDR Aerials scenes. 20+ underwater vistas, new scenes from existing locations, and globe-spanning shots from the International Space Station are all now included.
+- At some point, Apple started vending the JSON manifest at a [new URL](https://sylvan.apple.com/Aerials/resources.tar) and bundled into a tarball `resources.tar`. (Thanks to the [other](https://github.com/JohnCoates/Aerial/issues/463#issuecomment-423128752) big Aerials project for this)
+  - As part of this update, Apple removed the `timeOfDay` key so this plugin's filtering based on time of day is no longer possible without manually adding JSON keys for each scene
+  - The new JSON contained new URLs for the original videos, possibly behind a Content Delivery Network (CDN). For example, a Greenland video's URL changed from http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/comp_GL_G004_C010_v03_6Mbps.mov to http://a1.v2.phobos.apple.com.edgesuite.net/us/r1000/000/Features/atv/AutumnResources/videos/comp_GL_G004_C010_v03_6Mbps.mov (adding `.v2` and `.edgesuite.net`)
+- [Benjamin Mayo](https://github.com/benjaminmayo) published a [Google Doc](https://docs.google.com/spreadsheets/d/1bboTohF06r-fafrImTExAPqM9m6h2m2lgJyAkQuYVJI/edit?usp=sharing) with a historical record of all the Aerials videos and links to all their different variants (H264, HDR, 4K, etc.) and also hosts a [website](https://bzamayo.com/watch-all-the-apple-tv-aerial-video-screensavers) for streaming all the different options
 
 # Screenshots
 
--- screensaver.atv4/resources/lib/offline.py
+++ screensaver.atv4/resources/lib/offline.py
@@ -6,33 +6,69 @@
    See LICENSE for more information.
 """
 
+import xbmc
 import xbmcvfs
-from .commonatv import dialog, addon, translate, places
-from .playlist import AtvPlaylist
-from .downloader import Downloader
 
+from .commonatv import dialog, addon, translate, find_ranked_key_in_dict, compute_block_key_list
+from .downloader import Downloader
+from .playlist import AtvPlaylist
+
+# Array of "All" plus each unique "accessibilityLabel" in entries.json
+# Used in a popup to allow the user to choose what to download
+# Sort the locations list alphabetically and in place
+locations = sorted(["All", "Italy to Asia", "Iran and Afghanistan", "Dubai", "Africa and the Middle East",
+                    "California to Vegas", "Southern California to Baja", "China", "Antarctica", "Liwa",
+                    "Sahara and Italy",
+                    "Los Angeles", "San Francisco", "London", "Ireland to Asia", "New York", "West Africa to the Alps",
+                    "New Zealand", "Caribbean Day", "Hawaii", "Caribbean", "Africa Night", "North America Aurora",
+                    "New York Night", "Greenland", "Hong Kong", "Korean and Japan Night"])
+
+
+# Parse the JSON to get a list of URLs and download the files to the download folder
 def offline():
+    # NOTE: the download folder must be saved by pushing OK in the settings dialog before this will succeed
     if addon.getSetting("download-folder") and xbmcvfs.exists(addon.getSetting("download-folder")):
-        choose = dialog.select(translate(32014),places)
-        if choose > -1:
-            atv_playlist = AtvPlaylist()
-            playlist_dict = atv_playlist.getPlaylistJson()
+        # Present a popup to the user and allow them to select a single location to download, or all
+        locations_chosen_index = dialog.select(translate(32014), locations)
+        if locations_chosen_index > -1:
+            # Initialize the Playlist class, and get the JSON containing URLs
+            top_level_json = AtvPlaylist().get_playlist_json()
             download_list = []
-            if playlist_dict:
-                for block in playlist_dict:
-                    for video in block['assets']:
-                        if places[choose].lower() == "all":
-                            download_list.append(video['url'])
-                        else:
-                            if places[choose].lower() == video['accessibilityLabel'].lower():
-                                download_list.append(video['url'])
-            # call downloader
+            if top_level_json:
+
+                # Parse the H264, HDR, and 4K settings to determine URL preference.
+                block_key_list = compute_block_key_list(addon.getSettingBool("enable-4k"),
+                                                        addon.getSettingBool("enable-hdr"),
+                                                        addon.getSettingBool("enable-hevc"))
+
+                # Top-level JSON has assets array, initialAssetCount, version. Inspect each block in assets
+                for block in top_level_json["assets"]:
+
+                    # If the chosen location was a specific place and not All
+                    if not locations[locations_chosen_index] == "All":
+                        # Each block contains a location whose name is stored in accessibilityLabel. These may recur
+                        # Get the location from the current block
+                        location = block["accessibilityLabel"]
+                        # Exit block processing early if the location didn't match our preference.
+                        # This prevents the location from being added to the download list
+                        if not locations[locations_chosen_index] == location:
+                            xbmc.log("Current location {} is not chosen location, skipping download".format(location),
+                                     level=xbmc.LOGDEBUG)
+                            continue
+
+                    # Get the URL to download
+                    url = find_ranked_key_in_dict(block, block_key_list)
+
+                    # If the URL contains HTTPS, we need revert to HTTP to avoid bad SSL cert
+                    # NOTE: Old Apple URLs were HTTP, new URLs are HTTPS with a bad cert
+                    if "https" in url:
+                        url = url.replace("https://", "http://")
+                    download_list.append(url)
+
+            # call downloader if the download_list has been populated
             if download_list:
-                down = Downloader()
-                down.downloadall(download_list)
+                Downloader().download_videos_from_urls(download_list)
             else:
                 dialog.ok(translate(32000), translate(32012))
     else:
         dialog.ok(translate(32000), translate(32013))
-
-
