5e1869c03c5e879c2026d8202457cba004d761b6
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-03-22 09:24:50 -1000

    [script.speedfaninfo] v1.2.0
    - fix for Python3 logging issue
    - new translations
    - code refactoring

--- script.speedfaninfo/default.py
+++ script.speedfaninfo/default.py
@@ -1,6 +1,4 @@
-from kodi_six import xbmc
 import resources.lib.spid as SpeedFan
 
 if ( __name__ == "__main__" ):
     window = SpeedFan.Main()
-xbmc.log( '[SpeedFan Info] script stopped', xbmc.LOGINFO )
