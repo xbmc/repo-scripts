446c56b769b692ebe4e2ef1e7235168d8419ef3d
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-03-23 13:24:25 -1000

    [script.speedfaninfo] v1.2.0 (#1948)
    - fix for Python3 logging issue
    - new translations
    - code refactoring

--- script.speedfaninfo/resources/lib/xlogger.py
+++ script.speedfaninfo/resources/lib/xlogger.py
@@ -1,25 +1,30 @@
-#v.0.4.1
+#v.0.4.14
 
 try:
     from kodi_six import xbmc
     LOGTYPE = 'xbmc'
 except ImportError:
-    import logging, logging.handlers
+    import os, logging, logging.handlers
     LOGTYPE = 'file'
 
-#this class creates an object used to log stuff to the log file
+#this class creates an object used to log stuff to the xbmc log file
 class Logger( object ):
-    def __init__( self, logconfig="file", logformat='%(asctime)-15s %(levelname)-8s %(message)s', logfile='logfile.log',
-                  logname='_logger', numbackups=5, logdebug=False, maxsize=100000, when='midnight', interval=1, preamble='' ):
+    def __init__( self, logconfig="timed", logformat='%(asctime)-15s %(levelname)-8s %(message)s', logfile='logfile.log',
+                  logname='_logger', numbackups=5, logdebug=False, maxsize=100000, when='midnight', preamble='' ):
+        """Logs interactions."""
         self.LOGPREAMBLE = preamble
         self.LOGDEBUG = logdebug
         if LOGTYPE == 'file':
+            checkdir = os.sep.join( logfile.split(os.sep)[:-1] )
+            if not checkdir == 'logfile.log':
+                if not os.path.exists( checkdir ):
+                    os.makedirs( checkdir )
             self.logger = logging.getLogger( logname )
             self.logger.setLevel( logging.DEBUG )
             if logconfig == 'timed':
-                lr = logging.handlers.TimedRotatingFileHandler( logfile, when=when, backupCount=numbackups)
+                lr = logging.handlers.TimedRotatingFileHandler( logfile, when=when, backupCount=numbackups, encoding='utf-8')
             else:
-                lr = logging.handlers.RotatingFileHandler( logfile, maxBytes=maxsize, backupCount=numbackups )
+                lr = logging.handlers.RotatingFileHandler( logfile, maxBytes=maxsize, backupCount=numbackups, encoding='utf-8' )
             lr.setLevel( logging.DEBUG )
             lr.setFormatter( logging.Formatter( logformat ) )
             self.logger.addHandler( lr )
@@ -39,11 +44,8 @@ class Logger( object ):
                 loglevel = self.logger.critical
             else:
                 loglevel = self.logger.debug
-
         for line in loglines:
             try:
-                if type(line).__name__=='unicode':
-                    line = line.encode('utf-8')
                 str_line = line.__str__()
             except Exception as e:
                 str_line = ''
@@ -63,16 +65,16 @@ class Logger( object ):
     def _output_file( self, line, loglevel ):
         if self.LOGDEBUG or loglevel != self.logger.debug:
             try:
-                loglevel( "%s %s" % (self.LOGPREAMBLE, line.__str__()) )
+                loglevel( '%s %s' % (self.LOGPREAMBLE, line ) )
             except Exception as e:
-                self.logger.debug( "%s unable to output logline" % self.LOGPREAMBLE )
-                self.logger.debug( "%s %s" % (self.LOGPREAMBLE, e.__str__()) )
+                self.logger.debug( '%s unable to output logline' % self.LOGPREAMBLE )
+                self.logger.debug( '%s %s' % (self.LOGPREAMBLE, e.__str__()) )
 
 
     def _output_xbmc( self, line, loglevel ):
         if self.LOGDEBUG or loglevel != xbmc.LOGDEBUG:
             try:
-                xbmc.log( "%s %s" % (self.LOGPREAMBLE, line.__str__()), loglevel)
+                xbmc.log( '%s %s' % (self.LOGPREAMBLE, line.__str__()), loglevel)
             except Exception as e:
-                xbmc.log( "%s unable to output logline" % self.LOGPREAMBLE, loglevel)
-                xbmc.log ("%s %s" % (self.LOGPREAMBLE, e.__str__()), loglevel)
+                xbmc.log( '%s unable to output logline' % self.LOGPREAMBLE, loglevel)
+                xbmc.log ('%s %s' % (self.LOGPREAMBLE, e.__str__()), loglevel)
