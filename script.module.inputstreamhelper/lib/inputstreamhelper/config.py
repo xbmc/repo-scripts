76bb135d96096b5b64cd9e31692796e91638b2f0
emilsvennesson <emilsvennesson@users.noreply.github.com>
2021-05-27 21:05:27 +0200

    [script.module.inputstreamhelper] 0.5.4 (#2026)

--- script.module.inputstreamhelper/lib/inputstreamhelper/config.py
+++ script.module.inputstreamhelper/lib/inputstreamhelper/config.py
@@ -67,6 +67,14 @@ WIDEVINE_MINIMUM_KODI_VERSION = {
     'Darwin': '18.0'
 }
 
+HARDCODED_CHROMEOS_IMAGE = {
+    'hwid': 'FIEVEL',
+    'url': 'https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13505.73.0_veyron-fievel_recovery_stable-channel_fievel-mp.bin.zip',
+    'sha1': '9904e3141a2537f28469c7653c62200c1b03b057',
+    'version': '13505.73.0',
+    'zipfilesize': '1032442523'
+}
+
 WIDEVINE_VERSIONS_URL = 'https://dl.google.com/widevine-cdm/versions.txt'
 
 WIDEVINE_DOWNLOAD_URL = 'https://dl.google.com/widevine-cdm/{version}-{os}-{arch}.zip'
@@ -82,31 +90,29 @@ CHROMEOS_RECOVERY_URL = 'https://dl.google.com/dl/edgedl/chromeos/recovery/recov
 # To keep the Chrome OS ARM hardware ID list up to date, the following resources can be used:
 # https://www.chromium.org/chromium-os/developer-information-for-chrome-os-devices
 # https://cros-updates-serving.appspot.com/
-# Last updated: 2021-05-10
-# Temporary fix for https://github.com/emilsvennesson/script.module.inputstreamhelper/issues/437
-# Widevine on ARM hardware will probably fail again on June 1st 2021 when Chrome OS 91 will be released: https://chromiumdash.appspot.com/schedule
+# Last updated: 2021-05-05
 CHROMEOS_RECOVERY_ARM_HWIDS = [
-    # 'BOB',
-    # 'BURNET',
-    # 'DAMU',
-    # 'DRUWL',
-    # 'DUMO',
-    # 'ELM',
-    # 'ESCHE',
-    # 'FIEVEL',
-    # 'HANA',
-    # 'JUNIPER-HVPU',
-    # 'KAKADU-WFIQ',
-    # 'KAPPA',
-    # 'KENZO-IGRW',
-    # 'KEVIN',
-    # 'KODAMA',
-    # 'KRANE-ZDKS',
-    # 'LAZOR',
-    # 'POMPOM',
+    'BOB',
+    'BURNET',
+    'DAMU',
+    'DRUWL',
+    'DUMO',
+    'ELM',
+    'ESCHE',
+    'FIEVEL',
+    'HANA',
+    'JUNIPER-HVPU',
+    'KAKADU-WFIQ',
+    'KAPPA',
+    'KENZO-IGRW',
+    'KEVIN',
+    'KODAMA',
+    'KRANE-ZDKS',
+    'LAZOR',
+    'POMPOM',
     'SCARLET',
-    # 'TIGER',
-    # 'WILLOW-TFIY',
+    'TIGER',
+    'WILLOW-TFIY',
 ]
 
 CHROMEOS_BLOCK_SIZE = 512
