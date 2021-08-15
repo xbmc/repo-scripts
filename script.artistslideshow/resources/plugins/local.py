538b1333292a6aac220b39906d8a3f1a0e0350bb
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-08-15 15:37:52 -0400

    [script.artistslideshow] 3.3.2
    - fix for future Python 3.9 compatibility
    - updated language files
    - reformatted Python files (started using Visual Code Studio)

--- script.artistslideshow/resources/plugins/local.py
+++ script.artistslideshow/resources/plugins/local.py
@@ -1,4 +1,4 @@
-#v.0.3.0
+# v.0.3.0
 
 import os
 import defusedxml.ElementTree as _xmltree
@@ -6,105 +6,106 @@ from resources.lib.fileops import readFile, checkPath
 from kodi_six.utils import py2_encode, py2_decode
 
 
-class objectConfig( object ):
-    def __init__( self ):
+class objectConfig(object):
+    def __init__(self):
         self.loglines = []
-        self.BIOFILEPATH = os.path.join( 'override', 'artistbio.nfo' )
-        self.ALBUMFILEPATH = os.path.join( 'override', 'artistsalbums.nfo' )
-        self.SIMILARFILEPATH = os.path.join( 'override', 'artistsimilar.nfo' )
+        self.BIOFILEPATH = os.path.join('override', 'artistbio.nfo')
+        self.ALBUMFILEPATH = os.path.join('override', 'artistsalbums.nfo')
+        self.SIMILARFILEPATH = os.path.join('override', 'artistsimilar.nfo')
 
-
-    def provides( self ):
+    def provides(self):
         return ['bio', 'albums', 'similar', 'mbid']
 
-
-    def getAlbumList( self, album_params ):
+    def getAlbumList(self, album_params):
         self.loglines = []
         albums = []
-        filepath = os.path.join( album_params.get( 'localartistdir', '' ), self.ALBUMFILEPATH )
-        local_path = os.path.join( album_params.get( 'localartistdir', ''), py2_decode( album_params.get( 'artist', '' ) ), 'override' )
-        self.loglines.append( 'checking ' + filepath )
-        rloglines, rawxml = readFile( filepath )
-        self.loglines.extend( rloglines )
+        filepath = os.path.join(album_params.get(
+            'localartistdir', ''), self.ALBUMFILEPATH)
+        local_path = os.path.join(album_params.get('localartistdir', ''), py2_decode(
+            album_params.get('artist', '')), 'override')
+        self.loglines.append('checking ' + filepath)
+        rloglines, rawxml = readFile(filepath)
+        self.loglines.extend(rloglines)
         if rawxml:
-            xmldata = _xmltree.fromstring( py2_encode( rawxml ) )
+            xmldata = _xmltree.fromstring(py2_encode(rawxml))
         else:
             return [], self.loglines
-        for element in xmldata.getiterator():
+        for element in xmldata.iter():
             if element.tag == "name":
-                name = py2_encode( element.text )
+                name = py2_encode(element.text)
             elif element.tag == "image":
                 image_text = element.text
                 if not image_text:
                     image = ''
                 else:
-                    image = os.path.join( local_path, 'albums', image_text )
-                albums.append( ( name , image ) )
+                    image = os.path.join(local_path, 'albums', image_text)
+                albums.append((name, image))
         if albums == []:
-            self.loglines.append( 'no albums found in local xml file' )
+            self.loglines.append('no albums found in local xml file')
             return [], self.loglines
         else:
             return albums, self.loglines
 
-
-    def getBio( self, bio_params ):
+    def getBio(self, bio_params):
         self.loglines = []
         bio = ''
-        filepath = os.path.join( bio_params.get( 'localartistdir', '' ), self.BIOFILEPATH )
-        self.loglines.append( 'checking ' + filepath )
-        loglines, rawxml = readFile( filepath )
-        self.loglines.extend( loglines )
+        filepath = os.path.join(bio_params.get(
+            'localartistdir', ''), self.BIOFILEPATH)
+        self.loglines.append('checking ' + filepath)
+        loglines, rawxml = readFile(filepath)
+        self.loglines.extend(loglines)
         if rawxml:
-            xmldata = _xmltree.fromstring( py2_encode( rawxml ) )
+            xmldata = _xmltree.fromstring(py2_encode(rawxml))
         else:
             return '', self.loglines
-        for element in xmldata.getiterator():
+        for element in xmldata.iter():
             if element.tag == "content":
                 bio = element.text
         if not bio:
-            self.loglines.append( 'no bio found in local xml file' )
+            self.loglines.append('no bio found in local xml file')
             return '', self.loglines
         else:
             return bio, self.loglines
 
-
-    def getMBID( self, mbid_params ):
+    def getMBID(self, mbid_params):
         self.loglines = []
-        filename = os.path.join( mbid_params.get( 'infodir', '' ), 'musicbrainz.nfo' )
-        exists, cloglines = checkPath( filename, False )
-        self.loglines.extend( cloglines )
+        filename = os.path.join(mbid_params.get(
+            'infodir', ''), 'musicbrainz.nfo')
+        exists, cloglines = checkPath(filename, False)
+        self.loglines.extend(cloglines)
         if exists:
-            cloglines, rawdata = readFile( filename )
-            self.loglines.extend( cloglines )
-            return rawdata.rstrip( '\n' ), self.loglines
+            cloglines, rawdata = readFile(filename)
+            self.loglines.extend(cloglines)
+            return rawdata.rstrip('\n'), self.loglines
         else:
             return '', self.loglines
 
-
-    def getSimilarArtists( self, sim_params ):
+    def getSimilarArtists(self, sim_params):
         self.loglines = []
         similar_artists = []
-        filepath = os.path.join( sim_params.get( 'localartistdir', '' ), self.SIMILARFILEPATH )
-        local_path = os.path.join( sim_params.get( 'localartistdir', '' ), py2_decode( sim_params.get( 'artist', '' ) ), 'override' )
-        self.loglines.append( 'checking ' + filepath )
-        rloglines, rawxml = readFile( filepath )
-        self.loglines.extend( rloglines )
+        filepath = os.path.join(sim_params.get(
+            'localartistdir', ''), self.SIMILARFILEPATH)
+        local_path = os.path.join(sim_params.get('localartistdir', ''), py2_decode(
+            sim_params.get('artist', '')), 'override')
+        self.loglines.append('checking ' + filepath)
+        rloglines, rawxml = readFile(filepath)
+        self.loglines.extend(rloglines)
         if rawxml:
-            xmldata = _xmltree.fromstring( py2_encode( rawxml ) )
+            xmldata = _xmltree.fromstring(py2_encode(rawxml))
         else:
             return [], self.loglines
-        for element in xmldata.getiterator():
+        for element in xmldata.iter():
             if element.tag == "name":
-                name = py2_encode( element.text )
+                name = py2_encode(element.text)
             elif element.tag == "image":
                 image_text = element.text
                 if not image_text:
                     image = ''
                 else:
-                    image = os.path.join( local_path, 'similar', image_text )
-                similar_artists.append( ( name , image ) )
+                    image = os.path.join(local_path, 'similar', image_text)
+                similar_artists.append((name, image))
         if similar_artists == []:
-            self.loglines.append( 'no similar artists found in local xml file' )
+            self.loglines.append('no similar artists found in local xml file')
             return [], self.loglines
         else:
             return similar_artists, self.loglines
--- script.artistslideshow/resources/plugins/theaudiodb.py
+++ script.artistslideshow/resources/plugins/theaudiodb.py
@@ -1,6 +1,9 @@
-#v.0.3.0
+# v.0.3.0
 
-import base64, os, time, random
+import base64
+import os
+import time
+import random
 from kodi_six import xbmcvfs
 from resources.lib.url import URL
 from resources.lib.fileops import readFile, writeFile, deleteFile, checkPath
@@ -16,11 +19,11 @@ except AttributeError:
     clowncar = ''
 
 
-
-class objectConfig( object ):
-    def __init__( self ):
-        url = 'https://www.theaudiodb.com/api/v1/json/%s/' % base64.b64decode(clowncar.encode('ascii')).decode('ascii')
-        secsinweek = int( 7 * 24 * 60 * 60 )
+class objectConfig(object):
+    def __init__(self):
+        url = 'https://www.theaudiodb.com/api/v1/json/%s/' % base64.b64decode(
+            clowncar.encode('ascii')).decode('ascii')
+        secsinweek = int(7 * 24 * 60 * 60)
         self.ARTISTMBIDURL = url + 'artist-mb.php'
         self.ARTISTSEARCHURL = url + 'search.php'
         self.ARTISTTADBIDURL = url + 'artist.php'
@@ -33,232 +36,246 @@ class objectConfig( object ):
         self.ALBUMCACHETIMEFILENAME = 'theaudiodbalbumcachetime.nfo'
         self.HASDONATION = False
         self.CACHEEXPIRE = {}
-        self.CACHEEXPIRE['low'] = int( 1 * secsinweek )
-        self.CACHEEXPIRE['high'] = int( 2 * secsinweek )
-        self.CACHEEXPIREWITHDONATION = int( secsinweek / 7 )
+        self.CACHEEXPIRE['low'] = int(1 * secsinweek)
+        self.CACHEEXPIRE['high'] = int(2 * secsinweek)
+        self.CACHEEXPIREWITHDONATION = int(secsinweek / 7)
         self.LOGLINES = []
-        self.JSONURL = URL( 'json' )
+        self.JSONURL = URL('json')
 
-
-    def provides( self ):
+    def provides(self):
         return ['bio', 'albums', 'images', 'mbid']
 
-
-    def getAlbumList( self, album_params ):
+    def getAlbumList(self, album_params):
         self.LOGLINES = []
-        self._set_filepaths( album_params )
+        self._set_filepaths(album_params)
         url_params = {}
         albums = []
         json_data = ''
-        self._check_donation( album_params.get( 'donated', False ) )
-        url, url_params = self._determine_url( album_params, '', self.ALBUMURL, self.ALBUMSEARCHURL )
+        self._check_donation(album_params.get('donated', False))
+        url, url_params = self._determine_url(
+            album_params, '', self.ALBUMURL, self.ALBUMSEARCHURL)
         if url:
-            json_data = self._get_data( self.ALBUMFILEPATH, self.ALBUMCACHEFILEPATH, url, url_params )
+            json_data = self._get_data(
+                self.ALBUMFILEPATH, self.ALBUMCACHEFILEPATH, url, url_params)
         if json_data:
-            rawalbums = json_data.get( 'album' )
+            rawalbums = json_data.get('album')
             if rawalbums is not None:
                 for album in rawalbums:
-                    albums.append( ( album.get( 'strAlbum', '' ), album.get( 'strAlbumThumb', '' ) ) )
+                    albums.append((album.get('strAlbum', ''),
+                                  album.get('strAlbumThumb', '')))
         return albums, self.LOGLINES
 
-
-    def getBio( self, bio_params ):
+    def getBio(self, bio_params):
         self.LOGLINES = []
-        self._set_filepaths( bio_params )
+        self._set_filepaths(bio_params)
         url_params = {}
         bio = ''
         json_data = ''
-        self._check_donation( bio_params.get( 'donated', False ) )
-        url, url_params = self._determine_url( bio_params, self.ARTISTMBIDURL, self.ARTISTTADBIDURL, self.ARTISTSEARCHURL )
+        self._check_donation(bio_params.get('donated', False))
+        url, url_params = self._determine_url(
+            bio_params, self.ARTISTMBIDURL, self.ARTISTTADBIDURL, self.ARTISTSEARCHURL)
         if url:
-            json_data = self._get_data( self.ARTISTFILEPATH, self.CACHEFILEPATH, url, url_params )
-            self.LOGLINES.extend( ['the json data is:', json_data] )
+            json_data = self._get_data(
+                self.ARTISTFILEPATH, self.CACHEFILEPATH, url, url_params)
+            self.LOGLINES.extend(['the json data is:', json_data])
         if json_data:
-            artist = json_data.get( 'artists' )
+            artist = json_data.get('artists')
             if artist is not None:
-                bio = artist[0].get( 'strBiography' + bio_params.get( 'lang', '' ).upper(), '' )
+                bio = artist[0].get(
+                    'strBiography' + bio_params.get('lang', '').upper(), '')
         return bio, self.LOGLINES
 
-
-    def getImageList( self, img_params ):
+    def getImageList(self, img_params):
         self.LOGLINES = []
-        self._set_filepaths( img_params )
+        self._set_filepaths(img_params)
         url_params = {}
         images = []
         json_data = ''
-        self._check_donation( img_params.get( 'donated', False ) )
-        url, url_params = self._determine_url( img_params, self.ARTISTMBIDURL, self.ARTISTTADBIDURL, self.ARTISTSEARCHURL )
+        self._check_donation(img_params.get('donated', False))
+        url, url_params = self._determine_url(
+            img_params, self.ARTISTMBIDURL, self.ARTISTTADBIDURL, self.ARTISTSEARCHURL)
         if url:
-            json_data = self._get_data( self.ARTISTFILEPATH, self.CACHEFILEPATH, url, url_params )
+            json_data = self._get_data(
+                self.ARTISTFILEPATH, self.CACHEFILEPATH, url, url_params)
         if json_data:
-            artist = json_data.get( 'artists' )
+            artist = json_data.get('artists')
             if artist is not None:
-                for count in range( 3 ):
+                for count in range(3):
                     if count == 0:
                         num = ''
                     else:
-                        num = str( count + 1 )
-                    image = artist[0].get( 'strArtistFanart' + num, '' )
+                        num = str(count + 1)
+                    image = artist[0].get('strArtistFanart' + num, '')
                     if image:
-                        images.append( image )
-                if img_params.get( 'getall', False ):
-                    image = artist[0].get( 'strArtistThumb' )
+                        images.append(image)
+                if img_params.get('getall', False):
+                    image = artist[0].get('strArtistThumb')
                     if image:
-                        images.append( image )
+                        images.append(image)
         return images, self.LOGLINES
 
-
-    def getMBID( self, mbid_params ):
+    def getMBID(self, mbid_params):
         self.LOGLINES = []
-        self._set_filepaths( mbid_params )
-        exists, cloglines = checkPath( self.ARTISTFILEPATH, False )
-        self.LOGLINES.extend( cloglines )
+        self._set_filepaths(mbid_params)
+        exists, cloglines = checkPath(self.ARTISTFILEPATH, False)
+        self.LOGLINES.extend(cloglines)
         if exists:
-            cloglines, rawdata = readFile( self.ARTISTFILEPATH )
-            self.LOGLINES.extend( cloglines )
+            cloglines, rawdata = readFile(self.ARTISTFILEPATH)
+            self.LOGLINES.extend(cloglines)
             try:
-                json_data = _json.loads( rawdata )
+                json_data = _json.loads(rawdata)
             except ValueError:
-                self.LOGLINES.append( 'no valid JSON data returned from ' + self.ARTISTFILEPATH )
+                self.LOGLINES.append(
+                    'no valid JSON data returned from ' + self.ARTISTFILEPATH)
                 return '', self.LOGLINES
-            self.LOGLINES.append( 'musicbrainz ID found in %s file' % self.ARTISTFILEPATH )
+            self.LOGLINES.append(
+                'musicbrainz ID found in %s file' % self.ARTISTFILEPATH)
             try:
-                return json_data.get( 'artists' )[0].get( 'strMusicBrainzID', '' ), self.LOGLINES
+                return json_data.get('artists')[0].get('strMusicBrainzID', ''), self.LOGLINES
             except TypeError:
-                self.LOGLINES.append( 'error reading musicbrainz ID from ' + self.ARTISTFILEPATH )
+                self.LOGLINES.append(
+                    'error reading musicbrainz ID from ' + self.ARTISTFILEPATH)
                 return '', self.LOGLINES
         else:
             return '', self.LOGLINES
 
-
-    def _check_donation( self, donation ):
+    def _check_donation(self, donation):
         if donation:
             self.HASDONATION = True
             self.CACHEEXPIRE['low'] = self.CACHEEXPIREWITHDONATION
             self.CACHEEXPIRE['high'] = self.CACHEEXPIREWITHDONATION
 
-
-    def _determine_url( self, params, mbidurl, tadbidurl, nameurl ):
+    def _determine_url(self, params, mbidurl, tadbidurl, nameurl):
         url_params = {}
         if mbidurl:
-            mbid = params.get( 'mbid', '' )
+            mbid = params.get('mbid', '')
             if mbid:
-                url_params['i'] = params.get( 'mbid', '' )
-                self.LOGLINES.append( 'found mbid, using mbidurl to get information from theaudiodb' )
+                url_params['i'] = params.get('mbid', '')
+                self.LOGLINES.append(
+                    'found mbid, using mbidurl to get information from theaudiodb')
                 return mbidurl, url_params
         if tadbidurl:
-            tadbid = self._get_audiodbid( )
+            tadbid = self._get_audiodbid()
             if tadbid:
                 url_params['i'] = tadbid
-                self.LOGLINES.append( 'found tadbid, using tadbidurl to get information from theaudiodb' )
+                self.LOGLINES.append(
+                    'found tadbid, using tadbidurl to get information from theaudiodb')
                 return tadbidurl, url_params
         if nameurl:
-            url_params['s'] = params.get( 'artist', '' )
-            self.LOGLINES.append( 'no mbid or tadbid found, using artist name to get information from theaudiodb' )
+            url_params['s'] = params.get('artist', '')
+            self.LOGLINES.append(
+                'no mbid or tadbid found, using artist name to get information from theaudiodb')
             return nameurl, url_params
         return '', ''
 
-
-    def _get_audiodbid( self ):
+    def _get_audiodbid(self):
         audiodbid = ''
-        exists, cloglines = checkPath( self.IDFILEPATH, False )
-        self.LOGLINES.extend( cloglines )
+        exists, cloglines = checkPath(self.IDFILEPATH, False)
+        self.LOGLINES.extend(cloglines)
         if not exists:
-            exists, cloglines = checkPath( self.ARTISTFILEPATH, False )
-            self.LOGLINES.extend( cloglines )
+            exists, cloglines = checkPath(self.ARTISTFILEPATH, False)
+            self.LOGLINES.extend(cloglines)
             if exists:
-                rloglines, rawdata = readFile( self.ARTISTFILEPATH )
-                self.LOGLINES.extend( rloglines )
+                rloglines, rawdata = readFile(self.ARTISTFILEPATH)
+                self.LOGLINES.extend(rloglines)
                 try:
                     gotData = True
-                    json_data = _json.loads( rawdata )
+                    json_data = _json.loads(rawdata)
                 except ValueError:
-                    self.LOGLINES.append( 'no valid JSON data returned from theaudiodb.com, setting artist to None' )
+                    self.LOGLINES.append(
+                        'no valid JSON data returned from theaudiodb.com, setting artist to None')
                     gotData = False
                 if gotData:
-                    artist = json_data.get( 'artists' )
+                    artist = json_data.get('artists')
                 else:
                     artist = None
                 if artist is not None:
-                    audiodbid = artist[0].get( 'idArtist', '' )
+                    audiodbid = artist[0].get('idArtist', '')
                 if audiodbid:
-                    success, wloglines = writeFile( audiodbid, self.IDFILEPATH )
-                    self.LOGLINES.extend( wloglines )
-        rloglines, audiodbid = readFile( self.IDFILEPATH )
-        self.LOGLINES.extend( rloglines )
+                    success, wloglines = writeFile(audiodbid, self.IDFILEPATH)
+                    self.LOGLINES.extend(wloglines)
+        rloglines, audiodbid = readFile(self.IDFILEPATH)
+        self.LOGLINES.extend(rloglines)
         return audiodbid
 
-
-    def _get_cache_time( self, cachefilepath ):
+    def _get_cache_time(self, cachefilepath):
         rawdata = ''
-        self.LOGLINES.append( 'getting the cache timeout information for theaudiodb' )
-        exists, cloglines = checkPath( cachefilepath, False )
-        self.LOGLINES.extend( cloglines )
+        self.LOGLINES.append(
+            'getting the cache timeout information for theaudiodb')
+        exists, cloglines = checkPath(cachefilepath, False)
+        self.LOGLINES.extend(cloglines)
         if exists:
             success = True
         else:
-            success = self._put_cache_time( cachefilepath )
+            success = self._put_cache_time(cachefilepath)
         if success:
-            rloglines, rawdata = readFile( cachefilepath )
-            self.LOGLINES.extend( rloglines )
+            rloglines, rawdata = readFile(cachefilepath)
+            self.LOGLINES.extend(rloglines)
         try:
-            cachetime = int( rawdata )
+            cachetime = int(rawdata)
         except ValueError:
             cachetime = 0
         return cachetime
 
-
-    def _get_data( self, filepath, cachefilepath, url, url_params ):
+    def _get_data(self, filepath, cachefilepath, url, url_params):
         json_data = ''
-        if self._update_cache( filepath, cachefilepath ):
-            success, uloglines, json_data = self.JSONURL.Get( url, params=url_params )
-            self.LOGLINES.extend( uloglines )
+        if self._update_cache(filepath, cachefilepath):
+            success, uloglines, json_data = self.JSONURL.Get(
+                url, params=url_params)
+            self.LOGLINES.extend(uloglines)
             if success:
-                success, wloglines = writeFile( py2_encode( _json.dumps( json_data ) ), filepath )
-                self.LOGLINES.extend( wloglines )
-        exists, cloglines = checkPath( filepath, False )
-        self.LOGLINES.extend( cloglines )
+                success, wloglines = writeFile(
+                    py2_encode(_json.dumps(json_data)), filepath)
+                self.LOGLINES.extend(wloglines)
+        exists, cloglines = checkPath(filepath, False)
+        self.LOGLINES.extend(cloglines)
         if exists:
-            self._get_audiodbid( ) # this is to generate the id file if it doesn't exist
-            rloglines, rawdata = readFile( filepath )
-            self.LOGLINES.extend( rloglines )
+            self._get_audiodbid()  # this is to generate the id file if it doesn't exist
+            rloglines, rawdata = readFile(filepath)
+            self.LOGLINES.extend(rloglines)
             try:
-                json_data = _json.loads( rawdata )
+                json_data = _json.loads(rawdata)
             except ValueError:
-                success, dloglines = deleteFile( filepath )
-                self.LOGLINES.extend( dloglines )
-                self.LOGLINES.append( 'Deleted old cache file. New file will be download on next run.' )
+                success, dloglines = deleteFile(filepath)
+                self.LOGLINES.extend(dloglines)
+                self.LOGLINES.append(
+                    'Deleted old cache file. New file will be download on next run.')
                 json_data = ''
         return json_data
 
-
-    def _put_cache_time( self, cachefilepath ):
-        cachetime = random.randint( self.CACHEEXPIRE['low'], self.CACHEEXPIRE['high'] )
-        success, wloglines = writeFile( str( cachetime ), cachefilepath )
-        self.LOGLINES.append( wloglines)
+    def _put_cache_time(self, cachefilepath):
+        cachetime = random.randint(
+            self.CACHEEXPIRE['low'], self.CACHEEXPIRE['high'])
+        success, wloglines = writeFile(str(cachetime), cachefilepath)
+        self.LOGLINES.append(wloglines)
         return success
 
+    def _set_filepaths(self, params):
+        self.ARTISTFILEPATH = os.path.join(
+            params.get('infodir', ''), self.ARTISTFILENAME)
+        self.CACHEFILEPATH = os.path.join(
+            params.get('infodir', ''), self.CACHETIMEFILENAME)
+        self.ALBUMCACHEFILEPATH = os.path.join(
+            params.get('infodir', ''), self.ALBUMCACHETIMEFILENAME)
+        self.ALBUMFILEPATH = os.path.join(
+            params.get('infodir', ''), self.ALBUMFILENAME)
+        self.IDFILEPATH = os.path.join(
+            params.get('infodir', ''), self.IDFILENAME)
+        self.EXCLUSIONFILEPATH = params.get('exclusionsfile', '')
 
-    def _set_filepaths( self, params ):
-        self.ARTISTFILEPATH = os.path.join( params.get( 'infodir', '' ), self.ARTISTFILENAME )
-        self.CACHEFILEPATH = os.path.join( params.get( 'infodir', '' ), self.CACHETIMEFILENAME )
-        self.ALBUMCACHEFILEPATH = os.path.join( params.get( 'infodir', '' ), self.ALBUMCACHETIMEFILENAME )
-        self.ALBUMFILEPATH = os.path.join( params.get( 'infodir', '' ), self.ALBUMFILENAME )
-        self.IDFILEPATH = os.path.join( params.get( 'infodir', '' ), self.IDFILENAME )
-        self.EXCLUSIONFILEPATH = params.get( 'exclusionsfile', '' )
-
-
-    def _update_cache( self, filepath, cachefilepath ):
-        exists, cloglines = checkPath( filepath, False )
-        self.LOGLINES.extend( cloglines )
+    def _update_cache(self, filepath, cachefilepath):
+        exists, cloglines = checkPath(filepath, False)
+        self.LOGLINES.extend(cloglines)
         if exists:
-            st = xbmcvfs.Stat( filepath )
-            if time.time() - st.st_mtime() < self._get_cache_time( cachefilepath ):
-                self.LOGLINES.append( 'cached info found for theaudiodb' )
+            st = xbmcvfs.Stat(filepath)
+            if time.time() - st.st_mtime() < self._get_cache_time(cachefilepath):
+                self.LOGLINES.append('cached info found for theaudiodb')
                 return False
             else:
-                self.LOGLINES.append( 'outdated cached info found for theaudiodb' )
-                return self._put_cache_time( cachefilepath )
+                self.LOGLINES.append(
+                    'outdated cached info found for theaudiodb')
+                return self._put_cache_time(cachefilepath)
         else:
-            self.LOGLINES.append( 'no theaudiodb cachetime file found, creating it' )
-            return self._put_cache_time( cachefilepath )
+            self.LOGLINES.append(
+                'no theaudiodb cachetime file found, creating it')
+            return self._put_cache_time(cachefilepath)
