5fad9fc0bceaf415573bd8d767f1e1348388a02d
bossanova808 <jd@imagescience.com.au>
2021-08-28 04:22:36 +0000

    [script.service.playbackresumer] 2.0.3

--- script.service.playbackresumer/resources/lib/store.py
+++ script.service.playbackresumer/resources/lib/store.py
@@ -76,6 +76,23 @@ class Store:
         log(f"Using ignoresecondsatstart: {Store.ignore_seconds_at_start}, ignorepercentatend: {Store.ignore_percent_at_end}")
 
     @staticmethod
+    def clear_old_play_details():
+        """
+        As soon as a new file is played, clear out all old references to anything that was being stored as the currently playing file
+        :return:
+        """
+        log("New playback - clearing legacy now playing details")
+        Store.library_id = None
+        Store.currently_playing_file_path = None
+        Store.type_of_video = None
+        Store.paused_time = None
+        Store.length_of_currently_playing_file = None
+        with open(Store.file_to_store_last_played, 'w+') as f:
+            f.write('')
+        with open(Store.file_to_store_resume_point, 'w+') as f:
+            f.write('')
+
+    @staticmethod
     def load_config_from_settings():
         """
         Load in the addon settings, at start or reload them if they have been changed
