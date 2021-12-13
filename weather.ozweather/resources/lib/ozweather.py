10a98afe4913980a5ebcf691ac8ecf1d735ce67e
bossanova808 <jd@imagescience.com.au>
2021-12-13 00:37:54 +0000

    [weather.ozweather] 2.0.4

--- weather.ozweather/resources/lib/ozweather.py
+++ weather.ozweather/resources/lib/ozweather.py
@@ -4,7 +4,6 @@ import socket
 from .forecast import *
 from .locations import *
 from .bom.bom_location import *
-from .weatherzone.weatherzone_location import *
 
 
 def run(args):
@@ -18,35 +17,10 @@ def run(args):
     footprints()
     socket.setdefaulttimeout(100)
 
-    # Translate old, pre BOM api settings to the new equivalents
-    # The old Weatherzone locations now become the fallback settings
-    # (After a transition period (say after the Kodi N* release) - this should be removed)
-    if ADDON.getSetting('Location1'):
-        ADDON.setSetting('Location1Weatherzone', ADDON.getSetting('Location1'))
-        ADDON.setSetting('Location1', '')
-    if ADDON.getSetting('Location2'):
-        ADDON.setSetting('Location2Weatherzone', ADDON.getSetting('Location2'))
-        ADDON.setSetting('Location2', '')
-    if ADDON.getSetting('Location3'):
-        ADDON.setSetting('Location3Weatherzone', ADDON.getSetting('Location3'))
-        ADDON.setSetting('Location3', '')
-    if ADDON.getSetting('Location1UrlPath'):
-        ADDON.setSetting('Location1WeatherzoneUrlPath', ADDON.getSetting('Location1UrlPath'))
-        ADDON.setSetting('Location1UrlPath', '')
-    if ADDON.getSetting('Location2UrlPath'):
-        ADDON.setSetting('Location2WeatherzoneUrlPath', ADDON.getSetting('Location2UrlPath'))
-        ADDON.setSetting('Location2UrlPath', '')
-    if ADDON.getSetting('Location3UrlPath'):
-        ADDON.setSetting('Location3WeatherzoneUrlPath', ADDON.getSetting('Location3UrlPath'))
-        ADDON.setSetting('Location3UrlPath', '')
-
     # RUN MODE - ADDON CALLED FORM Kodi SETTINGS
     # the addon is being called from the settings section where the user enters their postcodes
     if args[1].startswith('Location'):
-        if args[1].endswith('WeatherZone'):
-            find_weatherzone_location()
-        else:
-            find_bom_location()
+        find_bom_location()
 
     # RUN MODE - GET WEATHER OBSERVATIONS AND FORECAST
     # script is being called in general use, not from the settings page
