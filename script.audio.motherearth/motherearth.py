37f015879d41b580a41184f8a6b8cc1cecc977cf
FlorianReiterer <59761265+FlorianReiterer@users.noreply.github.com>
2022-01-11 13:30:48 +0100

    [script.audio.motherearth] 1.0 (#2188)

---
+++ script.audio.motherearth/motherearth.py
@@ -0,0 +1,116 @@
+import re
+import time
+
+import requests
+
+
+BREAK_COVER_URL = 'https://motherearthradio.de/images/folder.jpg'
+BREAK_KEY = ('429hz', 'High Resolution')
+
+#COVER_URL = 'https://img.radioparadise.com/{}'
+
+KEY_FILTER_RE = re.compile(r'[^\w\']+')
+
+NOWPLAYING_URL = 'http://server9.streamserver24.com:9090/api/nowplaying/motherearth'
+
+#SLIDESHOW_URL = 'https://img.radioparadise.com/slideshow/720/{}.jpg'
+
+STREAMS = [
+    {
+        'channel': 0,
+        'title': 'Mother Earth',
+           'url_aac': 'http://server9.streamserver24.com:18900/motherearth.aac',
+           'url_flac': 'http://server9.streamserver24.com:18900/motherearth',
+    },
+    {
+        'channel': 1,
+        'title': 'Mother Earth Klassik',
+           'url_aac': 'http://server9.streamserver24.com:18900/motherearth.klassik.aac',
+           'url_flac': 'http://server9.streamserver24.com:18900/motherearth.klassik',
+    },
+    {
+        'channel': 2,
+        'title': 'Mother Earth Instrumental',
+           'url_aac': 'http://server9.streamserver24.com:18900/motherearth.instrumental.aac',
+           'url_flac': 'http://server9.streamserver24.com:18900/motherearth.instrumental',
+    },
+]
+STREAM_INFO = {s['url_aac']: s for s in STREAMS}
+STREAM_INFO.update({s['url_flac']: s for s in STREAMS})
+
+
+class NowPlaying():
+    """Provides song information from the "nowplaying" API."""
+
+    def __init__(self):
+        """Constructor"""
+        self.set_channel(None)
+
+    @property
+    def current(self):
+        """Return a dict for the current "nowplaying" song, or None.
+
+        The "cover" value will be an absolute URL.
+        """
+        return self._current
+
+    def get_song_data(self, song_key):
+        """Return a dict for the (artist, title) key, or None.
+
+        The "cover" value will be an absolute URL.
+        """
+        key = build_key(song_key)
+        return self.songs.get(key)
+
+    def set_channel(self, channel):
+        """Set the channel number, or None."""
+        if channel is not None:
+            self.url = NOWPLAYING_URL
+        else:
+            self.url = None
+        self._current = None
+        self.next_update = 0
+        self.songs = {}
+
+    def update(self):
+        """Update song information from the API, if necessary.
+
+        Calls the API only if the "refresh" timer has expired.
+
+        Raises an exception on error responses or timeouts.
+        """
+        if self.url is None:
+            return
+        now = time.time()
+        if now < self.next_update:
+            return
+        res = requests.get(self.url, timeout=2)
+        res.raise_for_status()
+        data = res.json()
+        current = None
+        songs = {}
+        for index, song in data['song'].items():
+            song['cover'] = COVER_URL
+            key = build_key((song['artist'], song['title']))
+            songs[key] = song
+            if index == '0':
+                current = song
+        if BREAK_KEY not in songs:
+            key = build_key(BREAK_KEY)
+            songs[key] = {
+                'artist': BREAK_KEY[0],
+                'cover': BREAK_COVER_URL,
+                'title': BREAK_KEY[1],
+            }
+        self._current = current
+        self.songs = songs
+        self.next_update = now + data['refresh']
+
+
+def build_key(strings):
+    """Return a normalized tuple of words in the strings."""
+    result = []
+    for s in strings:
+        words = KEY_FILTER_RE.sub(' ', s).casefold().split()
+        result.extend(words)
+    return tuple(sorted(result))
---
+++ script.audio.motherearth/service.py
@@ -0,0 +1,169 @@
+from collections import namedtuple
+import time
+
+import requests
+import xbmc
+import xbmcaddon
+import xbmcgui
+
+from motherearth import STREAM_INFO, NowPlaying
+
+
+RESTART_INTERVAL = 1.0
+RESTART_TIMEOUT = 1.0
+
+
+Song = namedtuple('Song', 'data cover fanart')
+
+
+class Player(xbmc.Player):
+    """Adds xbmc.Player callbacks and integrates with the API."""
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.last_key = None
+        self.last_song = None
+        self.stream_url = None
+        self.restart_time = 0
+        self.now_playing = NowPlaying()
+
+    def get_song_key(self):
+        """Return (artist, title) for the current song, or None."""
+        result = None
+        if self.isPlayingAudio():
+            try:
+                info = self.getMusicInfoTag()
+                result = (info.getArtist(), info.getTitle())
+            except Exception:
+                pass
+        return result
+
+    def reset(self):
+        """Reset internal state when not playing."""
+        self.last_key = None
+        self.last_song = None
+        self.stream_url = None
+        self.restart_time = 0
+        self.now_playing.set_channel(None)
+        self.slideshow.set_slides(None)
+
+    def restart(self):
+        """Restart playback, if necessary."""
+        now = time.time()
+        if not self.restart_time or now < self.restart_time:
+            return
+        try:
+            res = requests.head(self.stream_url, timeout=RESTART_TIMEOUT)
+            do_restart = res.status_code == 200
+        except Exception:
+            do_restart = False
+        if do_restart:
+            self.restart_time = 0
+            self.play(self.stream_url)
+        else:
+            self.restart_time = now + RESTART_INTERVAL
+
+    def update(self):
+        """Perform updates."""
+        if self.restart_time:
+            self.restart()
+        elif self.stream_url:
+            self.now_playing.update()
+#      self.update_slideshow()
+            self.update_song()
+
+    def update_player(self):
+        """Update the Kodi player with song metadata."""
+        song = self.last_song
+        if song and self.isPlayingAudio():
+            info = {
+                'artist': song.data['artist'],
+                'title': song.data['title'],
+                'genre': '',
+            }
+            if 'album' in song.data:
+                info['album'] = song.data['album']
+            if 'rating' in song.data:
+                rating = float(song.data['rating'])
+                info['rating'] = rating
+                info['userrating'] = int(round(rating))
+            if 'year' in song.data:
+                info['year'] = int(song.data['year'])
+            item = xbmcgui.ListItem()
+            item.setPath(self.getPlayingFile())
+            item.setArt({'thumb': song.cover})
+            item.setArt({'fanart': song.fanart})
+            item.setInfo('music', info)
+            self.updateInfoTag(item)
+
+    def update_song(self):
+        """Update song metadata, if necessary."""
+        last_key = self.last_key
+        last_song = self.last_song
+        song_key = self.get_song_key()
+        if song_key is None or song_key == last_key:
+            return
+
+        if song_key != ('', ''):
+            xbmc.log(f'rp_service: song_key {song_key}', xbmc.LOGDEBUG)
+            song_data = self.now_playing.get_song_data(song_key)
+            if song_data:
+                self.last_key = song_key
+        else:
+            song_data = self.now_playing.current
+        if song_data is None or last_song and last_song.data == song_data:
+            return
+
+        cover = song_data.get('cover')
+        xbmc.log(f'me_service: cover {cover}', xbmc.LOGDEBUG)
+
+        addon = xbmcaddon.Addon()
+
+        self.last_song = Song(song_data, cover, fanart)
+        self.update_player()
+
+    def onAVStarted(self):
+        if self.isPlaying() and self.getPlayingFile() in STREAM_INFO:
+            url = self.getPlayingFile()
+            info = STREAM_INFO[url]
+            # Kodi switches to fullscreen for FLAC, but not AAC
+            if url == info['url_aac']:
+                xbmc.executebuiltin('Action(FullScreen)')
+        else:
+            self.reset()
+
+    def onPlayBackEnded(self):
+        if self.stream_url:
+            self.restart_time = time.time()
+        else:
+            self.reset()
+
+    def onPlayBackError(self):
+        self.reset()
+
+    def onPlayBackStarted(self):
+        if self.isPlaying() and self.getPlayingFile() in STREAM_INFO:
+            url = self.getPlayingFile()
+            self.stream_url = url
+            self.restart_time = 0
+            info = STREAM_INFO[url]
+            self.now_playing.set_channel(info['channel'])
+        else:
+            self.reset()
+
+    def onPlayBackStopped(self):
+        self.reset()
+
+
+
+if __name__ == '__main__':
+    player = Player()
+    monitor = xbmc.Monitor()
+    while not monitor.abortRequested():
+        if monitor.waitForAbort(1):
+            break
+        try:
+            player.update()
+        except Exception as e:
+            xbmc.log(f'mer_service: {e}', xbmc.LOGERROR)
