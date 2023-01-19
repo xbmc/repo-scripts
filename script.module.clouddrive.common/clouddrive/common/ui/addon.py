b4f64234b7a0a50903c868f183fbc9c7947c104f
Martin Vician <Martin.Vician@ibm.com>
2023-01-19 18:29:39 +0000

    [script.module.clouddrive.common] 1.3.10

--- script.module.clouddrive.common/clouddrive/common/ui/addon.py
+++ script.module.clouddrive.common/clouddrive/common/ui/addon.py
@@ -699,7 +699,7 @@ class CloudDriveAddon:
                 self._rename_action()
                 method = getattr(self, self._action)
                 arguments = {}
-                for name in inspect.getargspec(method)[0]:
+                for name in inspect.getfullargspec(method)[0]:
                     if name in self._addon_params:
                         arguments[name] = self._addon_params[name]
                 method(**arguments)
