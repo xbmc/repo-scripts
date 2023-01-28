673cad39c62b1e6a92e863974b0f7ee28ee10965
jurialmunkey <jurialmunkey@kodi.tv>
2023-01-29 03:54:41 +1100

    [script.module.infotagger] v0.0.4 (#2385)

--- script.module.infotagger/resources/modules/infotagger/listitem.py
+++ script.module.infotagger/resources/modules/infotagger/listitem.py
@@ -11,104 +11,118 @@ class ListItemInfoTag():
         'video': {
             'tag_getter': 'getVideoInfoTag',
             'tag_attr': {
-                'genre': {'attr': 'setGenres', 'convert': lambda x: [x]},
-                'country': {'attr': 'setCountries', 'convert': lambda x: [x]},
-                'year': {'attr': 'setYear', 'convert': int},
-                'episode': {'attr': 'setEpisode', 'convert': int},
-                'season': {'attr': 'setSeason', 'convert': int},
-                'sortepisode': {'attr': 'setSortEpisode', 'convert': int},
-                'sortseason': {'attr': 'setSortSeason', 'convert': int},
-                'episodeguide': {'attr': 'setEpisodeGuide', 'convert': str},
-                'showlink': {'attr': 'setShowLinks', 'convert': lambda x: [x]},
-                'top250': {'attr': 'setTop250', 'convert': int},
-                'setid': {'attr': 'setSetId', 'convert': int},
-                'tracknumber': {'attr': 'setTrackNumber', 'convert': int},
-                'rating': {'attr': 'setRating', 'convert': float},
-                'userrating': {'attr': 'setUserRating', 'convert': int},
+                'genre': {'attr': 'setGenres', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'country': {'attr': 'setCountries', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'year': {'attr': 'setYear', 'convert': int, 'classinfo': int},
+                'episode': {'attr': 'setEpisode', 'convert': int, 'classinfo': int},
+                'season': {'attr': 'setSeason', 'convert': int, 'classinfo': int},
+                'sortepisode': {'attr': 'setSortEpisode', 'convert': int, 'classinfo': int},
+                'sortseason': {'attr': 'setSortSeason', 'convert': int, 'classinfo': int},
+                'episodeguide': {'attr': 'setEpisodeGuide', 'convert': str, 'classinfo': str},
+                'showlink': {'attr': 'setShowLinks', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'top250': {'attr': 'setTop250', 'convert': int, 'classinfo': int},
+                'setid': {'attr': 'setSetId', 'convert': int, 'classinfo': int},
+                'tracknumber': {'attr': 'setTrackNumber', 'convert': int, 'classinfo': int},
+                'rating': {'attr': 'setRating', 'convert': float, 'classinfo': float},
+                'userrating': {'attr': 'setUserRating', 'convert': int, 'classinfo': int},
                 'watched': {'skip': True},  # Evaluated internally in Nexus based on playcount so skip
-                'playcount': {'attr': 'setPlaycount', 'convert': int},
+                'playcount': {'attr': 'setPlaycount', 'convert': int, 'classinfo': int},
                 'overlay': {'skip': True},  # Evaluated internally in Nexus based on playcount so skip
                 'cast': {'route': 'set_info_cast'},
                 'castandrole': {'route': 'set_info_cast'},
-                'director': {'attr': 'setDirectors', 'convert': lambda x: [x]},
-                'mpaa': {'attr': 'setMpaa', 'convert': str},
-                'plot': {'attr': 'setPlot', 'convert': str},
-                'plotoutline': {'attr': 'setPlotOutline', 'convert': str},
-                'title': {'attr': 'setTitle', 'convert': str},
-                'originaltitle': {'attr': 'setOriginalTitle', 'convert': str},
-                'sorttitle': {'attr': 'setSortTitle', 'convert': str},
-                'duration': {'attr': 'setDuration', 'convert': int},
-                'studio': {'attr': 'setStudios', 'convert': lambda x: [x]},
-                'tagline': {'attr': 'setTagLine', 'convert': str},
-                'writer': {'attr': 'setWriters', 'convert': lambda x: [x]},
-                'tvshowtitle': {'attr': 'setTvShowTitle', 'convert': str},
-                'premiered': {'attr': 'setPremiered', 'convert': str},
-                'status': {'attr': 'setTvShowStatus', 'convert': str},
-                'set': {'attr': 'setSet', 'convert': str},
-                'setoverview': {'attr': 'setSetOverview', 'convert': str},
-                'tag': {'attr': 'setTags', 'convert': lambda x: [x]},
-                'imdbnumber': {'attr': 'setIMDBNumber', 'convert': str},
-                'code': {'attr': 'setProductionCode', 'convert': str},
-                'aired': {'attr': 'setFirstAired', 'convert': str},
-                'credits': {'attr': 'setWriters', 'convert': lambda x: [x]},
-                'lastplayed': {'attr': 'setLastPlayed', 'convert': str},
-                'album': {'attr': 'setAlbum', 'convert': str},
-                'artist': {'attr': 'setArtists', 'convert': lambda x: [x]},
-                'votes': {'attr': 'setVotes', 'convert': int},
-                'path': {'attr': 'setPath', 'convert': str},
-                'trailer': {'attr': 'setTrailer', 'convert': str},
-                'dateadded': {'attr': 'setDateAdded', 'convert': str},
-                'mediatype': {'attr': 'setMediaType', 'convert': str},
-                'dbid': {'attr': 'setDbId', 'convert': int},
+                'director': {'attr': 'setDirectors', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'mpaa': {'attr': 'setMpaa', 'convert': str, 'classinfo': str},
+                'plot': {'attr': 'setPlot', 'convert': str, 'classinfo': str},
+                'plotoutline': {'attr': 'setPlotOutline', 'convert': str, 'classinfo': str},
+                'title': {'attr': 'setTitle', 'convert': str, 'classinfo': str},
+                'originaltitle': {'attr': 'setOriginalTitle', 'convert': str, 'classinfo': str},
+                'sorttitle': {'attr': 'setSortTitle', 'convert': str, 'classinfo': str},
+                'duration': {'attr': 'setDuration', 'convert': int, 'classinfo': int},
+                'studio': {'attr': 'setStudios', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'tagline': {'attr': 'setTagLine', 'convert': str, 'classinfo': str},
+                'writer': {'attr': 'setWriters', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'tvshowtitle': {'attr': 'setTvShowTitle', 'convert': str, 'classinfo': str},
+                'premiered': {'attr': 'setPremiered', 'convert': str, 'classinfo': str},
+                'status': {'attr': 'setTvShowStatus', 'convert': str, 'classinfo': str},
+                'set': {'attr': 'setSet', 'convert': str, 'classinfo': str},
+                'setoverview': {'attr': 'setSetOverview', 'convert': str, 'classinfo': str},
+                'tag': {'attr': 'setTags', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'imdbnumber': {'attr': 'setIMDBNumber', 'convert': str, 'classinfo': str},
+                'code': {'attr': 'setProductionCode', 'convert': str, 'classinfo': str},
+                'aired': {'attr': 'setFirstAired', 'convert': str, 'classinfo': str},
+                'credits': {'attr': 'setWriters', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'lastplayed': {'attr': 'setLastPlayed', 'convert': str, 'classinfo': str},
+                'album': {'attr': 'setAlbum', 'convert': str, 'classinfo': str},
+                'artist': {'attr': 'setArtists', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'votes': {'attr': 'setVotes', 'convert': int, 'classinfo': int},
+                'path': {'attr': 'setPath', 'convert': str, 'classinfo': str},
+                'trailer': {'attr': 'setTrailer', 'convert': str, 'classinfo': str},
+                'dateadded': {'attr': 'setDateAdded', 'convert': str, 'classinfo': str},
+                'date': {'attr': 'setDateAdded', 'convert': str, 'classinfo': str},
+                'mediatype': {'attr': 'setMediaType', 'convert': str, 'classinfo': str},
+                'dbid': {'attr': 'setDbId', 'convert': int, 'classinfo': int},
             }
         },
         'music': {
             'tag_getter': 'getMusicInfoTag',
             'tag_attr': {
-                'tracknumber': {'attr': 'setTrack', 'convert': int},
-                'discnumber': {'attr': 'setDisc', 'convert': int},
-                'duration': {'attr': 'setDuration', 'convert': int},
-                'year': {'attr': 'setYear', 'convert': int},
-                'genre': {'attr': 'setGenres', 'convert': lambda x: [x]},
-                'album': {'attr': 'setAlbum', 'convert': str},
-                'artist': {'attr': 'setArtist', 'convert': str},
-                'title': {'attr': 'setTitle', 'convert': str},
-                'rating': {'attr': 'setRating', 'convert': float},
-                'userrating': {'attr': 'setUserRating', 'convert': int},
-                'lyrics': {'attr': 'setLyrics', 'convert': str},
-                'playcount': {'attr': 'setPlayCount', 'convert': int},
-                'lastplayed': {'attr': 'setLastPlayed', 'convert': str},
-                'mediatype': {'attr': 'setMediaType', 'convert': str},
+                'tracknumber': {'attr': 'setTrack', 'convert': int, 'classinfo': int},
+                'discnumber': {'attr': 'setDisc', 'convert': int, 'classinfo': int},
+                'duration': {'attr': 'setDuration', 'convert': int, 'classinfo': int},
+                'year': {'attr': 'setYear', 'convert': int, 'classinfo': int},
+                'genre': {'attr': 'setGenres', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'album': {'attr': 'setAlbum', 'convert': str, 'classinfo': str},
+                'artist': {'attr': 'setArtist', 'convert': str, 'classinfo': str},
+                'title': {'attr': 'setTitle', 'convert': str, 'classinfo': str},
+                'rating': {'attr': 'setRating', 'convert': float, 'classinfo': float},
+                'userrating': {'attr': 'setUserRating', 'convert': int, 'classinfo': int},
+                'lyrics': {'attr': 'setLyrics', 'convert': str, 'classinfo': str},
+                'playcount': {'attr': 'setPlayCount', 'convert': int, 'classinfo': int},
+                'lastplayed': {'attr': 'setLastPlayed', 'convert': str, 'classinfo': str},
+                'mediatype': {'attr': 'setMediaType', 'convert': str, 'classinfo': str},
                 'dbid': {'route': 'set_info_music_dbid'},
-                'listeners': {'attr': 'setListeners', 'convert': int},
-                'musicbrainztrackid': {'attr': 'setMusicBrainzTrackID', 'convert': str},
-                'musicbrainzartistid': {'attr': 'setMusicBrainzArtistID', 'convert': lambda x: [x]},
-                'musicbrainzalbumid': {'attr': 'setMusicBrainzAlbumID', 'convert': str},
-                'musicbrainzalbumartistid': {'attr': 'setMusicBrainzAlbumArtistID', 'convert': lambda x: [x]},
-                'comment': {'attr': 'setComment', 'convert': str},
-                'albumartist': {'attr': 'setAlbumArtist', 'convert': str},  # Not listed in setInfo docs but included for forward compatibility
+                'listeners': {'attr': 'setListeners', 'convert': int, 'classinfo': int},
+                'musicbrainztrackid': {'attr': 'setMusicBrainzTrackID', 'convert': str, 'classinfo': str},
+                'musicbrainzartistid': {'attr': 'setMusicBrainzArtistID', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'musicbrainzalbumid': {'attr': 'setMusicBrainzAlbumID', 'convert': str, 'classinfo': str},
+                'musicbrainzalbumartistid': {'attr': 'setMusicBrainzAlbumArtistID', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'comment': {'attr': 'setComment', 'convert': str, 'classinfo': str},
+                'albumartist': {'attr': 'setAlbumArtist', 'convert': str, 'classinfo': str},  # Not listed in setInfo docs but included for forward compatibility
             }
         },
         'game': {
             'tag_getter': 'getGameInfoTag',
             'tag_attr': {
-                'title': {'attr': 'setTitle', 'convert': str},
-                'platform': {'attr': 'setPlatform', 'convert': str},
-                'genres': {'attr': 'setGenres', 'convert': lambda x: [x]},
-                'publisher': {'attr': 'setPublisher', 'convert': str},
-                'developer': {'attr': 'setDeveloper', 'convert': str},
-                'overview': {'attr': 'setOverview', 'convert': str},
-                'year': {'attr': 'setYear', 'convert': int},
-                'gameclient': {'attr': 'setGameClient', 'convert': str},
+                'title': {'attr': 'setTitle', 'convert': str, 'classinfo': str},
+                'platform': {'attr': 'setPlatform', 'convert': str, 'classinfo': str},
+                'genres': {'attr': 'setGenres', 'convert': lambda x: [x], 'classinfo': (list, tuple)},
+                'publisher': {'attr': 'setPublisher', 'convert': str, 'classinfo': str},
+                'developer': {'attr': 'setDeveloper', 'convert': str, 'classinfo': str},
+                'overview': {'attr': 'setOverview', 'convert': str, 'classinfo': str},
+                'year': {'attr': 'setYear', 'convert': int, 'classinfo': int},
+                'gameclient': {'attr': 'setGameClient', 'convert': str, 'classinfo': str},
             }
         }
     }
 
-    def __init__(self, listitem, tag_type: str = 'video'):
+    def __init__(self, listitem, tag_type: str = 'video', type_check=False):
+        """
+        Pass xbmcgui.ListItem() to listitem with tag_type to the library type normally in li.setInfo(type=)
+        Optional set type_check=
+            - False: (default)
+                - Slightly increases performance by avoiding additional internal type checks
+                - Relys on Kodi Python API raising a TypeError to determine when to force type conversion
+                - Kodi creates EXCEPTION log spam when infolabels require type conversion
+            - True:
+                - Slightly descreases performance by requiring additional internal type checks
+                - Uses internal isinstance type check to determine when to force type conversion
+                - Prevents Kodi EXCEPTION log spam when infolabels require type conversion
+        """
         self._listitem = listitem
         self._tag_type = tag_type
         self._tag_attr = self.INFO_TAG_ATTR[tag_type]['tag_attr']
         self._info_tag = getattr(self._listitem, self.INFO_TAG_ATTR[tag_type]['tag_getter'])()
+        self._type_chk = type_check
 
     def set_info(self, infolabels: dict):
         """ Wrapper for compatibility with Matrix ListItem.setInfo() method """
@@ -116,28 +130,32 @@ class ListItemInfoTag():
             if v is None:
                 continue
             try:
-                func = getattr(self._info_tag, self._tag_attr[k]['attr'])
+                _tag_attr = self._tag_attr[k]
+                func = getattr(self._info_tag, _tag_attr['attr'])
+                if self._type_chk and not isinstance(v, _tag_attr['classinfo']):
+                    raise TypeError
                 func(v)
+
             except KeyError:
                 if k not in self._tag_attr:
                     log_msg = f'[script.module.infotagger] set_info:\nKeyError: {k}'
                     kodi_log(log_msg, level=LOGINFO)
                     continue
 
-                if self._tag_attr[k].get('skip'):
+                if _tag_attr.get('skip'):
                     continue
 
-                if 'route' in self._tag_attr[k]:
-                    getattr(self, self._tag_attr[k]['route'])(v, infolabels)
+                if 'route' in _tag_attr:
+                    getattr(self, _tag_attr['route'])(v, infolabels)
                     continue
 
-                log_msg = self._tag_attr[k].get('log_msg') or ''
+                log_msg = _tag_attr.get('log_msg') or ''
                 log_msg = f'[script.module.infotagger] set_info:\nKeyError: {log_msg}'
                 kodi_log(log_msg, level=LOGINFO)
                 continue
 
             except TypeError:
-                func(self._tag_attr[k]['convert'](v))  # Attempt to force conversion to correct type
+                func(_tag_attr['convert'](v))  # Attempt to force conversion to correct type
 
     def set_info_music_dbid(self, dbid: int, infolabels: dict, *args, **kwargs):
         """ Wrapper for InfoTagMusic.setDbId to retrieve mediatype """
