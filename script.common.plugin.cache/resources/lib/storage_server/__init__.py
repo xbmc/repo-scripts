ef88f0a5e5eb6191296d7adfcb2150d120e67b65
anxdpanic <anxdpanic@users.noreply.github.com>
2023-02-27 16:13:07 -0500

    [script.common.plugin.cache] 3.0.0 (#2415)

---
+++ script.common.plugin.cache/resources/lib/storage_server/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2019 anxdpanic
+
+    This file is part of script.common.plugin.cache
+
+    SPDX-License-Identifier: GPL-3.0-only
+    See LICENSES/GPL-3.0-only.txt for more information.
+"""
+
+__all__ = ['StorageServer', 'storageserverdummy']
