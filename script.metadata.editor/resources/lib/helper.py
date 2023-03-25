f119c86becc218cd99ed9f1deaea2a18f4a34e7f
b-jesch <birger.jesch@gmail.com>
2023-03-25 10:45:14 +0100

    Fix deprecated xbmc.translatePath by transferring translatePath to xbmcvfs, correct spelling for german language, bump version.

--- script.metadata.editor/resources/lib/helper.py
+++ script.metadata.editor/resources/lib/helper.py
@@ -24,7 +24,7 @@ from contextlib import contextmanager
 
 ADDON = xbmcaddon.Addon()
 ADDON_ID = ADDON.getAddonInfo('id')
-ADDON_DATA_PATH = os.path.join(xbmc.translatePath("special://profile/addon_data/%s" % ADDON_ID))
+ADDON_DATA_PATH = os.path.join(xbmcvfs.translatePath("special://profile/addon_data/%s" % ADDON_ID))
 
 NOTICE = xbmc.LOGINFO
 WARNING = xbmc.LOGWARNING
