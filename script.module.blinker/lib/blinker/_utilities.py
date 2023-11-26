885509ffe373e78d733e80911c0bb1d1007bb03d
L2501 <L2501@users.noreply.github.com>
2023-11-26 12:32:07 +0000

    [script.module.blinker] 1.7.0

--- script.module.blinker/lib/blinker/_utilities.py
+++ script.module.blinker/lib/blinker/_utilities.py
@@ -1,74 +1,14 @@
+from __future__ import annotations
+
+import typing as t
 from weakref import ref
 
 from blinker._saferef import BoundMethodWeakref
 
-
-try:
-    callable
-except NameError:
-    def callable(object):
-        return hasattr(object, '__call__')
+IdentityType = t.Union[t.Tuple[int, int], str, int]
 
 
-try:
-    from collections import defaultdict
-except:
-    class defaultdict(dict):
-
-        def __init__(self, default_factory=None, *a, **kw):
-            if (default_factory is not None and
-                not hasattr(default_factory, '__call__')):
-                raise TypeError('first argument must be callable')
-            dict.__init__(self, *a, **kw)
-            self.default_factory = default_factory
-
-        def __getitem__(self, key):
-            try:
-                return dict.__getitem__(self, key)
-            except KeyError:
-                return self.__missing__(key)
-
-        def __missing__(self, key):
-            if self.default_factory is None:
-                raise KeyError(key)
-            self[key] = value = self.default_factory()
-            return value
-
-        def __reduce__(self):
-            if self.default_factory is None:
-                args = tuple()
-            else:
-                args = self.default_factory,
-            return type(self), args, None, None, self.items()
-
-        def copy(self):
-            return self.__copy__()
-
-        def __copy__(self):
-            return type(self)(self.default_factory, self)
-
-        def __deepcopy__(self, memo):
-            import copy
-            return type(self)(self.default_factory,
-                              copy.deepcopy(self.items()))
-
-        def __repr__(self):
-            return 'defaultdict(%s, %s)' % (self.default_factory,
-                                            dict.__repr__(self))
-
-
-try:
-    from contextlib import contextmanager
-except ImportError:
-    def contextmanager(fn):
-        def oops(*args, **kw):
-            raise RuntimeError("Python 2.5 or above is required to use "
-                               "context managers.")
-        oops.__name__ = fn.__name__
-        return oops
-
-class _symbol(object):
-
+class _symbol:
     def __init__(self, name):
         """Construct a new named symbol."""
         self.__name__ = self.name = name
@@ -78,10 +18,12 @@ class _symbol(object):
 
     def __repr__(self):
         return self.name
-_symbol.__name__ = 'symbol'
 
 
-class symbol(object):
+_symbol.__name__ = "symbol"
+
+
+class symbol:
     """A constant symbol.
 
     >>> symbol('foo') is symbol('foo')
@@ -95,7 +37,8 @@ class symbol(object):
     Repeated calls of symbol('name') will all return the same instance.
 
     """
-    symbols = {}
+
+    symbols = {}  # type: ignore[var-annotated]
 
     def __new__(cls, name):
         try:
@@ -104,18 +47,12 @@ class symbol(object):
             return cls.symbols.setdefault(name, _symbol(name))
 
 
-try:
-    text = (str, unicode)
-except NameError:
-    text = str
-
-
-def hashable_identity(obj):
-    if hasattr(obj, '__func__'):
-        return (id(obj.__func__), id(obj.__self__))
-    elif hasattr(obj, 'im_func'):
-        return (id(obj.im_func), id(obj.im_self))
-    elif isinstance(obj, text):
+def hashable_identity(obj: object) -> IdentityType:
+    if hasattr(obj, "__func__"):
+        return (id(obj.__func__), id(obj.__self__))  # type: ignore[attr-defined]
+    elif hasattr(obj, "im_func"):
+        return (id(obj.im_func), id(obj.im_self))  # type: ignore[attr-defined]
+    elif isinstance(obj, (int, str)):
         return obj
     else:
         return id(obj)
@@ -127,8 +64,13 @@ WeakTypes = (ref, BoundMethodWeakref)
 class annotatable_weakref(ref):
     """A weakref.ref that supports custom instance attributes."""
 
+    receiver_id: t.Optional[IdentityType]
+    sender_id: t.Optional[IdentityType]
 
-def reference(object, callback=None, **annotations):
+
+def reference(  # type: ignore[no-untyped-def]
+    object, callback=None, **annotations
+) -> annotatable_weakref:
     """Return an annotated weak ref."""
     if callable(object):
         weak = callable_reference(object, callback)
@@ -136,19 +78,19 @@ def reference(object, callback=None, **annotations):
         weak = annotatable_weakref(object, callback)
     for key, value in annotations.items():
         setattr(weak, key, value)
-    return weak
+    return weak  # type: ignore[no-any-return]
 
 
 def callable_reference(object, callback=None):
     """Return an annotated weak ref, supporting bound instance methods."""
-    if hasattr(object, 'im_self') and object.im_self is not None:
+    if hasattr(object, "im_self") and object.im_self is not None:
         return BoundMethodWeakref(target=object, on_delete=callback)
-    elif hasattr(object, '__self__') and object.__self__ is not None:
+    elif hasattr(object, "__self__") and object.__self__ is not None:
         return BoundMethodWeakref(target=object, on_delete=callback)
     return annotatable_weakref(object, callback)
 
 
-class lazy_property(object):
+class lazy_property:
     """A @property that is only evaluated once."""
 
     def __init__(self, deferred):
