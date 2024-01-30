9b05daaadd7a0a0e98101c10cc5410c097edb29b
Kyle Johnson <pkscout@kodi.tv>
2024-01-30 13:37:11 -0500

    [script.tvmaze.integration] v.0.8.0 (#2582)
    - fix for translatePath error on Nexus
    - updated translation files
    - code reformatting

--- script.tvmaze.integration/resources/lib/tvmisettings.py
+++ script.tvmaze.integration/resources/lib/tvmisettings.py
@@ -1,18 +1,18 @@
 
 from resources.lib.kodisettings import *
 
-SETTINGSLIST = [ {'name': 'version_upgrade', 'default':''},
-                 {'name': 'show_cache', 'default': ''},
-                 {'name': 'tvmaze_user', 'default': ''},
-                 {'name': 'tvmaze_apikey', 'default': ''},
-                 {'name': 'add_followed', 'default': False},
-                 {'name': 'mark_acquired', 'default': True},
-                 {'name': 'mark_watched', 'default': True},
-                 {'name': 'mark_on_remove', 'default': True},
-                 {'name': 'percent_watched', 'default': 85},
-                 {'name': 'hidemenu', 'default': False},
-                 {'name': 'debug', 'default': False}
-               ]
+SETTINGSLIST = [{'name': 'version_upgrade', 'default': ''},
+                {'name': 'show_cache', 'default': ''},
+                {'name': 'tvmaze_user', 'default': ''},
+                {'name': 'tvmaze_apikey', 'default': ''},
+                {'name': 'add_followed', 'default': False},
+                {'name': 'mark_acquired', 'default': True},
+                {'name': 'mark_watched', 'default': True},
+                {'name': 'mark_on_remove', 'default': True},
+                {'name': 'percent_watched', 'default': 85},
+                {'name': 'hidemenu', 'default': False},
+                {'name': 'debug', 'default': False}
+                ]
 
 
 def loadSettings():
@@ -26,13 +26,13 @@ def loadSettings():
     settings['ADDONICON'] = ADDONICON
     settings['ADDONLANGUAGE'] = ADDONLANGUAGE
     for item in SETTINGSLIST:
-        if isinstance( item['default'], bool ):
+        if isinstance(item['default'], bool):
             getset = getSettingBool
-        elif isinstance( item['default'], int ):
+        elif isinstance(item['default'], int):
             getset = getSettingInt
-        elif isinstance( item['default'], float ):
+        elif isinstance(item['default'], float):
             getset = getSettingNumber
         else:
             getset = getSettingString
-        settings[item['name']] = getset( item['name'], item['default'] )
+        settings[item['name']] = getset(item['name'], item['default'])
     return settings
