dacfc57aaa58923a0651dae82f8e1a294363d587
ronie <ronie@kodi.tv>
2024-04-05 19:46:03 +0200

    [script.module.iptcinfo3] 2.1.4+matrix.2

--- script.module.iptcinfo3/lib/iptcinfo3.py
+++ script.module.iptcinfo3/lib/iptcinfo3.py
@@ -790,7 +790,7 @@ class IPTCInfo:
                         try:
                             cs = unpack('!H', temp)[0]
                         except Exception:  # TODO better exception
-                            logger.warn('WARNING: problems with charset recognition (%r)', temp)
+                            #logger.warn('WARNING: problems with charset recognition (%r)', temp)
                             cs = None
                         if cs in c_charset:
                             self.inp_charset = c_charset[cs]
