0dea9213ccf535339ade9e5ff27ef4eaaf09073c
i96751414 <i96751414@gmail.com>
2024-09-24 18:00:50 +0000

    [script.logviewer] 2.1.7+matrix.1

--- script.logviewer/resources/lib/service.py
+++ script.logviewer/resources/lib/service.py
@@ -14,18 +14,18 @@ from resources.lib.logreader import LogReader
 class Monitor(xbmc.Monitor):
     def __init__(self):
         super(Monitor, self).__init__()
-        self._running = False
+        self._port = utils.get_int_setting("port")
         self._error_popup_runner = None
         self._http_server_runner = None
+        self._lock = threading.Lock()
 
     def start(self):
-        self._running = True
         self.onSettingsChanged()
 
     def stop(self):
-        self._running = False
-        self._stop_error_popup_runner()
-        self._stop_http_server_runner()
+        with self._lock:
+            self._stop_error_popup_runner()
+            self._stop_http_server_runner()
 
     def _start_error_popup_runner(self):
         if self._error_popup_runner is None:
@@ -37,49 +37,61 @@ class Monitor(xbmc.Monitor):
         if self._error_popup_runner is not None:
             logging.debug("Stopping error popup runner")
             self._error_popup_runner.stop()
+            self._error_popup_runner.join()
             self._error_popup_runner = None
 
     def _start_http_server_runner(self):
         if self._http_server_runner is None:
             logging.debug("Starting http server runner")
-            self._http_server_runner = HTTPServerRunner(self, utils.get_int_setting("port"))
+            self._http_server_runner = HTTPServerRunner(self._port)
             self._http_server_runner.start()
 
     def _stop_http_server_runner(self):
         if self._http_server_runner is not None:
             logging.debug("Stopping http server runner")
             self._http_server_runner.stop()
+            self._http_server_runner.join()
             self._http_server_runner = None
 
     def onSettingsChanged(self):
-        if self._running:
-            self._start_error_popup_runner() if utils.get_boolean_setting(
-                "error_popup") else self._stop_error_popup_runner()
+        with self._lock:
+            http_port = utils.get_int_setting("port")
+            run_http_server = utils.get_boolean_setting("http_server")
+            run_error_popup = utils.get_boolean_setting("error_popup")
 
-            self._start_http_server_runner() if utils.get_boolean_setting(
-                "http_server") else self._stop_http_server_runner()
+            if run_http_server:
+                if http_port != self._port:
+                    self._port = http_port
+                    self._stop_http_server_runner()
+                self._start_http_server_runner()
+            else:
+                self._stop_http_server_runner()
+
+            if run_error_popup:
+                self._start_error_popup_runner()
+            else:
+                self._stop_error_popup_runner()
 
 
 class HTTPServerRunner(threading.Thread):
-    def __init__(self, monitor, port):
-        self._monitor = monitor
+    def __init__(self, port):
         self._port = port
         self._server = None
         super(HTTPServerRunner, self).__init__()
 
     def run(self):
-        self._server = ThreadedHTTPServer(("", self._port), ServerHandler)
-        self._server.daemon_threads = True
-
-        logging.debug("Server started at port {}".format(self._port))
-        logging.debug("Local IP: {}".format(xbmc.getIPAddress()))
-
-        self._server.serve_until_shutdown(self._monitor.abortRequested)
-        self._server.server_close()
+        self._server = server = ThreadedHTTPServer(("", self._port), ServerHandler)
+        logging.debug("Server started at port %d", self._port)
+        logging.debug("Local IP is %s", xbmc.getIPAddress())
+        server.serve_forever()
+        logging.debug("Closing server")
+        server.server_close()
+        logging.debug("Server terminated")
 
     def stop(self):
         if self._server is not None:
-            self._server.shutdown_server()
+            self._server.shutdown()
+            self._server = None
 
 
 class ErrorPopupRunner(threading.Thread):
@@ -106,17 +118,14 @@ class ErrorPopupRunner(threading.Thread):
         # Ignore initial errors
         reader.tail()
 
-        while not self._monitor.abortRequested() and self._running:
+        while not self._monitor.waitForAbort(1) and self._running:
             content = reader.tail()
             parsed_errors = logviewer.parse_errors(content, set_style=True, exceptions_only=exceptions)
             if parsed_errors:
                 logviewer.window(utils.ADDON_NAME, parsed_errors, default=utils.is_default_window())
-            self._monitor.waitForAbort(1)
 
     def stop(self):
         self._running = False
-        # Wait for thread to stop
-        self.join()
 
 
 def run(start_delay=20):
