dcba6a023df62cade48d6d79001087a9aeffd3b4
bossanova808 <jd@imagescience.com.au>
2024-10-10 01:39:05 +0000

    [script.cabertoss] 1.0.0

---
+++ script.cabertoss/README.md
@@ -0,0 +1,21 @@
+
+Caber Toss for Kodi
+===================================
+
+_script.cabertoss_
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/bossanova808)
+
+A script to quickly toss your Kodi logs from your Kodi client machine to somewhere more handy, for easier review.
+
+When something unexpected or bad happens, just run this add-on to copy the latest Kodi logs to your chosen destination.
+
+Even better, bind this to a remote button (use `Runscript(script.cabertoss)`) so you can carry on with your relaxing and deal with the issue later (without having to wade through all the stuff you did after the issue happened!).
+
+
+
+
+
+
+
+
---
+++ script.cabertoss/resources/lib/store.py
@@ -0,0 +1,37 @@
+from bossanova808.constants import *
+from bossanova808.logger import Logger
+
+
+class Store:
+    """
+    Helper class to read in and store the addon settings, and to provide a centralised store
+
+    from resources.lib.store import Store
+    log(f'{Store.whatever}')
+    """
+
+    # Static class variables, referred to elsewhere by Store.whatever
+    # https://docs.python.org/3/faq/programming.html#how-do-i-create-static-class-data-and-static-class-methods
+    replaces = (('//.+?:.+?@', '//USER:PASSWORD@'), ('<user>.+?</user>', '<user>USER</user>'), ('<pass>.+?</pass>', '<pass>PASSWORD</pass>'),)
+    destination_path = None
+
+    def __init__(self):
+        """
+        Load in the addon settings and do basic initialisation stuff
+        """
+        Store.load_config_from_settings()
+
+    @staticmethod
+    def load_config_from_settings():
+        """
+        Load in the addon settings, at start or reload them if they have been changed
+        Log each setting as it is loaded
+        """
+        Logger.info("Loading configuration from settings")
+        Store.destination_path = ADDON.getSetting('log_path')
+        Logger.info(f'Logs will be tossed to: {Store.destination_path}')
+
+
+
+
+
