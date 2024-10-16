752333a15b64e8cf2af88fef6a90942fe13933fa
bossanova808 <jd@imagescience.com.au>
2024-10-16 02:48:21 +0000

    [script.service.checkpreviousepisode] 0.4.8

--- script.service.checkpreviousepisode/default.py
+++ script.service.checkpreviousepisode/default.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from resources.lib import check_previous_episode
 import sys
+from bossanova808 import exception_logger
 
 if __name__ == "__main__":
-    check_previous_episode.run(sys.argv)
+    with exception_logger.log_exception():
+        check_previous_episode.run(sys.argv)
