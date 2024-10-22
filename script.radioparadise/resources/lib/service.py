098ed70324b2d1dfeb4494aa43e60ef808dfa27e
Alexander Dietrich <alexander@dietrich.cx>
2024-10-22 23:28:45 +0200

    [script.radioparadise] 2.1.0

--- script.radioparadise/resources/lib/service.py
+++ script.radioparadise/resources/lib/service.py
@@ -1,20 +1,20 @@
 import time
-import traceback
 
 import requests
 import xbmc
 import xbmcaddon
 import xbmcgui
 
-from .radioparadise import STREAM_INFO, NowPlaying, build_key
+from .logger import Logger
+from .radioparadise import CHANNEL_INFO, NowPlaying, build_key
 
-
-DEVELOPMENT = False
 
 EXPIRATION_DELAY = 10
 
 RESTART_DELAY = 1.0
 RESTART_TIMEOUT = 1.0
+
+LOG = Logger('rp_service')
 
 
 class Song():
@@ -196,13 +196,13 @@ class Player(xbmc.Player):
 
         song_key = build_key((song_data['artist'], song_data['title']))
         self.song = Song(song_key, song_data, fanart, start_time)
-        log(f'Song: {self.song}')
+        LOG.log(f'Song: {self.song}')
         self.update_player()
 
     def onAVStarted(self):
-        if self.isPlaying() and self.getPlayingFile() in STREAM_INFO:
+        if self.isPlaying() and self.getPlayingFile() in CHANNEL_INFO:
             url = self.getPlayingFile()
-            info = STREAM_INFO[url]
+            info = CHANNEL_INFO[url]
             # Kodi switches to fullscreen for FLAC, but not AAC
             if url == info['url_aac']:
                 xbmc.executebuiltin('Action(FullScreen)')
@@ -219,12 +219,12 @@ class Player(xbmc.Player):
         self.reset()
 
     def onPlayBackStarted(self):
-        if self.isPlaying() and self.getPlayingFile() in STREAM_INFO:
+        if self.isPlaying() and self.getPlayingFile() in CHANNEL_INFO:
             url = self.getPlayingFile()
             self.stream_url = url
             self.restart_time = 0
-            info = STREAM_INFO[url]
-            self.now_playing.set_channel(info['channel'])
+            info = CHANNEL_INFO[url]
+            self.now_playing.set_channel(info['channel_id'])
         else:
             self.reset()
 
@@ -259,18 +259,8 @@ class Slideshow():
         return result
 
 
-def log(message, level=None):
-    """Write to the Kodi log."""
-    if level is not None:
-        xbmc.log(f'rp_service: {message}', level)
-    elif DEVELOPMENT:
-        xbmc.log(f'rp_service: {message}', xbmc.LOGINFO)
-    else:
-        xbmc.log(f'rp_service: {message}', xbmc.LOGDEBUG)
-
-
 def run_service():
-    log('Service started.')
+    LOG.log('Service started.')
     player = Player()
     monitor = xbmc.Monitor()
     while not monitor.abortRequested():
@@ -279,8 +269,5 @@ def run_service():
         try:
             player.update()
         except Exception as e:
-            if DEVELOPMENT:
-                log(traceback.format_exc(), xbmc.LOGERROR)
-            else:
-                log(repr(e), xbmc.LOGERROR)
-    log('Service exiting.')
+            LOG.exception(e)
+    LOG.log('Service exiting.')
