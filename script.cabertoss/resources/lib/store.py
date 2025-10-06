ae6bf9d942289657091063498988d0c789254cbf
bossanova808 <jd@imagescience.com.au>
2025-10-06 21:58:36 +0000

    [script.cabertoss] 1.0.2

--- script.cabertoss/resources/lib/store.py
+++ script.cabertoss/resources/lib/store.py
@@ -1,6 +1,6 @@
-from bossanova808.constants import *
+from bossanova808.constants import ADDON
 from bossanova808.logger import Logger
-from resources.lib.clean import *
+from resources.lib.clean import clean_log
 
 
 class Store:
@@ -13,7 +13,8 @@ class Store:
 
     # Static class variables, referred to elsewhere by Store.whatever
     # https://docs.python.org/3/faq/programming.html#how-do-i-create-static-class-data-and-static-class-methods
-    destination_path = None
+    destination_path: str = ''
+    crashlog_max_days: int = 3
 
     def __init__(self):
         """
@@ -24,15 +25,15 @@ class Store:
     @staticmethod
     def load_config_from_settings():
         """
-        Load in the addon settings, at start or reload them if they have been changed
-        Log each setting as it is loaded
+        Load the addon's configuration from persistent settings.
+
+        Reads the 'log_path' setting and assigns it to Store.destination_path, then logs the resolved path (sanitized with clean_log because these paths may be URLs with embedded user/password details). This is called at startup and when settings are reloaded; it has no return value.
         """
         Logger.info("Loading configuration from settings")
-        Store.destination_path = ADDON.getSetting('log_path')
-
-        Logger.info(f'Logs will be tossed to: {clean_log(Store.destination_path)}')
-
-
-
-
-
+        Store.destination_path = ADDON.getSetting('log_path') or ''
+        if Store.destination_path:
+            Logger.info(f'Logs will be tossed to: {clean_log(Store.destination_path)}')
+        else:
+            Logger.warning('No path set to toss logs to.')
+        Store.crashlog_max_days = int(ADDON.getSetting('crashlog_max_days')) or 3
+        Logger.info(f'Crashlog max days: {Store.crashlog_max_days}')
